02ead834db47bc3d32a429485e986d2e9e149e83
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2007-11-24 15:35:49 +0000

    Fix bug http://code.google.com/p/r-oce/issues/detail?id=1 (geod.demo used m in the example, not km)
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@4 3dc979fe-ab3f-0410-b117-8d365baceae5

--- demo/geod.dist.R
+++ demo/geod.dist.R
@@ -1,7 +1,7 @@
 library(oce);
-cat("Originally, the metre was defined as 1/10000 of the distance
+cat("Originally, the kilometre was defined as 1/10000 of the distance
 from the equator to the pole on a line running through Paris.  This
 deviates from the modern definition by",
 round(0.01*abs(1 - geod.dist(0,2,90,2)/1e4),digits=7),
-"%, according to the
+"percent, according to the
 geodesic distance calculated by geod.dist().\n")
