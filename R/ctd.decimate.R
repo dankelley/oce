dac3c56c5185994eedc11720125d56eeb9e24c9c
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2007-12-12 14:42:42 +0000

    Working on ctd.decimate(), trying to incorporate ideas from grid.section.  Stuck on shortening data frame items, now; see http://www.nabble.com/how-to-shorten-elements-in-a-data-frame--to14296509.html
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@75 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/ctd.decimate.R
+++ R/ctd.decimate.R
@@ -1,30 +1,55 @@
-ctd.decimate <- function(x, p=NULL, method=c("boxcar","lm"), e=1)
+ctd.decimate <- function(x, p, method=c("boxcar","lm"), e=1)
 # ADD: BIO method; spline; supsmu; ...
 {
   	if (!inherits(x, "ctd"))
     	stop("method is only for ctd objects")
-  	result <- x
+  	res <- x
   	n <- length(x$data$pressure)
-  	if (n < 2) {
-    	warning("too few data to trim.decimate()")
-  	}
- 	else {
-	  	method <- match.arg(method)
-		if (is.null(p)) {
-			# autoscale
-			dp.exact <- median(abs(diff(x$data$pressure)))
-			dp <- pretty(3 * dp.exact)[2] # try for 3 data at least
-			pout <- seq(0, dp * floor(max(x$data$pressure) / dp), dp)
-			log.item <- paste("modified by ctd.decimate(x, p=NULL, method=\"", method[1], "\")",sep="")
+  	if (n < 2) { 
+		warning("too few data to trim.decimate()")
+		return(res)
+	}
+	# Figure out pressure targets, pt
+	if (missing(p)) {
+		# autoscale
+		dp.exact <- median(abs(diff(x$data$pressure)))
+		dp <- pretty(3 * dp.exact)[2] # try for 3 data at least
+		pt <- seq(0, dp * floor(max(x$data$pressure) / dp), dp)
+		log.item <- paste("modified by ctd.decimate(x, p=NULL, method=\"", method[1], "\")",sep="")
+	} else {
+		if (length(p) == 1) {
+			pt <- seq(0, p * floor(max(x$data$pressure) / p), p)
+			log.item <- paste("modified by ctd.decimate(x, p=",p[1],", method=\"",method[1], "\")",sep="")
 		} else {
-			if (length(p) == 1) {
-				pout <- seq(0, p * floor(max(x$data$pressure) / p), p)
-				log.item <- paste("modified by ctd.decimate(x, p=",p[1],", method=\"",method[1], "\")",sep="")
-			} else {
-				pout <- p
-				log.item <- paste("modified by ctd.decimate(x, p=c(",p[1],",",p[2],",...), method=\"",method[1], "\")",sep="")
-			}
+			pt <- p
+			log.item <- paste("modified by ctd.decimate(x, p=c(",p[1],",",p[2],",...), method=\"",method[1], "\")",sep="")
 		}
+	}
+	npt <- length(pt)
+	# Step through each variable.
+	data.names <- names(x$data)
+	cat("A\n")
+	for (datum.name in data.names) {
+		cat("B\n")
+		if (datum.name != "pressure") {
+			cat("C1\n")
+			cat(datum.name, ":")
+			length(res$data[[datum.name]]) <- npt
+			cat("C2\n")
+			res$data[[datum.name]] <- approx(x$data[["pressure"]], x$data[[datum.name]], pt)$y
+			cat("C3\n")
+		}
+	}
+	# Now replace pressure
+#	length(res$data[["pressure"]]) <- npt
+	res$data[["pressure"]][1:npt] <- pt
+	res <- processing.log.append(res, log.item)
+	return(res)
+	stop()
+  	if (n < 2) { 
+		warning("too few data to trim.decimate()")
+  	} else {
+	  	method <- match.arg(method)
 		n <- length(pout)
 		# FIXME: should probably do this based on names(x$data)
 		# FIXME: maybe should do this with apply()???
@@ -90,10 +115,3 @@ ctd.decimate <- function(x, p=NULL, method=c("boxcar","lm"), e=1)
 	result <- processing.log.append(result, log.item)
 	return(result) # FIXME
 }
-
-# plot(cc$data$salinity, cc$data$pressure, cex=0.3)
-# S.smooth <- smooth.spline(cc$data$pressure, cc$data$salinity, df=163 / 6)
-# lines(predict(S.smooth, seq(0,50,1))$y, seq(0,50,1))
-# S.smooth <- smooth.spline(cc$data$pressure, cc$data$salinity, df=163 / 3)
-# lines(predict(S.smooth, seq(0,50,1))$y, seq(0,50,1), col="red")
-# SS<-smooth(cc$data$salinity)
