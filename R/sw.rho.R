295d7e94bd3e6ad5a4e7b50d0bd2905ef473e6f4
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2007-12-14 03:17:40 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@106 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/sw.rho.R
+++ R/sw.rho.R
@@ -1,5 +1,10 @@
-sw.rho <- function(S, t, p)
+sw.rho <- function(S, t=NULL, p=NULL)
 {
+	if ("ctd" == class(S)) {
+		t <- S$data$temperature
+		p <- S$data$pressure
+		S <- S$data$salinity # note: this destroys the ctd object
+	}
 	dim <- dim(S)
   	nS <- length(S)
   	nt <- length(t)
