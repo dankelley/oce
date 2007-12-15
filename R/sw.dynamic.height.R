74d78e2254f14e1b9e0da812e615e86743e9fff4
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2007-12-15 15:25:06 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@124 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/sw.dynamic.height.R
+++ R/sw.dynamic.height.R
@@ -1,10 +1,29 @@
-sw.dynamic.height <- function(ctd, pref=2000)
+sw.dynamic.height <- function(x, pref=2000)
 {
-	if (!inherits(ctd, "ctd")) stop("method is only for ctd objects")
-	g <- if (is.na(ctd$latitude)) 9.8 else gravity(ctd$latitude)
-	if (pref > max(ctd$data$pressure, na.rm=TRUE)) return(NA)
-	n <- length(ctd$data$pressure)
-	dzdp <- ((1/sw.rho(ctd) - 1/sw.rho(rep(35, n), rep(0,n), ctd$data$pressure))/g) * 1e4
-	integrand <- approxfun(ctd$data$pressure, dzdp, rule=2)
-	integrate(integrand, 0, pref)$value
+	height <- function(ctd, pref)
+	{
+		g <- if (is.na(ctd$latitude)) 9.8 else gravity(ctd$latitude)
+		if (pref > max(ctd$data$pressure, na.rm=TRUE)) return(NA)
+		np <- length(ctd$data$pressure)
+		# 1e4 dbar per pascal
+		dzdp <- ((1/sw.rho(ctd) - 1/sw.rho(rep(35,np),rep(0,np),ctd$data$pressure))/g)*1e4
+		integrand <- approxfun(ctd$data$pressure, dzdp, rule=2)
+		integrate(integrand, 0, pref)$value
+	}
+	if ("section" == class(x)) {
+		lon0 <- x$stations[[1]]$longitude
+		lat0 <- x$stations[[1]]$latitude
+		ns <- length(x$stations)
+		d <- vector("numeric", ns)
+		h <- vector("numeric", ns)
+		for (i in 1:ns) {
+			d[i] <- geod.dist(x$station[[i]]$latitude, x$station[[i]]$longitude, lat0, lon0)
+			h[i] <- height(x$station[[i]], pref)
+		}
+		return(list(distance=d, height=h))
+	} else if ("ctd" == class(x)) {
+		return(height(x, pref))
+	} else {
+		stop("method only works for 'section' or 'ctd' objects")
+	}
 }
