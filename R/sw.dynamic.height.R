2c79f120f325ca2fd4c5bd5c8170acae909facb7
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2007-12-15 01:37:25 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@119 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/sw.dynamic.height.R
+++ R/sw.dynamic.height.R
@@ -1,7 +1,7 @@
 sw.dynamic.height <- function(ctd, pref=2000)
 {
 	if (!inherits(ctd, "ctd")) stop("method is only for ctd objects")
-	g <- 9.8 # FIXEME: should have slight correction to g for latitude
+	g <- if (is.NA(ctd$latitude)) 9.8 else gravity(ctd$latitude)
 	if (pref > max(ctd$data$pressure, na.rm=TRUE)) return(NA)
 	n <- length(ctd$data$pressure)
 	dzdp <- ((1/sw.rho(ctd) - 1/sw.rho(rep(35, n), rep(0,n), ctd$data$pressure))/g) * 1e4
