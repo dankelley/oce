9b75ad44a1555304fa46af770caab6f72e92b587
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-12 20:18:22 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@193 3dc979fe-ab3f-0410-b117-8d365baceae5

---
+++ tests/tide.constituents.R
@@ -0,0 +1,11 @@
+create.constituents <- function(file="tide3.dat", nrows=146) # FIXME: work with rest of file
+{
+	x <- read.table(file=file, nrows=nrows, fill=TRUE, as.is=TRUE, header=FALSE)
+	data.frame(name=x$V1, frequency=x$V2, compare=x$V3, standard=x$V3!="", stringsAsFactors=FALSE)
+}
+tidal.constituents <- create.constituents()
+# save(tidal.constituents, file="oce/data/tidal.constituents.rda")
+stopifnot(tidal.constituents$name[1] == "Z0")
+stopifnot(tidal.constituents$name[2] == "SA")
+stopifnot(tidal.constituents$frequency[2] == 0.0001140741)
+stopifnot(tidal.constituents$compare[2] == "SSA")
