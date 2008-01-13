bd485a61c18de1172de0b9ae14ff22883664130b
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-13 19:50:23 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@212 3dc979fe-ab3f-0410-b117-8d365baceae5

--- tests/tide.constituents.R
+++ tests/tide.constituents.R
@@ -4,7 +4,7 @@ create.constituents <- function(file="tide3.dat", nrows=146) # FIXME: work with 
 	data.frame(name=x$V1, frequency=x$V2, compare=x$V3, standard=x$V3!="", stringsAsFactors=FALSE)
 }
 tidal.constituents <- create.constituents()
-# save(tidal.constituents, file="oce/data/tidal.constituents.rda")
+# These values are then hand-edited into the code of tide.constituents.R
 stopifnot(tidal.constituents$name[1] == "Z0")
 stopifnot(tidal.constituents$name[2] == "SA")
 stopifnot(tidal.constituents$frequency[2] == 0.0001140741)
