5f3992547582eb716bc58666000b98402d910e92
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-19 22:30:57 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@282 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/summary.tide.R
+++ R/summary.tide.R
@@ -1,4 +1,4 @@
-summary.tide <- function(object, p=0, ...)
+summary.tide <- function(object, p=0, constituent, ...)
 {
 	n <- length(object$p)
 	if (!missing(p)) ok <- (object$p <= p) else ok = seq(1, n)
@@ -8,9 +8,18 @@ summary.tide <- function(object, p=0, ...)
 	sig[object$p<0.05]  <- "*  "
 	sig[object$p<0.01]  <- "** "
 	sig[object$p<0.001] <- "***"
-	rval <- data.frame(Name=object$name[ok], Frequency=object$frequency[ok],
-                       Amplitude=object$amplitude[ok],	Phase=object$phase[ok],
-                       p=object$p[ok], sig=sig[ok]) # FIXME: do these p values make any sense?
+        if (missing(constituent)) {
+            rval <- data.frame(Name=object$name[ok], Frequency=object$frequency[ok],
+                               Amplitude=object$amplitude[ok],	Phase=object$phase[ok],
+                               p=object$p[ok], sig=sig[ok])
+        }
+        else {
+            i <- which(object$name==constituent)
+            if (length(i) == 0) stop("there is no such constituent '", constituent, "'")
+            rval <- data.frame(Name=object$name[i], Frequency=object$frequency[i],
+                               Amplitude=object$amplitude[i],	Phase=object$phase[i],
+                               p=object$p[i], sig=sig[i])
+        }
 	class(rval) <- c("data.frame", "summary.tide")
 	rval
 }
