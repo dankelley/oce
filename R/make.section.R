282accff1e0efb49bbec971a1b09ce2bd4daa037
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-20 18:56:22 +0000

    Add the rest of Foreman's (1977) tide-constituent data ... not used yet, though.
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@301 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/make.section.R
+++ R/make.section.R
@@ -1,47 +1,47 @@
 make.section <- function(item, ...)
 {
     if (inherits(item, "ctd")) {
-		extra.args <- list(...)
-		if (length(extra.args) < 1) stop("cannot make a section from one station")
-		num.stations <- 1 + length(extra.args)
-		station <- vector("list", num.stations)
-		stn <- vector("character", num.stations)
-		lon <- vector("numeric", num.stations)
-		lat <- vector("numeric", num.stations)
-		stn[1] <- item$metadata$station
-		lat[1] <- item$metadata$latitude
-		lon[1] <- item$metadata$longitude
-		station[[1]] <- item
-		for (i in 2:num.stations) {
-			stn[i] <- extra.args[[i-1]]$metadata$station
-			lat[i] <- extra.args[[i-1]]$metadata$latitude
-			lon[i] <- extra.args[[i-1]]$metadata$longitude
-			station[[i]] <- extra.args[[i-1]]
-			summary(extra.args[i-1])
-		}
-	} else if (inherits(item, "list")) {
-		args <- list(...)
-		if (length(args) < 2) {
-			stop("cannot make a section from one station")
-		}
-		num.stations <- length(args)
-		station <- vector("list", num.stations)
-		stn <- vector("character", num.stations)
-		lon <- vector("numeric", num.stations)
-		lat <- vector("numeric", num.stations)
-		for (i in 1:num.stations) {
-			stn[i] <- args[[i]]$metadata$station
-			lat[i] <- args[[i]]$metadata$latitude
-			lon[i] <- args[[i]]$metadata$longitude
-			station[[i]] <- args[[i]]
-		}
-	} else {
-		stop("first argument must be of class \"ctd\" or a \"list\"")
-	}
-	data <- list(station=station)
-	metadata <- list(header="",section.id="",station.id=stn,latitude=lat,longitude=lon)
-	log.item <- list(time=c(Sys.time()), action="created by make.section")
-	res <- list(data=data, metadata=metadata, processing.log=log.item)
-  	class(res) <- c("section", "oce")
-	res
+        extra.args <- list(...)
+        if (length(extra.args) < 1) stop("cannot make a section from one station")
+        num.stations <- 1 + length(extra.args)
+        station <- vector("list", num.stations)
+        stn <- vector("character", num.stations)
+        lon <- vector("numeric", num.stations)
+        lat <- vector("numeric", num.stations)
+        stn[1] <- item$metadata$station
+        lat[1] <- item$metadata$latitude
+        lon[1] <- item$metadata$longitude
+        station[[1]] <- item
+        for (i in 2:num.stations) {
+            stn[i] <- extra.args[[i-1]]$metadata$station
+            lat[i] <- extra.args[[i-1]]$metadata$latitude
+            lon[i] <- extra.args[[i-1]]$metadata$longitude
+            station[[i]] <- extra.args[[i-1]]
+            summary(extra.args[i-1])
+        }
+    } else if (inherits(item, "list")) {
+        args <- list(...)
+        if (length(args) < 2) {
+            stop("cannot make a section from one station")
+        }
+        num.stations <- length(args)
+        station <- vector("list", num.stations)
+        stn <- vector("character", num.stations)
+        lon <- vector("numeric", num.stations)
+        lat <- vector("numeric", num.stations)
+        for (i in 1:num.stations) {
+            stn[i] <- args[[i]]$metadata$station
+            lat[i] <- args[[i]]$metadata$latitude
+            lon[i] <- args[[i]]$metadata$longitude
+            station[[i]] <- args[[i]]
+        }
+    } else {
+        stop("first argument must be of class \"ctd\" or a \"list\"")
+    }
+    data <- list(station=station)
+    metadata <- list(header="",section.id="",station.id=stn,latitude=lat,longitude=lon)
+    log.item <- list(time=c(Sys.time()), action="created by make.section")
+    res <- list(data=data, metadata=metadata, processing.log=log.item)
+    class(res) <- c("section", "oce")
+    res
 }
--- tests/tide.R
+++ tests/tide.R
@@ -1,11 +1,222 @@
-create.constituents <- function(file="tide3.dat", nrows=146) # FIXME: work with rest of file
-{
-	x <- read.table(file=file, nrows=nrows, fill=TRUE, as.is=TRUE, header=FALSE)
-	data.frame(name=x$V1, frequency=x$V2, compare=x$V3, standard=x$V3!="", stringsAsFactors=FALSE)
+## These values are hand-edited into tide.constituents.R
+## See page 2 of Foreman 1977 for the formate of tide3.dat,
+## which is provided in t-tide as (what seems to be) an exact
+## copy of the appendix in Foreman (1977).
+
+debug <- 0                              # 0, 1 or 2
+file <- file("tide3.dat", "r")
+
+############################
+## Constituents [const]   ##
+############################
+
+name <- compare <- vector("character", 1000) # will trim later
+frequency <- vector("numeric", 1000)
+nconst <- 1
+while (TRUE) {
+    items <- scan(file, "character", nlines=1, quiet=TRUE)
+    if (debug > 2) print(items)
+    nitems <- length(items)
+    if (nitems == 0)
+        break;
+    if (nitems != 2 && nitems != 3) stop("wrong number of entries on line", nconst)
+    compare[nconst]   <- if (nitems == 2) ""  else items[3]
+    name[nconst]      <- items[1]
+    frequency[nconst] <- as.numeric(items[2])
+    nconst <- nconst + 1
 }
-tidal.constituents <- create.constituents()
-# These values are then hand-edited into the code of tide.constituents.R
-stopifnot(tidal.constituents$name[1] == "Z0")
-stopifnot(tidal.constituents$name[2] == "SA")
-stopifnot(tidal.constituents$frequency[2] == 0.0001140741)
-stopifnot(tidal.constituents$compare[2] == "SSA")
+nconst <- nconst - 1
+length(name) <- nconst
+length(frequency) <- nconst
+length(compare) <- nconst
+const <- data.frame(name=name, frequency=frequency, compare=compare, standard=compare!="")
+rownames(const) <- name
+
+stopifnot(const$name[1] == "Z0")
+stopifnot(const$name[2] == "SA")
+stopifnot(const$compare[2] == "SSA")
+stopifnot(const["M2",2] == 1/12.42060119816049912345)
+
+if (debug > -1) {
+    cat("\n")
+    cat("/-----------------------------------------------------------\\\n")
+    cat("| Constituent data in data.frame named 'const' of dim", dim(const), " |\n")
+    cat("\\-----------------------------------------------------------/\n")
+}
+
+############################
+## Satellites [sat]       ##
+############################
+get.satellite <- function(x, o)
+{
+    if (debug > 2) cat("  SAT '",x,"'\n",sep="")
+    ldel  <- as.numeric(substr(x,o+01,o+03))            # I3
+    mdel  <- as.numeric(substr(x,o+04,o+06))            # I3
+    ndel  <- as.numeric(substr(x,o+07,o+09))            # I3
+    ph    <- as.numeric(substr(x,o+10,o+13))            # F4.2
+    ee    <- as.numeric(substr(x,o+14,o+20))            # F7.4
+    ir    <- as.numeric(substr(x,o+22,o+22))            # 1x, I1
+    if (debug > 2) cat("  SAT  ldel=",ldel, "mdel=",mdel,"ndel=",ndel,"ph=",ph,"ee=",ee,"ir=",ir,"\n")
+    c(ldel, mdel, ndel, ph, ee, ir)
+}
+
+
+scan(file, "character", nlines=3, quiet=TRUE) # skip 3 lines
+
+nsat <- 1
+sat <- vector("list", 1000)             # will trim later
+while (TRUE) {
+    x <- readLines(file, n=1)
+    nx <- nchar(x)
+    if (nx < 10) break
+                                        # Line format and content
+                                        # 6X,A5,1X,6I3,F5.2,I4
+                                        # kon, ii,jj,kk,ll,mm,nn semi nj
+    kon <- gsub(" ", "", substr(x, 7, 11))
+    ii <- as.numeric(substr(x, 13, 15))
+    jj <- as.numeric(substr(x, 16, 18))
+    kk <- as.numeric(substr(x, 19, 21))
+    ll <- as.numeric(substr(x, 22, 24))
+    mm <- as.numeric(substr(x, 25, 27))
+    nn <- as.numeric(substr(x, 28, 30))
+    semi <- as.numeric(substr(x, 31, 35))
+    nj <- as.numeric(substr(x, 36, 39))
+    if (debug > 1) {
+        cat(">>>", x, "\n", sep="")
+        cat("kon=", kon, " ii=",ii," jj=",jj," kk=",kk," ll=",ll," mm=",mm," nn=",nn," semi=",semi," nj=",nj,"\n",sep="")
+    }
+    ldel <- vector("numeric", nj)
+    mdel <- vector("numeric", nj)
+    ndel <- vector("numeric", nj)
+    ph   <- vector("numeric", nj)
+    ee   <- vector("numeric", nj)
+    ir   <- vector("numeric", nj)
+    if (nj > 0) {
+
+        ## ALP1    1 -4  2  1  0  0 -.25   2
+        ## ALP1  -1  0  0 .75 0.0360R1   0 -1  0 .00 0.1906
+        is <- 1
+        while (is <= nj) {
+            xs <- readLines(file, n=1)
+            if (debug > 1) cat(">>>", xs, "'\n",sep="")
+            nxs <- nchar(xs)
+            if (nxs != 31 && nxs != 33 && nxs != 54 && nxs != 56 && nxs != 77 && nxs != 79) {
+                cat("GOT BAD LINE AS FOLLOWS:\n12345678901234567890123456789012345678901234567890\n",xs,"\n",sep="")
+                stop("need 31, 54, 77 or 79 chars, but got ", nxs)
+            }
+            s <- get.satellite(xs, 11)
+            ldel[is]<-s[1];mdel[is]<-s[2];ndel[is]<-s[3];ph[is]<-s[4];ee[is]<-s[5];ir[is]<-s[6]
+            is <- is + 1
+            if (nxs > 50) {
+                s <- get.satellite(xs, 34)
+                ldel[is]<-s[1];mdel[is]<-s[2];ndel[is]<-s[3];ph[is]<-s[4];ee[is]<-s[5];ir[is]<-s[6]
+                is <- is + 1
+            }
+            if (nxs > 70) {
+                s <- get.satellite(xs, 57)
+                ldel[is]<-s[1];mdel[is]<-s[2];ndel[is]<-s[3];ph[is]<-s[4];ee[is]<-s[5];ir[is]<-s[6]
+                is <- is + 1
+            }
+        }
+        if (debug > 1) print(data.frame(ldel=ldel,mdel=mdel,ndel=ndel,ph=ph,ee=ee,ir=ir))
+        sat[[nsat]] <- list(name=kon, ii=ii, jj=jj, kk=kk, mm=mm, nn=nn, semi=semi, nj=nj,
+                            ldel=ldel, mdel=mdel, ndel=ndel, ph=ph, ee=ee, ir=ir)
+    } else {
+        sat[[nsat]] <- list(name=kon, ii=ii, jj=jj, kk=kk, mm=mm, nn=nn, semi=semi, nj=nj,
+                            ldel=NULL, mdel=NULL, ndel=NULL, ph=NULL, ee=NULL, ir=NULL)
+    }
+    if (debug>0) cat("\n")
+    nsat <- nsat + 1
+}
+if (nsat < 1) stop("failed to read any satellite entries")
+
+length(sat) <- nsat - 1
+
+## This portion of the file ends as follows
+##      M3      3  0  0  0  0  0 -.50   1
+##      M3     0 -1  0 .50  .0564
+## and see Foreman (1977) page 2 for how to read this.  Here,
+## we'll check every aspect of the last satellite.
+
+stopifnot(sat[[44]]$name == "M3")
+stopifnot(sat[[44]]$ii == 3)
+stopifnot(sat[[44]]$jj == 0)
+stopifnot(sat[[44]]$kk == 0)
+stopifnot(sat[[44]]$mm == 0)
+stopifnot(sat[[44]]$nn == 0)
+stopifnot(sat[[44]]$semi == -0.50)
+stopifnot(sat[[44]]$nj == 1)
+stopifnot(sat[[44]]$ldel == 0)
+stopifnot(sat[[44]]$mdel == -1)
+stopifnot(sat[[44]]$ndel == 0)
+stopifnot(sat[[44]]$ph == 0.50)
+stopifnot(sat[[44]]$ee == 0.0564)
+stopifnot(is.na(sat[[44]]$ir))
+
+if (debug > -1) {
+    cat("\n")
+    cat("/---------------------------------------------------------\\\n")
+    cat("| Satellite data in list named 'sat' of length", length(sat), "         |\n")
+    cat("\\---------------------------------------------------------/\n")
+}
+
+##
+############################
+## Shallow [shallow]      ##
+############################
+
+# (6X,A5,I1,2X,4(F5.2,A5,5X))
+# KON = name of the shallow water constituent;
+# NJ = number of main constituents from which it is derived;
+# COEF,KONCO = combination number and name of these main constituents.
+
+shallow <- vector("list", 1000)         # will trim later
+nshallow <- 1
+while(TRUE) {
+    x <- readLines(file, n=1)
+    nx <- nchar(x)
+    if (nx < 10) break
+    kon <- gsub(" ", "", substr(x, 7, 11))
+    nj <- as.numeric(substr(x, 12, 12))
+    if (debug > 1) cat("kon: '", kon, "' nj=", nj, "\n",sep="")
+    coef <- konco <- NULL
+    if (nj > 0) {
+        for (j in 1:nj) {
+            o <- 15 + (j-1)*15
+            ##cat("  x='",x,"'\n",sep="")
+            ##cat("'", substr(x, o, nx), "'\n",sep="")
+            coef  <- c(coef,  as.numeric(substr(x, o, o+4)))
+            konco <- c(konco, gsub(" ", "", substr(x, o+5, o+9)))
+            ##cat("  coef= ",coef," konco= '",konco,"'\n",sep="")
+        }
+    }
+    shallow[[nshallow]] <- list(name=kon, nj=nj, coef=coef, konco=konco)
+    nshallow <- nshallow + 1
+}
+nshallow <- nshallow - 1
+length(shallow) <- nshallow
+
+stopifnot(length(shallow) == 101)
+stopifnot(shallow[[101]]$name == "ST35")
+stopifnot(shallow[[101]]$nj   == 4)
+stopifnot(shallow[[101]]$coef == c(3, 1, 1, 1))
+stopifnot(shallow[[101]]$konco == c("M2", "N2", "K2", "S2"))
+
+if (debug > -1) {
+    cat("\n")
+    cat("/---------------------------------------------------------\\\n")
+    cat("| Shallow data in list named 'shallow' of length", length(shallow), " |\n")
+    cat("\\---------------------------------------------------------/\n")
+}
+
+close(file)
+tideconst   <- const
+tidesat     <- sat
+tideshallow <- shallow
+
+cat("
+DO MANUALLY:
+    save(tideconst, tidesat, tideshallow, file=\"../data/tidesetup.rda\")
+TO SET UP THE SYSTEM.
+")
+
