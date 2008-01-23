269b8301cd7663a0c452abeef2797bb977e0c2a7
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-23 13:14:57 +0000

    git-svn-id: https://r-oce.googlecode.com/svn/trunk@329 3dc979fe-ab3f-0410-b117-8d365baceae5

--- tests/tide.R
+++ tests/tide.R
@@ -220,3 +220,10 @@ DO MANUALLY:
 TO SET UP THE SYSTEM.
 ")
 
+#####################
+## Low-level tests ##
+#####################
+                                        # Test against matlab t_astron
+a <- tidem.astron(as.POSIXct("2008-01-22 18:50:24"))
+stopifnot(all.equal(a$astro, c(1.2886, 0.3339, 0.8375, 0.1423, 0.0856, 0.7863), 0.001))
+stopifnot(all.equal(a$ader,  c(0.9661, 0.0366, 0.0027, 0.0003, 0.0001, 0.0000), 0.001))
