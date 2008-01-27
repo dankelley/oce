2f369f1825c36a26265d160ec01546a10d840e62
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-27 19:58:03 +0000

    Fix some problems with date and time classes (I hope!)
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@360 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/tidem.astron.R
+++ R/tidem.astron.R
@@ -1,9 +1,13 @@
 tidem.astron <- function(t)
 {
                                         # Code mimics t_astron in t_tide
-    d <- as.numeric(difftime(t, ISOdatetime(1899,12,31,12,0,0), units="days"))
+    d <- as.numeric(difftime(t, ISOdatetime(1899,12,31,12,0,0,tz="GMT"),
+                             units="days"))
     D <- d / 10000
     a <- matrix(c(1, d, D^2, D^3), 4, 1)
+
+    ##cat("d=",formatC(d,digits=10),"D=",D,"a=", a, "\n")
+
     sc.hc.pc.np.pp <-
         matrix(c(270.434164, 13.1763965268,-0.0000850, 0.000000039,
                  279.696678,  0.9856473354, 0.00002267,0.000000000,
@@ -12,7 +16,12 @@ tidem.astron <- function(t)
                  281.220844,  0.0000470684, 0.0000339, 0.000000070),
                nrow=5, ncol=4, byrow=TRUE)
     astro <- ((sc.hc.pc.np.pp %*% a) / 360) %% 1
-    rem <- as.numeric(difftime(t, trunc.POSIXt(t, units="days"), units="days"))
+    ttrunc <- as.POSIXct(trunc.POSIXt(t, units="days"),tz="GMT")
+    ##print(t-ttrunc)
+    ##cat("t orig :");print(t);     cat(unclass(t)[1],"\n")
+    ##cat("t trunc=");print(ttrunc);cat(unclass(ttrunc)[1],"\n")
+    rem <- as.numeric(t - ttrunc)
+    ##cat("rem=",rem,"\n")
     tau <- rem + astro[2,1] - astro[1,1]
     astro <- c(tau, astro)
     da <- matrix(c(0, 1, 2e-4*D, 3e-4*D^2), 4, 1)
