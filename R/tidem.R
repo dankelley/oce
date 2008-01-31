fe84bb4132d9d1df03db2306fa9b0efc539a61b6
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-31 20:24:32 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@372 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/tidem.R
+++ R/tidem.R
@@ -1,18 +1,13 @@
-tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
+tidem <- function(sl, constituents, latitude=NULL, start.time=NULL, rc=1, quiet = TRUE)
 {
     if (!inherits(sl, "sealevel")) stop("method is only for sealevel objects")
 
-    central.time.index <- floor(length(sl$data$t)/2)
+    if (is.null(start.time)) start.time <- sl$data$t[which(!is.na(sl$data$t))][1]
+cat("start.time=");    print(start.time);cat("\n")
 
-    ###
-    ##    load(paste(system.file(package="oce"),"data/tidesetup.rda",sep="/"), pos=globalenv())
-    ##    load(paste(system.file(package="oce"),"data/tidesetup.rda",sep="/"))
-    ###
     data("tidedata")
     td <- get("tidedata", pos=globalenv())
     tc <- td$const
-    ###
-
     ntc <- length(tc$name)
 
     if (!quiet) print(tc)
@@ -37,7 +32,6 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
                 freq <- tc$freq[standard][-1]
                 kmpr <- tc$kmpr[standard][-1]
                 indices <- c(indices, seq(1:ntc)[tc$standard])
-                ##cat("INDICES:", indices, "\n")
             }
             else {
                 if (substr(constituents[i], 1, 1) == "-") {
@@ -61,10 +55,6 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
     indices <- indices[order(indices)]
     tc2 <- list(name=tc$name[indices], freq=tc$freq[indices], kmpr=tc$kmpr[indices])
 
-    cat("L 62 length(indices)=",length(indices)," first few",indices[1], " ", indices[2], "\n")
-
-    ##print(data.frame(tc2))
-
     iZ0 <- which(tc2$name == "Z0")      # Remove Z0
     name <- tc2$name
     if (!quiet) print(name)
@@ -74,8 +64,6 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
     freq <- vector("numeric", nc)
     kmpr <- vector("numeric", nc)
 
-    cat("L 75 nc=",nc,"\n")
-
     for (i in 1:nc) {                   # Build up based on constituent names
         ic <- which(tc$name == name[i])
         if (!length(ic)) stop("there is no tidal constituent named \"", name[i], "\"")
@@ -83,13 +71,9 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
         freq[i] <- tc$freq[ic]
         kmpr[i] <- tc$kmpr[ic]
     }
-    ##cat("A:\n")
-    ##print(data.frame(name,freq,kmpr))
-
     nc <- length(freq)
                                         # Check Rayleigh criterion
     interval <- as.numeric(difftime(max(sl$data$t,na.rm=TRUE),min(sl$data$t,na.rm=TRUE),units="hours"))
-    ##cat("interval:",interval,"\n")
     drop.term <- NULL
     for (i in 1:nc) {
         cc <- which(tc2$name == kmpr[i])
@@ -99,8 +83,6 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
             if (cannot.fit)	drop.term <- c(drop.term, i)
         }
     }
-    ##cat("DROP:",drop.term,"\n")
-    cat("L101 index before:",index,"\n")
     if (length(drop.term) > 0) {
         if (!quiet) cat("Record is too short to fit for constituents:", name[drop.term],"\n")
         index <- index[-drop.term]
@@ -108,52 +90,17 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
         freq <- freq[-drop.term]
         kmpr <- kmpr[-drop.term]
     }
-    cat("L109 index after:",index,"\n")
-
-    ##cat("FITTING TO\n")
-    ##print(data.frame(name,freq))
 
     nc <- length(freq)
     nt <- length(sl$data$eta)
     x <- array(dim=c(nt, 2 * nc))
     x[,1] <- rep(1, nt)
-                                        # Offset time to GMT noon on first day of timeseries
-    ##delete    t <- sl$data$t
-    ##delete    hour2pi <- 2 * pi * as.numeric(sl$data$t) / 3600
-    #####first.day.noon <- as.POSIXlt(paste(substr(sl$data$t[1], 1, 10), "00:00:00"),tz="GMT")
-    #####hour.since.noon <- as.numeric(as.POSIXlt(sl$data$t, tz="GMT") - as.POSIXlt(first.day.noon, tz="GMT"))/3600
-    #####if (!quiet) {
-    #####cat("First point at time");print(as.POSIXlt(sl$data$t[1],  tz="GMT"))
-    #####cat("Noon on that day   ");print(as.POSIXlt(first.day.noon,tz="GMT"))
-    #####cat("hour.since.noon[1:12]:\n");print(hour.since.noon[1:12])
-    #####}
-
-    cat("L129 index after:",index,"(length=",length(index),")\n")
 
     hour <- unclass(as.POSIXct(sl$data$t, tz="GMT")) / 3600 # seconds since 0000-01-01 00:00:00
 
-    if (TRUE) {                         # Correct for fact that R puts t=0 at 1970
-        zero <- unclass(as.POSIXct("0000-01-01 00:00:00", tz="GMT")) / 3600
-        if (!quiet) {
-            cat("zero")
-            print(zero)
-            cat("first few hour:")
-            print(hour[1:3])
-        }
-        hour <- hour + zero
-        if (!quiet) {
-            cat("first few hour:")
-            print(hour[1:3])
-        }
-        ## stop()
-    }
-
-    #####hour2pi <- 2 * pi * hour.since.noon
-    hour2pi <- 2 * pi * (hour - hour[central.time.index]) # FIXME: should this be central time?
-
-    danhour <<- hour2pi/2/pi
-
-    cat("L147 nc=",nc,"\n")
+    centralindex <- floor(length(sl$data$t) / 2)
+    hour.wrt.centre <- unclass(hour - hour[centralindex])
+    hour2pi <- 2 * pi * hour.wrt.centre
 
     for (i in 1:nc) {
         omega.t <- freq[i] * hour2pi
@@ -175,39 +122,34 @@ tidem <- function(sl, constituents, latitude=NULL, rc=1, quiet = TRUE)
     for (i in seq(2,nc+1)) {
         is <- 2 * (i - 1)
         ic <- 2 * (i - 1) + 1
-        s <- coef[is]                   # cos(phase)
-        c <- coef[ic]                   # -sin(phase)
+        s <- coef[is]                   # coefficient on sin(t)
+        c <- coef[ic]                   # coefficient on cos(t)
         if (!quiet) cat(name[i-1], "gives s=",s,"and c=",c,"\n")
         amplitude[i] <- sqrt(s^2 + c^2)
                                         # sin(t - phase) == cos(phase)*sin(t) - sin(phase)*cos(t)
                                         #                == s * sin(t) + c * cos(t)
-        phase[i]     <- atan2(c, s) # atan2(y,x) ... made into degrees later
+                                        # thus tan(phase) is -c/s
+        phase[i]     <- atan2(-c, s)    # atan2(y,x)
+                                        # FIXME: is the sign right?
         p[i]         <- 0.5 * (p.all[is] + p.all[ic])
     }
     if (!quiet) cat("coef:", coef, "\n")
     phase <- phase * 180 / pi
 
-    centraltime <- as.POSIXct("1975-08-08", tz="GMT")
-    danindex <<-c(0,index)
-
+    centraltime <- as.POSIXct(sl$data$t[1] + 3600*centralindex, tz="GMT")
+    cat("centraltime=");print(centraltime);cat("\n")
     cat("L199 index:",index,"(length=",length(index),")\n")
 
     if (is.null(latitude)) latitude <- sl$metadata$latitude
-
-    vuf <<- tidem.vuf(centraltime, c(0, index), latitude) # FIXME: should be centraltime?
-
-    vu <<- c(0, (vuf$v + vuf$u) * 360)
+    vuf <- tidem.vuf(centraltime, c(0, index), latitude)
+    vu <- c(0, (vuf$v + vuf$u) * 360)
     phase2 <- phase - vu                # FIXME: plus or minus??
     negate <- phase2 < 0
     phase2[negate] <- 360 + phase2[negate]
-
     phase <- phase2
 
-    vuf48<<-tidem.vuf(centraltime, 48, latitude)
-
-    cat("vu at 48 with lat:", 360*(vuf48$u+vuf48$v)," (lat=",latitude,")\n")
-
     cat("vu=",vu,"\n")
+
     rval <- list(model=model,
                  const=c(1,   index),
                  name=c("Z0", name),
