7d1e43dd2b4369534a26009e1ff9debdc0df911f
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-01-31 21:07:09 +0000

    .
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@373 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/summary.tide.R
+++ R/summary.tide.R
@@ -11,17 +11,23 @@ summary.tide <- function(object, p=0, constituent, ...)
 
     if (missing(constituent)) {
         rval <- data.frame(Const=object$const[ok],
-                           Name=object$name[ok], Freq=object$freq[ok],
-                           Amplitude=object$amplitude[ok],	Phase=object$phase[ok],
-                           p=object$p[ok], sig=sig[ok])
+                           Name=object$name[ok],
+                           Freq=object$freq[ok],
+                           Amplitude=format(object$amplitude[ok], digits=3),
+                           Phase=format(object$phase[ok], digits=2),
+                           p=format(object$p[ok], digits=3),
+                           sig=sig[ok])
     }
     else {
         i <- which(object$name==constituent)
         if (length(i) == 0) stop("there is no such constituent '", constituent, "'")
         rval <- data.frame(Const=object$const[i],
-                           Name=object$name[i], Freq=object$freq[i],
-                           Amplitude=object$amplitude[i],	Phase=object$phase[i],
-                           p=object$p[i], sig=sig[i])
+                           Name=object$name[i],
+                           Freq=object$freq[i],
+                           Amplitude=format(object$amplitude[i], digits=3),
+                           Phase=format(object$phase[i], digits=2),
+                           p=format(object$p[i], digits=3),
+                           sig=sig[i])
     }
     class(rval) <- c("data.frame", "summary.tide")
     rval
