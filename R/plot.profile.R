c504e7198c03d2f88aedae416cc07c4c8df51dc7
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-02-24 22:27:18 +0000

    Fix bug preventing plot.ctd from obeying cex.axis (http://code.google.com/p/r-oce/issues/detail?id=13)
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@389 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.profile.R
+++ R/plot.profile.R
@@ -14,42 +14,39 @@ plot.profile <- function (x,
         plot(x$data$salinity, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
         mtext("Salinity [ PSU ]", side = 3, line = 3, col = col.S)
-        axis(2)
-        axis(3, col = col.S, col.axis = col.S, col.lab = col.S)
+        axis(2, ...)
+        axis(3, col = col.S, col.axis = col.S, col.lab = col.S, ...)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
         lines(x$data$salinity, x$data$pressure, col = col.S)
     } else if (type == "T") {
         plot(x$data$temperature, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
         mtext(expression(paste("Temperature [ ", degree, "C ]")),
               side = 3, line = 3, col = col.t)
-        axis(2)
-        axis(3, col = col.t, col.axis = col.t, col.lab = col.t)
+        axis(2, ...)
+        axis(3, col = col.t, col.axis = col.t, col.lab = col.t, ...)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
         lines(x$data$temperature, x$data$pressure, col = col.t)
     } else if (type == "density") {
         plot(x$data$sigma.theta, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
         mtext(expression(paste(sigma[theta], " [ ", kg/m^3, " ]")), side = 3,
               line = 3, col = col.rho)
-        axis(2)
-        axis(3, col = col.rho, col.axis = col.rho, col.lab = col.rho)
+        axis(2, ...)
+        axis(3, col = col.rho, col.axis = col.rho, col.lab = col.rho, ...)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
         lines(x$data$sigma.theta, x$data$pressure, col = col.rho)
     } else if (type == "density+N2") {
-		st <- sw.sigma.theta(x$data$salinity, x$data$temperature, x$data$pressure)
+	st <- sw.sigma.theta(x$data$salinity, x$data$temperature, x$data$pressure)
         plot(st, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
-        axis(3, col = col.rho, col.axis = col.rho, col.lab = col.rho)
+        axis(3, col = col.rho, col.axis = col.rho, col.lab = col.rho, ...)
         mtext(expression(paste(sigma[theta], " [ ", kg/m^3, " ]")), side = 3,
               line = 3, col = col.rho)
-        axis(2)
+        axis(2, ...)
         box()
         lines(st, x$data$pressure, col = col.rho)
         par(new = TRUE)
@@ -62,40 +59,37 @@ plot.profile <- function (x,
         mtext(expression(paste(N^2, " [ ", s^-2, " ]")), side = 1,
               line = 3, col = col.N2)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
     } else if (type == "N2") {
-	    N2 <- N2(x$data$pressure, x$data$sigma.theta, df = length(x$data$pressure)/4)
+        N2 <- N2(x$data$pressure, x$data$sigma.theta, df = length(x$data$pressure)/4)
         plot(N2, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
         mtext(expression(paste(N^2, " [ ", s^-2, " ]")), side = 3,
               line = 3, col = col.N2)
-        axis(2)
-        axis(3, col = col.N2, col.axis = col.N2, col.lab = col.N2)
+        axis(2, ...)
+        axis(3, col = col.N2, col.axis = col.N2, col.lab = col.N2, ...)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
         lines(N2, x$data$pressure, col = col.N2)
         abline(v = 0, col = col.N2)
     } else if (type == "S+T") {
         par(mfcol = c(2, 2))
         plot(x$data$temperature, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = pname, axes = FALSE)
-        axis(3, col = col.t, col.axis = col.t, col.lab = col.t)
+        axis(3, col = col.t, col.axis = col.t, col.lab = col.t, ...)
         mtext(expression(paste("Temperature [ ", degree, "C ]")),
               side = 3, line = 3, col = col.t)
-        axis(2)
+        axis(2, ...)
         box()
         lines(x$data$temperature, x$data$pressure, col = col.t)
         par(new = TRUE)
         plot(x$data$salinity, x$data$pressure, ylim = rev(range(x$data$pressure)),
              type = "n", xlab = "", ylab = "", axes = FALSE)
-        axis(1, col = col.S, col.axis = col.S, col.lab = col.S)
+        axis(1, col = col.S, col.axis = col.S, col.lab = col.S, ...)
         lines(x$data$salinity, x$data$pressure, col = col.S)
-        mtext("Salinity [ PSU ]", side = 1, line = 3, col = col.S)
+        mtext("Salinity [ PSU ]", side = 1, line = 3, col = col.S, ...)
         box()
-		if (grid)
-			grid(col=col.grid)
+        if (grid) grid(col=col.grid)
     } else {
         stop("unknown type, ", type, ", given")
     }
