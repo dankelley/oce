c065c1095b05d90fe127ee598a53016724af8fd5
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-04-06 14:27:14 +0000

    Add more options to plot.etopo2
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@415 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.etopo2.R
+++ R/plot.etopo2.R
@@ -1,37 +1,60 @@
-plot.etopo2 <- function(x, legend.loc="topright", ...)
+plot.etopo2 <- function(x, water.z, water.colors, land.z, land.colors, legend.loc="topright", ...)
 {
     if (!inherits(x, "etopo2")) stop("method is only for etop2 objects")
     zr <- range(x$data$z)
+    lat.range <- range(x$data$lat)
     contour(x$data$lon, x$data$lat, x$data$z, levels=0, drawlabels=FALSE,
             col="black",
-            asp=1/cos(mean(x$data$lat*pi/180)))
+            asp=1/cos(mean(lat.range)*pi/180), ...)
     w <- c()
     wc <- c()
     if (zr[1] < 0) {
-        w <- pretty(c(zr[1], 0))
-        w <- w[w!=0]
-        w <- rev(w)
-        wc <- gebco.colors(length(w), "water", "line")
+        if (missing(water.z)) {
+            if (zr[2] > 0) {
+                w <- pretty(c(zr[1], 0))
+                w <- w[w!=0]
+            } else {
+                w <- pretty(zr)
+            }
+            w <- rev(w)
+        } else {
+            w <- water.z
+        }
+        if (missing(water.colors))
+            wc <- gebco.colors(length(w), "water", "line")
+        else
+            wc <- water.colors
         contour(x$data$lon, x$data$lat, x$data$z, levels=w,
                 col=wc,
                 drawlabels=FALSE,
-                add=TRUE)
+                add=TRUE, ...)
     }
     l <- c()
     lc <- c()
     if (zr[2] > 0) {
-        l <- pretty(c(0, zr[2]))
-        l <- l[l!=0]
-        lc <- gebco.colors(length(l), "land", "line")
+        if (missing(land.z)) {
+            if (zr[1] < 0) {
+                l <- pretty(c(0, zr[2]))
+                l <- l[l!=0]
+            } else {
+                l <- pretty(zr)
+            }
+        } else {
+            l <- land.z
+        }
+        if (missing(land.colors))
+            lc <- gebco.colors(length(l), "land", "line")
+        else
+            lc <- land.colors
         contour(x$data$lon, x$data$lat, x$data$z, levels=l,
                 col=lc,
                 drawlabels=FALSE,
-                add=TRUE)
+                add=TRUE, ...)
     }
     if (!is.null(legend.loc)) {
         nl <- length(w) + length(l)
         legend(legend.loc, lwd=par("lwd"),
                legend=c(rev(l),0, w),
-               col=c(rev(lc), "black", wc))
+               col=c(rev(lc), "black", wc), ...)
     }
 }
