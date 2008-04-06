6dac8c1e0f3495163c187b6071dd6bf889edb849
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-04-06 00:49:20 +0000

    Improve plot.etopo2 by auto-scaling for land and water separately.
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@412 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.etopo2.R
+++ R/plot.etopo2.R
@@ -1,6 +1,32 @@
-plot.etopo2 <- function(x, ...)
+plot.etopo2 <- function(x, legend.loc="topright", ...)
 {
     if (!inherits(x, "etopo2")) stop("method is only for etop2 objects")
+    zr <- range(x$data$z)
     contour(x$data$lon, x$data$lat, x$data$z, levels=0, drawlabels=FALSE,
             asp=1/cos(mean(x$data$lat*pi/180)))
+    w <- c()
+    wc <- c()
+    if (zr[1] < 0) {
+        w <- pretty(c(zr[1], 0))
+        w <- rev(w[1:(length(w)-1)])
+        wc <- gebco.colors(length(w), "water", "line")
+        contour(x$data$lon, x$data$lat, x$data$z, levels=w,
+                col=wc,
+                drawlabels=FALSE,
+                add=TRUE)
+    }
+    l <- c()
+    lc <- c()
+    if (zr[2] > 0) {
+        l <- pretty(c(0, zr[2]))
+        lc <- gebco.colors(length(w), "land", "line")
+        contour(x$data$lon, x$data$lat, x$data$z, levels=l,
+                col=lc,
+                drawlabels=FALSE,
+                add=TRUE)
+    }
+    if (!is.null(legend.loc)) {
+        nl <- length(w) + length(l)
+        legend(legend.loc, lwd=par("lwd"),legend=c(rev(l), w), col=c(rev(lc), wc))
+    }
 }
