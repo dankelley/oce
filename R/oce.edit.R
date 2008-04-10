fb460a81e63b3b721b867b20790ee9d5a2d50456
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-04-10 10:05:49 +0000

    Clean code
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@444 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/oce.edit.R
+++ R/oce.edit.R
@@ -1,12 +1,9 @@
 oce.edit <- function(x, item, value, reason="not specified", person="not specified")
 {
-    if (!inherits(x, "oce"))
-    	stop("method is only for oce objects")
-    if (missing(item)) stop("must supply an 'item' to modify")
-    if (missing(value)) stop("must supply a new 'value' to for item '", item, "'")
-    allowed.items <- names(x$metadata)
-    if (!any(item == allowed.items)) stop("There is no item named '", item, "' in the metadata for this oce object")
-    ## OK, now do it
+    if (!inherits(x, "oce")) stop("method is only for oce objects")
+    if (missing(item))       stop("must supply an 'item' to modify")
+    if (missing(value))      stop("must supply a new 'value' to for item '", item, "'")
+    if (!any(item == names(x$metadata))) stop("There is no item named '", item, "' in the metadata for this oce object")
     res <- x
     res$metadata[item] <- value
     log.item <- paste("modified by oce.edit(x, item=\"", item, "\", value=\"", value, "\", reason=\"", reason, "\", person=\"", person, "\")",sep="")
