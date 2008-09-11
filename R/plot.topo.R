9931a091b71d80a60c573a8027cb72915091c61d
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-09-11 22:52:50 +0000

    working on new line-property features of plot.topo(), but still broken
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@481 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.topo.R
+++ R/plot.topo.R
@@ -1,5 +1,8 @@
-plot.topo <- function(x, water.z, water.colors, land.z, land.colors, legend.loc="topright",
-                      lwd=par("lwd"), ...)
+plot.topo <- function(x,
+                      water.z, water.col, water.lty, water.lwd,
+                      land.z,  land.col,   land.lty,  land.lwd,
+                      legend.loc="topright",
+                      ...)
 {
     if (!inherits(x, "topo")) stop("method is only for topo objects")
     lat.range <- range(x$data$lat)
@@ -9,58 +12,61 @@ plot.topo <- function(x, water.z, water.colors, land.z, land.colors, legend.loc=
     plot(range(x$data$lon), range(x$data$lat), asp=asp, xaxs="i", yaxs="i",
          type="n", xlab="", ylab="", ...)
 
-    contour(x$data$lon, x$data$lat, x$data$z, levels=0, drawlabels=FALSE, add=TRUE,
-            col="black", lwd=lwd)
-    w <- c()
-    wc <- c()
+    contour(x$data$lon, x$data$lat, x$data$z,
+            levels=0, drawlabels=FALSE, add=TRUE,
+            col="black")                # coastline is always black
+    if (missing(water.lty)) {
+        water.lty <- par("lty")
+    }
+    if (missing(water.lwd)) {
+        water.lwd <- par("lwd")
+    }
+    if (missing(land.lty)) {
+        land.lty <- par("lty")
+    }
+    if (missing(land.lwd)) {
+        land.lwd <- par("lwd")
+    }
     if (zr[1] < 0) {
         if (missing(water.z)) {
             if (zr[2] > 0) {
-                w <- pretty(c(zr[1], 0))
-                w <- w[w!=0]
+                water.z <- pretty(c(zr[1], 0))
+                water.z <- water.z[water.z!=0]
             } else {
-                w <- pretty(zr)
+                water.z <- pretty(zr)
             }
-            w <- rev(w)
-        } else {
-            w <- water.z
+            water.z <- rev(sort(water.z))
         }
-        if (missing(water.colors))
-            wc <- gebco.colors(length(w), "water", "line")
-        else
-            wc <- water.colors
-        contour(x$data$lon, x$data$lat, x$data$z, levels=w,
-                col=wc,
+        if (missing(water.col))
+            water.col <- gebco.colors(length(water.z), "water", "line")
+        contour(x$data$lon, x$data$lat, x$data$z,
+                levels=water.z, lwd=water.lwd, lty=water.lty, col=water.col,
                 drawlabels=FALSE,
                 add=TRUE, ...)
     }
-    l <- c()
-    lc <- c()
     if (zr[2] > 0) {
         if (missing(land.z)) {
             if (zr[1] < 0) {
-                l <- pretty(c(0, zr[2]))
-                l <- l[l!=0]
+                land.z <- pretty(c(0, zr[2]))
+                land.z <- land.z[land.z!=0]
             } else {
-                l <- pretty(zr)
+                land.z <- pretty(zr)
             }
-        } else {
-            l <- land.z
         }
-        if (missing(land.colors))
-            lc <- gebco.colors(length(l), "land", "line")
-        else
-            lc <- land.colors
-        contour(x$data$lon, x$data$lat, x$data$z, levels=l,
-                lwd=lwd,
-                col=lc,
+        if (missing(land.col))
+            land.col <- gebco.colors(length(land.z), "land", "line")
+        contour(x$data$lon, x$data$lat, x$data$z,
+                levels=land.z, lwd=land.lwd, lty=land.lty, col=land.col,
                 drawlabels=FALSE,
                 add=TRUE, ...)
     }
     if (!is.null(legend.loc)) {
-        nl <- length(w) + length(l)
-        legend(legend.loc, lwd=lwd, bg="white",
-               legend=c(rev(l),0, w),
-               col=c(rev(lc), "black", wc))
+        nl <- length(land.z) + length(water.z)
+        legend(legend.loc,
+               lwd=c(land.lwd, water.lwd),
+               lty=c(land.lty, water.lty),
+               bg="white",
+               legend=c(rev(land.z),0, water.z),
+               col=c(land.col, "black", water.col))
     }
 }
