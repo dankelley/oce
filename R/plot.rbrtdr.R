288ff5114a9e41dea2d8210f917cf34b0df62128
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-10-27 22:17:58 +0000

    first stab at rbr.trim()
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@499 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -3,8 +3,10 @@ plot.rbrtdr <- function (x, ...)
     if (!inherits(x, "rbrtdr")) stop("method is only for rbrtdr objects")
     oldpar <- par(no.readonly = TRUE)
     par(mfrow=c(2,1))
-    plot(x$data$t, x$data$temperature, xlab="Time", ylab="T [degC]", type='l')
-    plot(x$data$t, x$data$pressure,    xlab="Time", ylab="p [?]", type='l')
+    plot(x$data$t, x$data$temperature,
+         xlab="Time", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l')
+    plot(x$data$t, x$data$pressure,
+         xlab="Time", ylab="p [dbar]", type='l')
     par(oldpar)
     invisible()
 }
