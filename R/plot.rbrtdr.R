f0f33168735d220fadc26fb3f4b3d6ce9a3e2cff
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-10-27 23:36:49 +0000

    tighter margins on plot
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@503 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -3,6 +3,7 @@ plot.rbrtdr <- function (x, ...)
     if (!inherits(x, "rbrtdr")) stop("method is only for rbrtdr objects")
     oldpar <- par(no.readonly = TRUE)
     par(mfrow=c(2,1))
+    par(mar=c(4.5, 4.5, 1, 1))
     plot(x$data$t, x$data$temperature,
          xlab="Time", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l')
     plot(x$data$t, x$data$pressure,
