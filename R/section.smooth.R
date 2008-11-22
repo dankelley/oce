6d98189dcc287a5f557c78c219be5a23e177fd60
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-11-22 13:02:07 +0000

    fix bug
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@533 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/section.smooth.R
+++ R/section.smooth.R
@@ -1,20 +1,21 @@
 ## bugs: should ensure that every station has identical pressures
 ## FIXME: should have smoothing in the vertical also ... and is spline what I want??
-section.smooth <- function(s, ...)
+section.smooth <- function(section, ...)
 {
-    nstn <- length(s$data$station)
-    nprs <- length(s$data$station[[1]]$data$pressure)
+    if (!inherits(section, "section")) stop("method is only for section objects")
+    nstn <- length(section$data$station)
+    nprs <- length(section$data$station[[1]]$data$pressure)
     supplied.df <- "df" %in% names(list(...))
     if (!supplied.df) df <- nstn / 5
-    rval <- s
-    x <- geod.dist(s)
+    rval <- section
+    x <- geod.dist(section)
     temperature.mat <- array(dim=c(nprs, nstn))
     salinity.mat <- array(dim=c(nprs, nstn))
     sigma.theta.mat <- array(dim=c(nprs, nstn))
     for (s in 1:nstn) {
-        temperature.mat[,s] <- s$data$station[[s]]$data$temperature
-        salinity.mat[,s] <- s$data$station[[s]]$data$salinity
-        sigma.theta.mat[,s] <- s$data$station[[s]]$data$sigma.theta
+        temperature.mat[,s] <- section$data$station[[s]]$data$temperature
+        salinity.mat[,s] <- section$data$station[[s]]$data$salinity
+        sigma.theta.mat[,s] <- section$data$station[[s]]$data$sigma.theta
     }
     for (p in 1:nprs) {
         ok <- !is.na(temperature.mat[p,])
