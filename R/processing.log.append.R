3b59e91267df3e2ec311b855af6aa0034ba3b668
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-11-28 12:54:55 +0000

    Fixed issue 33 ... but there is a new issue and so some debugging lines
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@538 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/processing.log.append.R
+++ R/processing.log.append.R
@@ -1,6 +1,8 @@
 processing.log.append <- function (x, action="")
 {
-    x$processing.log$time <- c(x$processing.log$time, as.POSIXlt(Sys.time(),tz="GMT"))
+    ## FIXME was POSIXlt but problem with e.g. summary(section$data$station[[1])
+    ## for something made as in OCE3001-asst2-2008
+    x$processing.log$time <- c(x$processing.log$time, as.POSIXct(Sys.time(),tz="GMT"))
     x$processing.log$action <- c(x$processing.log$action, action)
     x
 }
--- R/sw.dynamic.height.R
+++ R/sw.dynamic.height.R
@@ -2,11 +2,14 @@ sw.dynamic.height <- function(x, pref=2000)
 {
     height <- function(ctd, pref)
     {
+        if (sum(!is.na(ctd$data$pressure)) < 2) return(NA) # cannot integrate then
         g <- if (is.na(ctd$metadata$latitude)) 9.8 else gravity(ctd$metadata$latitude)
-        if (pref > max(ctd$data$pressure, na.rm=TRUE)) return(NA)
         np <- length(ctd$data$pressure)
-                                        # 1e4 dbar per pascal
-        dzdp <- ((1/sw.rho(ctd) - 1/sw.rho(rep(35,np),rep(0,np),ctd$data$pressure))/g)*1e4
+        rho <- sw.rho(ctd)
+        if (sum(!is.na(rho)) < 2) return(NA)
+        ## 1e4 converts decibar to Pa
+        dzdp <- ((1/rho - 1/sw.rho(rep(35,np),rep(0,np),ctd$data$pressure))/g)*1e4
+        print(summary(ctd))
         integrand <- approxfun(ctd$data$pressure, dzdp, rule=2)
         integrate(integrand, 0, pref)$value
     }
@@ -17,6 +20,7 @@ sw.dynamic.height <- function(x, pref=2000)
         d <- vector("numeric", ns)
         h <- vector("numeric", ns)
         for (i in 1:ns) {
+            cat("i=",i,"\n")
             d[i] <- geod.dist(x$data$station[[i]]$metadata$latitude, x$data$station[[i]]$metadata$longitude, lat0, lon0)
             h[i] <- height(x$data$station[[i]], pref)
         }
