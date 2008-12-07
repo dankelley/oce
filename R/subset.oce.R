4b51251445e2e15936a7dee327219f51dbefcd29
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-07 16:20:32 +0000

    add log item
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@567 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/subset.oce.R
+++ R/subset.oce.R
@@ -4,6 +4,7 @@ subset.oce <- function (x, subset,...)
     r <- r & !is.na(r)
     rval <- x
     rval$data <- x$data[r,]
+    rval <- processing.log.append(rval, paste("modified by subset.oce(x, ", deparse(substitute(subset)), ")", sep=""))
     class(rval) <- class(x)
     rval
 }
