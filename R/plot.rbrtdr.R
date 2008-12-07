088644f7efd0453413aff8e23f86b89c49a41148
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-07 15:14:55 +0000

    Give control to panels in plotrbrtdr()
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@563 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -1,13 +1,29 @@
-plot.rbrtdr <- function (x, ...)
+plot.rbrtdr <- function (x, which=1:3, ...)
 {
     if (!inherits(x, "rbrtdr")) stop("method is only for rbrtdr objects")
-    oldpar <- par(no.readonly = TRUE)
-    par(mfrow=c(2,1))
-    par(mar=c(4.5, 4.5, 1, 1))
-    plot(x$data$t, x$data$temperature,
-         xlab="Time", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l')
-    plot(x$data$t, x$data$pressure,
-         xlab="Time", ylab="p [dbar]", type='l')
-    par(oldpar)
+    show <- rep(FALSE, 3)
+    show[which] <- TRUE
+    lw <- length(which)
+    if (lw == 2) {
+        oldpar <- par(no.readonly = TRUE)
+        par(mfrow=c(2,1))
+    } else if (lw==3) {
+        oldpar <- par(no.readonly = TRUE)
+        par(mfrow=c(2,2))
+    }
+    if (show[1]) {
+        plot(x$data$t, x$data$temperature,
+             xlab="Time", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l')
+    }
+    if (show[2]) {
+        plot(x$data$t, x$data$pressure,
+             xlab="Time", ylab="p [dbar]", type='l')
+    }
+    if (show[3]) {
+        plot(x$data$temperature, x$data$pressure,
+             xlab=expression(paste("Temperature [", degree, "C ]")),
+             ylab="p [dbar]", type='p')
+    }
+    if (lw != 1) par(oldpar)
     invisible()
 }
