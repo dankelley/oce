b7309511554c9f1521f3df75263fc8dd6e33f5bb
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-13 18:36:50 +0000

    fix typo
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@599 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/as.windrose.R
+++ R/as.windrose.R
@@ -21,7 +21,7 @@ as.windrose <- function(x, y, dtheta = 15)
     }
     data <- list(n=length(x), x.mean=mean(x, na.rm=TRUE), y.mean=mean(y, na.rm=TRUE), theta=theta*180/pi, count=count, mean=mean, fivenum=fivenum)
     metadata <- list(dtheta=dtheta)
-    log.item <- processing.log.item(paste(deparse(match.call(), sep="", collapse="")))
+    log.item <- processing.log.item(paste(deparse(match.call()), sep="", collapse=""))
     res <- list(data=data, metadata=metadata, log=log.item)
     class(res) <- c("windrose", "oce")
     res
