6cbb0d1bd064036dee10a30a2174a3158ff7747b
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-14 15:55:39 +0000

    flesh out rbr tdr example
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@612 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -6,23 +6,34 @@ plot.rbrtdr <- function (x, which=1:3, ...)
     lw <- length(which)
     if (lw == 2) {
         oldpar <- par(no.readonly = TRUE)
-        par(mfrow=c(2,1))
+        par(mfcol=c(2,1))
+        par(mar=c(3,3,1,1))
+        if (!"mgp" %in% names(list(...))) par(mgp = c(2, 2/3, 0))
     } else if (lw==3) {
         oldpar <- par(no.readonly = TRUE)
-        par(mfrow=c(2,2))
+        par(mfcol=c(2,2))
+        par(mar=c(3,3,1,1))
+        if (!"mgp" %in% names(list(...))) par(mgp = c(2, 2/3, 0))
     }
     if (show[1]) {
         plot(x$data$t, x$data$temperature,
-             xlab="Time", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l', ...)
+             xlab="", ylab=expression(paste("Temperature [ ", degree, "C ]")), type='l', ...)
     }
     if (show[2]) {
         plot(x$data$t, x$data$pressure,
-             xlab="Time", ylab="p [dbar]", type='l', ...)
+             xlab="", ylab="p [dbar]", type='l', ...)
     }
+
     if (show[3]) {
-        plot(x$data$temperature, x$data$pressure,
-             xlab=expression(paste("Temperature [", degree, "C ]")),
-             ylab="p [dbar]", type='p', ...)
+        if ("cex" %in% names(list(...))) {
+            plot(x$data$temperature, x$data$pressure,
+                 xlab=expression(paste("Temperature [", degree, "C ]")),
+                 ylab="p [dbar]", type='p', ...)
+        } else {
+            plot(x$data$temperature, x$data$pressure,
+                 xlab=expression(paste("Temperature [", degree, "C ]")),
+                 ylab="p [dbar]", type='p', cex=0.3, ...)
+        }
     }
     if (lw != 1) par(oldpar)
     invisible()
