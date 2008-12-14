652f1e9b921cdc2f5cb39e207264ebb13ee79578
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-14 18:43:37 +0000

    adjusting aesthetics of plot.rbrtdr
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@613 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -3,16 +3,17 @@ plot.rbrtdr <- function (x, which=1:3, ...)
     if (!inherits(x, "rbrtdr")) stop("method is only for rbrtdr objects")
     show <- rep(FALSE, 3)
     show[which] <- TRUE
+    oldpar <- par(no.readonly = TRUE)
     lw <- length(which)
-    if (lw == 2) {
-        oldpar <- par(no.readonly = TRUE)
+    if (!"mgp" %in% names(list(...))) par(mgp = c(2, 2/3, 0))
+    if (lw == 1) {
+        ;
+    } else if (lw == 2) {
+        par(mar=c(3,3,1,1))
         par(mfcol=c(2,1))
-        par(mar=c(3,3,1,1))
-        if (!"mgp" %in% names(list(...))) par(mgp = c(2, 2/3, 0))
     } else if (lw==3) {
-        oldpar <- par(no.readonly = TRUE)
-        par(mfcol=c(2,2))
         par(mar=c(3,3,1,1))
+        par(mfcol=c(2,2))
         if (!"mgp" %in% names(list(...))) par(mgp = c(2, 2/3, 0))
     }
     if (show[1]) {
