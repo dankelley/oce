c5690a04c18471b645c8a13ec1e36b38ffdbb4d6
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-15 20:30:02 +0000

    reverse the pressure axis for plot.rbrtdr()
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@620 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/plot.rbrtdr.R
+++ R/plot.rbrtdr.R
@@ -22,16 +22,20 @@ plot.rbrtdr <- function (x, which=1:4, ...)
     }
     if (show[2]) {
         plot(x$data$t, x$data$pressure,
-             xlab="", ylab="p [dbar]", type='l', ...)
+             xlab="", ylab="p [dbar]", type='l', 
+             ylim=rev(range(x$data$pressure)),
+             ...)
     }
 
     if (show[3]) {
         if ("cex" %in% names(list(...))) {
             plot(x$data$temperature, x$data$pressure,
+                 xlim=rev(range(x$data$pressure)),
                  xlab=expression(paste("Temperature [", degree, "C ]")),
                  ylab="p [dbar]", type='p', ...)
         } else {
             plot(x$data$temperature, x$data$pressure,
+                 xlim=rev(range(x$data$pressure)),
                  xlab=expression(paste("Temperature [", degree, "C ]")),
                  ylab="p [dbar]", type='p', cex=0.3, ...)
         }
