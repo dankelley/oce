48a29d63cff1d5f3d3eb7c66f9d376b4ac8366c5
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2008-12-18 12:41:52 +0000

    rewrite code in more familiar style
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@640 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/oce.edit.R
+++ R/oce.edit.R
@@ -3,7 +3,7 @@ oce.edit <- function(x, item, value, reason="not specified", person="not specifi
     if (!inherits(x, "oce")) stop("method is only for oce objects")
     if (missing(item))       stop("must supply an 'item' to modify")
     if (missing(value))      stop("must supply a new 'value' to for item '", item, "'")
-    if (!any(item == names(x$metadata))) stop("There is no item named '", item, "' in the metadata for this oce object")
+    if (!(item %in% names(x$metadata))) stop("There is no item named '", item, "' in the metadata for this oce object")
     res <- x
     res$metadata[item] <- value
     res <- processing.log.append(res, paste(deparse(match.call()), sep="", collapse=""))
