e9ac868c66d557cb855071cbd9cd840c262891b4
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-02-16 18:20:08 +0000

    make ctd.trim() report a suggested scan range, for manual work
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@705 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/ctd.trim.R
+++ R/ctd.trim.R
@@ -7,10 +7,10 @@ ctd.trim <- function(x, method="downcast", parameters, verbose=FALSE)
         warning("too few data to ctd.trim()")
     } else {
         which.method <- pmatch(method, c("index", "downcast"), nomatch=0)
-        if (verbose) cat("ctd.trim()\n  using method", which.method,"\n")
+        ##if (verbose) cat("ctd.trim()\n  using method", which.method,"\n")
         keep <- rep(TRUE, n)
         if (which.method == 1) {        # "index"
-            if (verbose)	cat("  parameters:",parameters,"\n");
+            ##if (verbose)	cat("  parameters:",parameters,"\n");
             if (min(parameters) < 1)
                 stop("Cannot select indices < 1");
             if (max(parameters) > n)
@@ -31,18 +31,16 @@ ctd.trim <- function(x, method="downcast", parameters, verbose=FALSE)
                                         # 3. trim the upcast and anything thereafter (ignore beginning and end)
             trim.top <- as.integer(0.1*n)
             trim.bottom <- as.integer(0.9*n)
-#            max.spot <- which.max(smooth(x$data$pressure[trim.top:trim.bottom],kind="3R"))
-
-            max.spot <- which.max(smooth(x$data$pressure[1:n],kind="3R")) # trim.top:trim.bottom])
-
-            max.location <- 0* trim.top + max.spot
+            max.spot <- which.max(smooth(x$data$pressure[trim.top:trim.bottom],kind="3R"))
+            max.location <- trim.top + max.spot
             keep[max.location:n] <- FALSE
-            if (verbose) cat("  pressure maximum at index=",max.spot,"\n")
+            ##if (verbose) cat("  pressure maximum at index=",max.spot,"\n")
             if (FALSE) {
                                         # deleted method: slowly-falling data
                 delta.p.sorted <- sort(delta.p)
                 if (!is.null(parameters)) {
-                    dp.cutoff <- t.test(delta.p[keep], conf.level=parameters[1])$conf.int[1]
+                    dp.cutoff <- t.test(delta.p[keep], conf.level=0.5)$conf.int[1]
+                    print(t.test(delta.p[keep], conf.level=0.05))#$conf.int[1]
                 } else {
                     dp.cutoff <- delta.p.sorted[0.1*n]
                 }
@@ -56,37 +54,31 @@ ctd.trim <- function(x, method="downcast", parameters, verbose=FALSE)
                 keep[equilibration] <- FALSE
             }
             if (TRUE) {                 # new method, after Feb 2008
-                bilinear1 <- function(s, s0, p0, dpds) { ifelse(s < s0, p0, dpds*(s-s0)) }
-                bilinear2 <- function(s, s1, p1, dpds) { cat("s1=",s1,"p1=",p1,"dpds=",dpds,"\n");ifelse(s > s1, p1, dpds*(s-min(s))) }
+                bilinear1 <- function(s, s0, dpds) {
+                    ifelse(s < s0, 0, dpds*(s-s0))
+                }
                 pp <- x$data$pressure[keep]
                 ss <- x$data$scan[keep]
+                ##if (verbose) plot(ss,pp,ylim=rev(range(pp)))
                 p0 <- 0
                 s0 <- ss[0.25*length(ss)]
-                s1 <- ss[0.95*length(ss)]
                 p0 <- pp[1]
                 p1 <- max(pp) #pp[0.9*length(pp)]
-                plot(ss, pp);abline(v=c(s0,s1))
                 dpds0 <-  diff(range(pp)) / diff(range(ss))
-                t <- try(m <- nls(pp ~ bilinear1(ss, s0, p0, dpds),
-                                  start=list(s0=s0, p0=p0, dpds=dpds0)),
+                t <- try(m <- nls(pp ~ bilinear1(ss, s0, dpds),
+                                  start=list(s0=s0, dpds=dpds0)),
                          silent=TRUE)
                 if (class(t) != "try-error") {
                     if (m$convInfo$isConv) {
                         s0 <- floor(coef(m)[[1]])
-                        if (verbose) cat("  trimming scan numbers below", s0, "\n")
-                        if (verbose) print(summary(m))
+                        ##if (verbose) cat("  trimming scan numbers below", s0, "\n")
+                        ##if (verbose) print(summary(m))
                         keep <- keep & (x$data$scan > (coef(m)[[1]]))
-##                        nn <- length(ss)
-##                        cat("s1=",ss[nn/2],"\n")
-##                        cat('length(ss)=',length(ss),'\n')
-##                        t <- try(m2 <- nls(pp ~ bilinear2(ss, s1, p1, dpds),
-##                                           start=list(s1=ss[nn], p1=pp[nn], dpds=coef(m)[[3]]),
-##                                           algorithm="port", lower=c(ss[3], 0, 0), upper=c(ss[nn], pp[nn], 1)),
-##                                 silent=FALSE)
                     }
                 } else {
                     warning("unable to complete step 5 of the trim operation (removal of initial equilibrium phase)")
                 }
+                if (verbose) cat("ctd.trim(read.oce(\"", x$metadata$filename, "\"), \"scan\", c(", min(x$data$scan[keep],na.rm=TRUE), ",", max(x$data$scan[keep],na.rm=TRUE),"))\n",sep="")
             }
         } else {
             if (verbose)	cat(paste("column",method,"; parameters ", parameters[1], parameters[2]))
