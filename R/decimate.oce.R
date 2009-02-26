a6cbb290bdf9b42c78f67f420b0e886fbebbddf7
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-02-26 23:53:14 +0000

    add decimate()
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@711 3dc979fe-ab3f-0410-b117-8d365baceae5

---
+++ R/decimate.oce.R
@@ -0,0 +1,12 @@
+decimate <- function(x, by=10, method=c("direct"))
+{
+    if (!inherits(x, "oce")) stop("method is only for oce objects")
+    method <- match.arg(method)
+    res <- x
+    if (method == "direct") {
+        i <- seq(1, dim(x$data)[1], by=by)
+        print(i)
+        res$data <- x$data[i,]
+        processing.log.append(res, paste(deparse(match.call()), sep="", collapse=""))
+    }                                   # other methods cause error
+}
