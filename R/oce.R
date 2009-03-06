3ccf6ece58fe99807b2ab2e3960b86d735937b40
dan.kelley@dal.ca <dan.kelley@dal.ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-03-06 23:05:09 +0000

    refactor subset.oce summary.oce
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@751 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/oce.R
+++ R/oce.R
@@ -66,3 +66,52 @@ oce.write.table <- function (x, file="", ...)
     if (!("row.names" %in% names(list(...)))) write.table(x$data, file, row.names=FALSE, ...)
     else write.table(x$data, file, ...)
 }
+subset.oce <- function (x, subset, indices=NULL, ...)
+{
+    if (!inherits(x, "oce")) stop("method is only for oce objects")
+    if (inherits(x, "section")) {
+        if (!is.null(indices)) {        # select a portion of the stations
+            n <- length(indices)
+            station <- vector("list", n)
+            stn <- vector("character", n)
+            lon <- vector("numeric", n)
+            lat <- vector("numeric", n)
+            for (i in 1:n) {
+                ii <- indices[i]
+                stn[i] <- x$metadata$station.id[ii]
+                lat[i] <- x$metadata$latitude[ii]
+                lon[i] <- x$metadata$longitude[ii]
+                station[[i]] <- x$data$station[[ii]]
+            }
+            data <- list(station=station)
+            metadata <- list(header=x$header,section.id=x$section.id,station.id=stn,latitude=lat,longitude=lon)
+            rval <- list(data=data, metadata=metadata, processing.log=x$processing.log)
+            class(rval) <- c("section", "oce")
+        } else {                        # subset within the stations
+            rval <- x
+            n <- length(x$data$station)
+            r <- eval(substitute(subset), x$data$station[[1]]$data, parent.frame())
+            for (i in 1:n) {
+                rval$data$station[[i]]$data <- x$data$station[[i]]$data[r,]
+            }
+        }
+        rval <- processing.log.append(rval, paste(deparse(match.call()), sep="", collapse=""))
+    } else {
+        r <- eval(substitute(subset), x$data, parent.frame())
+        r <- r & !is.na(r)
+        rval <- x
+        rval$data <- x$data[r,]
+        rval <- processing.log.append(rval, paste(deparse(match.call()), sep="", collapse=""))
+    }
+    rval
+}
+summary.oce <- function(object, ...)
+{
+    if (!inherits(object, "oce")) stop("method is only for oce objects")
+    cat("Data summary:\n")
+    print(summary(object$data))
+    cat("\nMetadata:\n")
+    print(object$metadata)
+    processing.log.summary(object)
+    return(invisible(object))
+}
