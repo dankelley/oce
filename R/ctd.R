c40b30e8d1e87e49275608b38c1b8c4a696c7f46
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-04-16 15:02:11 +0000

    clarify that ctd.write creates csv files
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@871 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/ctd.R
+++ R/ctd.R
@@ -305,16 +305,15 @@ ctd.update.header <- function (x, debug = FALSE)
 
 ctd.write <- function(object, file=stop("'file' must be specified"))
 {
-  	if (!inherits(object, "ctd")) stop("method is only for ctd objects")
-	if (is.character(file)) {
-		if (file == "") stop("'file' must be a non-empty string")
-		con <- file(file, "w")
-	} else if (inherits(file, "connection")) {
-		con <- file
+    if (!inherits(object, "ctd")) stop("method is only for ctd objects")
+    if (is.character(file)) {
+        if (file == "") stop("'file' must be a non-empty string")
+        con <- file(file, "w")
+    } else if (inherits(file, "connection")) {
+        con <- file
     }
-	cat(object$header, sep="\n", file=con) # bug: ranges are wrong
-	write.table(object$data, col.names=FALSE,row.names=FALSE, file=con)
-	close(con)
+    write.table(object$data, col.names=TRUE, row.names=FALSE, sep=",", file=con)
+    close(con)
 }
 
 plot.ctd <- function (x, ref.lat = NaN, ref.lon = NaN,
