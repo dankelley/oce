df37a18ab4c66932f3e1295a5ca0a6381c0d20df
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-05-05 12:40:20 +0000

    add default value for latitude argument of gravity()
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@911 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/misc.R
+++ R/misc.R
@@ -163,10 +163,10 @@ GMT.offset.from.tz <- function(tz)
     if (tz == "Z"  )	return(  0  ) # Zulu Time Zone	Military	UTC
 }
 
-gravity <- function(lat, degrees=TRUE)
+gravity <- function(latitude=45, degrees=TRUE)
 {
-    if (degrees) lat <- lat * 0.0174532925199433
-    9.780318*(1.0+5.3024e-3*sin(lat)^2-5.9e-6*sin(2*lat)^2)
+    if (degrees) latitude <- latitude * 0.0174532925199433
+    9.780318*(1.0+5.3024e-3*sin(latitude)^2-5.9e-6*sin(2*latitude)^2)
 }
 
 make.filter <- function(type=c("blackman-harris"), m)
