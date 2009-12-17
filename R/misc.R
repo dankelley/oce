9131b596755917590664b50f4827584db51d9764
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2009-12-17 18:58:17 +0000

    order x for oce.approx
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@1180 3dc979fe-ab3f-0410-b117-8d365baceae5

--- R/misc.R
+++ R/misc.R
@@ -7,11 +7,12 @@ oce.approx <- function(x, y, xout, n=1, m=1) # FIXME: make n and m match ref
     if (lx != ly) stop("length of x (", lx, ") and y (", ly, ") must agree")
     if (any(is.na(x))) stop("must not have any NA values in x")
     if (any(is.na(y))) stop("must not have any NA values in y")
+    o <- order(x)
     if (missing(xout))
         xout <- seq(min(x), max(x), length.out=lx)
     else
         if (any(is.na(xout))) stop("must not have any NA values in xout")
-    .Call("oce_approx", x=x, y=y, xout=xout, n=n, m=m);
+    .Call("oce_approx", x=x[o], y=y[o], xout=xout, n=n, m=m);
 }
 
 matrix.smooth <- function(m)
