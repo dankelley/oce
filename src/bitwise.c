42a4dd01cbb483511c62c5bc9d27ae979f7277c2
Dan.Kelley@Dal.Ca <Dan.Kelley@Dal.Ca@3dc979fe-ab3f-0410-b117-8d365baceae5>
2010-03-18 22:31:55 +0000

    working on checksums for nortek adv
    git-svn-id: https://r-oce.googlecode.com/svn/trunk@1226 3dc979fe-ab3f-0410-b117-8d365baceae5

--- src/bitwise.c
+++ src/bitwise.c
@@ -2,6 +2,7 @@
 #include <Rdefines.h>
 #include <Rinternals.h>
 
+
 /* 
  * 1. compile from commandline:
 
@@ -16,16 +17,22 @@
 
 */
 
+/*#define DEBUG*/
 SEXP nortek_checksum(SEXP buf, SEXP key)
 {
   /* http://www.nortek-as.com/en/knowledge-center/forum/current-profilers-and-current-meters/367698326 */
   /* 
      R CMD SHLIB bitwise.c 
-     dyn.load("~/src/R-kelley/oce/src/bitwise.so");.Call("nortek_checksum",buf[vvd.start[1]+0:23], c(0xb5, 0x8c))
+     library(oce)
+     f <- "/Users/kelley/data/archive/sleiwex/2008/moorings/m06/vector1943/194301.vec" ## dir will change; times are odd
+     buf <- readBin(f, what="raw", n=1e6)
+     vvd.start <- match.bytes(buf, 0xa5, 0x10)
+     dyn.load("~/src/R-kelley/oce/src/bitwise.so")
+     for(i in 1:200) {cat(.Call("nortek_checksum",buf[vvd.start[i]+0:23], c(0xb5, 0x8c)),"\n")}
   */
   int i, n;
   short check_value;
-  double *resp;
+  int *resp;
   /*short *bufp;*/
   unsigned char *bufp, *keyp;
   SEXP res;
@@ -33,23 +40,38 @@ SEXP nortek_checksum(SEXP buf, SEXP key)
   PROTECT(buf = AS_RAW(buf));
   bufp = (unsigned char*)RAW_POINTER(buf);
   keyp = (unsigned char*)RAW_POINTER(key);
+#ifdef DEBUG
   Rprintf("buf[0]=0x%02x\n",bufp[0]);
   Rprintf("buf[1]=0x%02x\n",bufp[1]);
   Rprintf("buf[2]=0x%02x\n",bufp[2]);
   Rprintf("key[0]=0x%02x\n", keyp[0]);
   Rprintf("key[1]=0x%02x\n", keyp[1]);
-  check_value = (((short)keyp[0]) << 8) | (short)keyp[1]; /* FIXME: should be -19060 Q: how to make a signed short from two hexes? */
-  Rprintf("key[0]=0x%02x (shifted 1)\n", keyp[0]<<1);
-  Rprintf("key[0]=0x%02x (shifted 8)\n", keyp[0]<<8);
-  Rprintf("key[0]=0x%02x\n", keyp[0]);
-  Rprintf("check_value= %d\n", check_value);
+#endif
   n = LENGTH(buf);
-  for (i = 0; i < n - 2; i++) {
-    check_value += bufp[i];
+  check_value = (((short)keyp[0]) << 8) | (short)keyp[1]; /* FIXME: should be -19060 Q: how to make a signed short from two hexes? */
+#ifdef DEBUG
+  Rprintf("check_value= %d\n", check_value);
+  Rprintf("n=%d\n", n);
+#endif
+  short *sbufp = (short*) bufp;
+  for (i = 0; i < (n - 2)/2; i++) {
+#ifdef DEBUG
+    Rprintf("i=%d buf=0x%02x\n", i, sbufp[i]);
+#endif
+    check_value += sbufp[i];
+#ifdef DEBUG
+    Rprintf("after, check_value=%d\n", check_value);
+#endif
   }
-  *resp = 0.0;                    /* FIXME: should be logical */
-  PROTECT(res = NEW_NUMERIC(1));
-  resp = NUMERIC_POINTER(res);
+  short checksum;
+  checksum = (((short)bufp[n-1]) << 8) | (short)bufp[n-2]; /* FIXME: should be -19060 Q: how to make a signed short from two hexes? */
+#ifdef DEBUG
+  Rprintf("CHECK AGAINST 0x%02x 0x%02x\n", bufp[n-2], bufp[n-1]);
+  Rprintf("CHECK AGAINST %d\n", checksum);
+#endif
+  PROTECT(res = NEW_LOGICAL(1));
+  resp = LOGICAL_POINTER(res);
+  *resp = check_value == checksum;
   UNPROTECT(3);
   return(res);
 }
