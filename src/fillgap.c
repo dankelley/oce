477efa6a71b600e293c1273ddb3a0db8fe7836f9
Dan Kelley <kelley.dan@gmail.com>
2010-05-23 21:58:48 -0300

    fillgap passes simplest test

--- src/fillgap.c
+++ src/fillgap.c
@@ -12,7 +12,7 @@
      ::
 
        library(oce)
-       x <- c(1:10, NA, NA, 13:15)
+       x <- c(1:2, NA, NA, 3:4)
        dyn.load("fillgap.so"); xx <- .Call("fillgap", x); print(xx)
 
 */
@@ -28,34 +28,33 @@ SEXP fillgap(SEXP x)
   PROTECT(res = allocVector(REALSXP, xlen));
   double *resp = REAL(res);
 #ifdef DEBUG
-// 
   for (i = 0; i < n; i++) {
     Rprintf("x[%d]=%f\n", i, xp[i]);
   }
 #endif
   int last_ok, next_ok;
-  double x_last_ok, x_next_ok;
+  double x_last_ok = 0.0;
   for (i = 0; i < xlen; i++) {
     if (ISNA(xp[i])) {
-      last_ok = i - 1;          /* FIXME: what if NA at start? */
       if (i == 0)
-        x_last_ok = 0.0;
-      else
-        x_last_ok = xp[last_ok];
+        error("fillgap cannot handle NA at start");
+      last_ok = i - 1;
+      x_last_ok = xp[last_ok];
       for (int j = i; j < xlen; j++) {
         if (!ISNA(xp[j])) {
-          /* interpolate */
           for (int ij = last_ok + 1; ij < j; ij++) {
-            resp[ij] = 999.99;       /* FIXME: interpolate here */
+            resp[ij] = x_last_ok + (ij - last_ok) * (xp[j] - x_last_ok) / (j - i + 1);
           }
           i = j - 1;
           break;
         }
+        if (j == xlen)
+          error("fillgap cannot handle NA at end");
       }
     } else {
       resp[i] = xp[i];
     }
-  } /* FIXME: what if ends in NA? */
+  }
   UNPROTECT(2);
   return(res);
 }
