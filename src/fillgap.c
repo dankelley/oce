18fdccb0349d1cd98873a456c84512ecc05a2335
Dan Kelley <kelley.dan@gmail.com>
2010-05-24 07:05:03 -0300

    improve, but temporarily limit, fill.gap

--- src/fillgap.c
+++ src/fillgap.c
@@ -29,25 +29,26 @@ SEXP fillgap(SEXP x)
   double *resp = REAL(res);
   int last_ok, next_ok;
   double x_last_ok = 0.0;
-  for (i = 0; i < xlen; i++) {
-    if (ISNA(xp[i])) {
-      if (i == 0)
-        error("fillgap cannot handle NA at start");
-      last_ok = i - 1;
-      x_last_ok = xp[last_ok];
-      for (int j = i; j < xlen; j++) {
-        if (!ISNA(xp[j])) {
-          for (int ij = last_ok + 1; ij < j; ij++) {
-            resp[ij] = x_last_ok + (ij - last_ok) * (xp[j] - x_last_ok) / (j - i + 1);
-          }
-          i = j - 1;
-          break;
-        }
-        if (j == xlen - 1)
-          error("fillgap cannot handle NA at end");
-      }
-    } else {
+  if (ISNA(xp[0]) || ISNA(xp[xlen - 1])) {
+    for (i = 0; i < xlen; i++)
       resp[i] = xp[i];
+  } else {
+    for (i = 0; i < xlen; i++) {
+      if (ISNA(xp[i])) {
+        last_ok = i - 1;
+        x_last_ok = xp[last_ok];
+        for (int j = i; j < xlen; j++) {
+          if (!ISNA(xp[j])) {
+            for (int ij = last_ok + 1; ij < j; ij++) {
+              resp[ij] = x_last_ok + (ij - last_ok) * (xp[j] - x_last_ok) / (j - i + 1);
+            }
+            i = j - 1;
+            break;
+          }
+        }
+      } else {
+        resp[i] = xp[i];
+      }
     }
   }
   UNPROTECT(2);
