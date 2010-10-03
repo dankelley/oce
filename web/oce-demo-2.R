6c6031924712c8caed9213b4a9da720c5fc2d38b
Dan Kelley <kelley.dan@gmail.com>
2010-10-03 14:55:54 -0300

    adp is already enu

--- web/oce-demo-2.R
+++ web/oce-demo-2.R
@@ -1,5 +1,4 @@
 library(oce)
 data(adp)
-e <- adp.xyz2enu(adp.beam2xyz(adp))
-plot(e,which=1:3,
+plot(adp, which=1:3,
      adorn=expression({lines(x$data$ts$time, x$data$ts$pressure, lwd=3, col='blue')}))
