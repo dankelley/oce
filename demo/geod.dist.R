f976b4de797c36c7f8d115bdf7d421bf0b95c44d
Dan Kelley <kelley.dan@gmail.com>
2011-01-23 14:22:49 -0400

    Reformat demo/*.R files.

--- demo/geod.dist.R
+++ demo/geod.dist.R
@@ -1,7 +1,7 @@
 library(oce);
-cat("Originally, the kilometre was defined as 1/10000 of the distance
-from the equator to the pole on a line running through Paris.  This
-deviates from the modern definition by",
-round(0.01*abs(1 - geod.dist(0,2,90,2)/1e4),digits=7),
-"percent, according to the
-geodesic distance calculated by geod.dist().\n")
+cat("Originally, the kilometre was defined as 1/10000 of the\n")
+cat("distance from the equator to the pole on a line running\n")
+cat("through Paris. This deviates from the present definition\n")
+cat("by ")
+cat(round(0.01*abs(1 - geod.dist(0,2,90,2)/1e4),digits=7))
+cat("%, according to geod.dist().\n")
