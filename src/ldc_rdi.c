78e0e878eb91c1b8faa00e288c28125d954682ea
Dan Kelley <kelley.dan@gmail.com>
2011-03-24 10:15:09 -0300

    clean ldc_rdi.c

--- src/ldc_rdi.c
+++ src/ldc_rdi.c
@@ -7,26 +7,20 @@
 
 /* 
 
-# Testing
+This code was based on similar for sontek.  But, the best plan is
+always to figure out the algorithm (how many bytes to skip, etc.)
+using R.  Below are some notes on how I did that; I'll retain them in
+the code as a guide.  All of the below is in R (but some comments are
+instructions).
 
 library(oce)
 # Temporarily instrument read.adp.rdi() to output ''b'' for ''buf'' in the code.
 source("../R/adp.rdi.R")
 d <- read.adp.rdi('/data/archive/sleiwex/2008/moorings/m11/adp/rdi_10485/raw/adp_rdi_10485.000',from=1,to=1000)
-# now have a local var 'b'
-system("R CMD SHLIB ldc_rdi.o");dyn.load("ldc_rdi.so");.Call("ldc_rdi", b, 3)
-
-# Algorithm development
-
-# Temporarily instrument read.adp.rdi() to output ''b'' for ''buf'' in the code.
-
-system("R CMD SHLIB ldc_rdi.o")
-source("../R/adp.rdi.R")
-d <- read.adp.rdi('/data/archive/sleiwex/2008/moorings/m11/adp/rdi_10485/raw/adp_rdi_10485.000',from=1,to=1000)
+# Now 'b' holds the data.  Find some trial start-points by matching bytes:
 p <- .Call("match2bytes", b, 0x7f, 0x7f, !TRUE)
 bytes.in.ensemble <- as.numeric(b[p[1]+2]) + 256*as.numeric(b[p[1]+3])
-
-# check some checsums
+# check some checksums
 for (i in 1:10) {
 calc <- sum(as.numeric(b[p[i]+seq(0, bytes.in.ensemble-1)]))%%2^16
 check <- as.numeric(b[p[i]+bytes.in.ensemble]) + 256 * as.numeric(b[p[i]+bytes.in.ensemble+1])
@@ -38,18 +32,19 @@ cat(i, calc, check, if(calc==check) "OK\n" else "BAD\n")
 SEXP ldc_rdi(SEXP buf, SEXP max)
 {
   /* ldc_rdi = locate data chunk for RDI
-   * Ref: WorHorse Commands and Output Data Format_Nov07.pdf
-   * p124: header structure (note that 'nuber of bytes in ensemble'
+   * Ref: WorkHorse Commands and Output Data Format_Nov07.pdf
+   * p124: header structure (note that 'number of bytes in ensemble'
    *       does *not* count the first 2 bytes; it's really an offset to the
    *       checksum)
    * p158 (section 5.8) checksum
    */
-  unsigned char *pbuf, *pmatch;
   PROTECT(buf = AS_RAW(buf));
   PROTECT(max = AS_INTEGER(max));
   /* FIXME: check lengths of match and key */
-  pbuf = RAW_POINTER(buf);
+  unsigned char *pbuf = RAW_POINTER(buf);
   int max_lres = *INTEGER_POINTER(max);
+  if (max_lres < 0)
+    error("'max' must be positive");
   int lres;
   int lbuf = LENGTH(buf);
   SEXP res;
@@ -62,41 +57,27 @@ SEXP ldc_rdi(SEXP buf, SEXP max)
   unsigned int matches = 0;
   unsigned short int check_sum, desired_check_sum, desired_check_sum2;
   unsigned int bytes_to_check = 0;
-  if (max_lres < 0)
-    max_lres = 0;
 #ifdef DEBUG
   Rprintf("max_lres %d\n", max_lres);
 #endif
   for (int i = 0; i < lbuf - 1; i++) { /* note that we don't look to the very end */
     if (pbuf[i] == byte1 && pbuf[i+1] == byte2) { /* match first 2 bytes, now check the checksum */
-#ifdef DEBUG
-      Rprintf("tentative match %d at i = %d ... ", matches, i);
-#endif
-      if (matches == 0) {
+      if (matches == 0)
 	bytes_to_check = pbuf[i+2] + 256 * pbuf[i+3];
-#ifdef DEBUG
-	Rprintf("bytes_to_check=%d\n", bytes_to_check);
-#endif
-      }
       check_sum = 0;
       for (int c = 0; c < bytes_to_check; c++)
 	check_sum += (unsigned short int)pbuf[i + c];
       desired_check_sum = ((unsigned short)pbuf[i+bytes_to_check+0]) | ((unsigned short)pbuf[i+bytes_to_check+1] << 8);
-#ifdef DEBUG
-      Rprintf("check1 %x  check2 %x\n", pbuf[i+bytes_to_check+0], pbuf[i+bytes_to_check+1]);
-      Rprintf("check_sum %d\n", check_sum);
-      Rprintf("desired_check_sum %d\n", desired_check_sum);
-#endif
       if (check_sum == desired_check_sum) {
 	matches++;
 #ifdef DEBUG
-	Rprintf("good match (check_sum=%d)\n", check_sum);
+	Rprintf("buf[%d] ok\n", i);
 #endif
 	if (max_lres != 0 && matches >= max_lres)
 	  break;
       } else {
 #ifdef DEBUG
-	Rprintf("bad checksum\n");
+	Rprintf("buf[%d] checksum %d (needed %d)\n", i, check_sum, desired_check_sum);
 #endif
       }
     }
@@ -115,21 +96,18 @@ SEXP ldc_rdi(SEXP buf, SEXP max)
       if (pbuf[i] == byte1 && pbuf[i+1] == byte2) { /* match first 2 bytes, now check the checksum */
 	for (int c = 0; c < bytes_to_check; c++)
 	  check_sum += (unsigned short int)pbuf[i + c];
-	desired_check_sum = ((unsigned short)pbuf[i+bytes_to_check+0]) | ((unsigned short)pbuf[i+bytes_to_check+1] << 8);
-	if (check_sum == desired_check_sum) {
+	desired_check_sum = ((unsigned short)pbuf[i+bytes_to_check]) | ((unsigned short)pbuf[i+bytes_to_check+1] << 8);
+	if (check_sum == desired_check_sum)
 	  pres[ires++] = i + 1; /* the +1 is to get R pointers */
-	}
-	if (ires > lres)        /* FIXME: or +1? */
+	if (ires >= lres)
 	  break;
       }
     }
-    UNPROTECT(3);
-    return(res);
   } else {
     PROTECT(res = NEW_INTEGER(1));
     int *pres = INTEGER_POINTER(res);
     pres[0] = 0;
-    UNPROTECT(3);
-    return(res);
   }
+  UNPROTECT(3);
+  return(res);
 }
