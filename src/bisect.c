292318949a7e0b5e12152b624edd166a2a16f1fb
Dan Kelley <kelley.dan@gmail.com>
2011-06-17 09:08:28 -0300

    .Call("bisect"...) can take multiple searches

--- src/bisect.c
+++ src/bisect.c
@@ -7,19 +7,18 @@
 //#define DEBUG
 
 /*
-To build this, build Oce or just do as follows:
+   To build this, build Oce or just do as follows:
 
-system("R CMD SHLIB bisect.c"); dyn.load("bisect.so"); .Call("bisect", seq(0, 10, 0.1), 2.1)
+   system("R CMD SHLIB bisect.c")
+   dyn.load("bisect.so")
 
-To use this, do the following ONE TIME in a given program:
+   To use it, do e.g.
 
-dyn.load("bisect.so")
+   x <- seq(0,10,0.1)
+   i <- .Call("bisect", x, c(2.1,3,3.3))
+   print(x[i])
 
-To use it, do e.g.
-
-.Call("bisect", seq(0, 10, 0.1), 2.1)
-
-which searches for 2.1 in the list (0, 0.1, 0.2,...).
+   which searches for 2.1, 3 and 3.3 in the list 0, 0.1, 0.2, ... ,10.
 
 */
 
@@ -28,44 +27,49 @@ SEXP bisect(SEXP x, SEXP X)
   // SYNOPSIS: bisection search
   // REQUIREMENT: x must be in order from smaller to larger
   // x = vector of numerical values
-  // X = value to search for
+  // X = value(s) to search for
   //
-  // RETURN VALUE: The index (in R notation, starting at 1) of first item for which
-  // x <= X & X < x
+  // RETURN VALUE: The index(es) (in R notation, starting at 1) of first item for which
+  //    x <= X & X < x
+  // for each X.
   //
   PROTECT(x= AS_NUMERIC(x));
   PROTECT(X= AS_NUMERIC(X));
   double *xp = REAL(x);
   double *Xp = REAL(X);
 
-  unsigned int n = LENGTH(x);
+  unsigned int nx = LENGTH(x);
+  unsigned int nX = LENGTH(X);
   SEXP res;
-  PROTECT(res = allocVector(REALSXP, 1));
+  PROTECT(res = allocVector(REALSXP, nX));
   double *resp = REAL(res);
 
-  if (n < 1) {
-    resp[0] = 0;
-  } else {
-    if (Xp[0] <= xp[0]) {
-      resp[0] = 1;
-    } else if (xp[n-1] <= Xp[0]) {
-      resp[0] = n;
+  for (int iX = 0; iX < nX; iX++) {
+
+    if (nx < 1) {
+      resp[iX] = 0;
     } else {
-      unsigned int lower = 0, upper = n-1, middle;
-      int npass = floor(5.0 + log2(0.0 + n)); // for safety, add 5; should never get close
-      for (unsigned int pass = 0; pass < npass; pass++) {
-	middle = (int)floor((upper + lower) / 2.0);
-	if (Xp[0] <= xp[middle])
-	  upper = middle;
-	else
-	  lower = middle;
-	if ((upper - lower) < 2)
-	  break;
+      if (Xp[iX] <= xp[0]) {
+	resp[iX] = 1;
+      } else if (xp[nx-1] <= Xp[iX]) {
+	resp[iX] = nx;
+      } else {
+	unsigned int lower = 0, upper = nx-1, middle;
+	int npass = floor(5.0 + log2(0.0 + nx)); // for safety, add 5; should never get close
+	for (unsigned int pass = 0; pass < npass; pass++) {
+	  middle = (int)floor((upper + lower) / 2.0);
+	  if (Xp[iX] <= xp[middle])
+	    upper = middle;
+	  else
+	    lower = middle;
+	  if ((upper - lower) < 2)
+	    break;
 #ifdef DEBUG
-	Rprintf("pass %d   lower %d   upper %d   middle %d\n", pass, lower, upper, middle);
+	  Rprintf("pass %d   lower %d   upper %d   middle %d\n", pass, lower, upper, middle);
 #endif
-      }	
-      resp[0] = middle + 1.0;
+	}	
+	resp[iX] = middle + 1.0;
+      }
     }
   }
   UNPROTECT(3);
