cc910ba39f329e2b4ea0a0e176be7f2352ffdebb
Dan Kelley <kelley.dan@gmail.com>
2011-06-20 21:00:51 -0300

    remove extraneous file

--- R/tt.R
+++
@@ -1,7 +0,0 @@
-library(oce)
-f <- "CTD_BCD2010666_01_01_DN.ODF"
-source('~/src/R-kelley/oce/R/oce.R')
-source('~/src/R-kelley/oce/R/ctd.R')
-d <- read.oce(f)
-plot(d)
-##plot(as.ctd(d$PSAL_01, d$TEMP_01, d$PRES_01))
