9eccda3aeb13f2eb645a8667766c80650fdc6b13
Dan Kelley <kelley.dan@gmail.com>
2011-08-15 14:15:54 -0300

    interpBarnes(): fewer messages

--- R/misc.R
+++ R/misc.R
@@ -721,32 +721,26 @@ interpBarnes <- function(x, y, z, w=NULL, xg=NULL, yg=NULL,
         stop("lengths of x and y disagree; they are ", n, " and ", length(y))
     if (length(z) != n)
         stop("lengths of x and z disagree; they are ", n, " and ", length(z))
-    if (is.null(w)) {
+    if (is.null(w))
         w <- rep(1.0, length(x))
-        cat("interp.barnes assuming equal weights on all data\n")
-    }
-    if (is.null(xg)) {
+    if (is.null(xg))
         xg <- pretty(x, n=50)
-        cat("interp.barnes using calculated value xg =", xg[1], ",", xg[2], ",...,", xg[length(xg)], "\n")
-    }
     if (is.null(yg)) {
         if (0 == diff(range(y))) {
             yg <- y[1]
-            cat("interp.barnes using calculated value yg =", yg[1], "\n")
         } else {
             yg <- pretty(y, n=50)
-            cat("interp.barnes using calculated value yg =", yg[1], ",", yg[2], ",...,", yg[length(yg)],"\n")
         }
     }
     if (is.null(xr)) {
         xr <- diff(range(x)) / sqrt(n)
-        if (xr == 0) xr <- 1
-        cat("interp.barnes using calculated value xr =", xr, "\n")
+        if (xr == 0)
+            xr <- 1
     }
     if (is.null(yr)) {
         yr <- diff(range(y)) / sqrt(n)
-        if (yr == 0) yr <- 1
-        cat("interp.barnes using calculated value yr =", yr, "\n")
+        if (yr == 0)
+            yr <- 1
     }
     zg <- .Call("interp_barnes",
                 as.double(x),
