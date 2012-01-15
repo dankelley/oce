5e67772185b2d505377faaf1ea89fb921b91bcb1
Dan Kelley <kelley.dan@gmail.com>
2012-01-15 18:58:34 -0400

    subset(adp, pressure...) permitted [issue 187]

--- R/oce.R
+++ R/oce.R
@@ -502,6 +502,21 @@ subset.oce <- function (x, subset, indices=NULL, debug=getOption("oceDebug"), ..
                         rval@data[[name]] <- x@data[[name]][,keep,]
                     }
                 }
+            } else if (length(grep("pressure", subsetString))) {
+                keep <- eval(substitute(subset), x@data, parent.frame())
+                rval <- x
+                rval@data$v <- rval@data$v[keep,,]
+                rval@data$a <- rval@data$a[keep,,]
+                rval@data$q <- rval@data$q[keep,,]
+                rval@data$bottomRange <- rval@data$bottomRange[keep,]
+                rval@data$time <- rval@data$time[keep]
+                rval@data$pressure <- rval@data$pressure[keep]
+                rval@data$temperature <- rval@data$temperature[keep]
+                rval@data$salinity <- rval@data$salinity[keep]
+                rval@data$depth <- rval@data$depth[keep]
+                rval@data$heading <- rval@data$heading[keep]
+                rval@data$pitch <- rval@data$pitch[keep]
+                rval@data$roll <- rval@data$roll[keep]
             } else {
                 stop("should express the subset in terms of distance or time")
             }
@@ -700,6 +715,7 @@ subset.oce <- function (x, subset, indices=NULL, debug=getOption("oceDebug"), ..
         }
         names(rval@data) <- names(x@data)
     }
+    ## fix up metadata
     if (inherits(x, "adp") || inherits(x, "adv")) {
         rval@metadata$numberOfSamples <- dim(rval@data$v)[1]
         rval@metadata$numberOfCells <- dim(rval@data$v)[2]
