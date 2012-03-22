012618045267496e7d75b4d8fb960b178c2a0957
Dan Kelley <kelley.dan@gmail.com>
2012-03-22 13:16:47 -0300

    bottom tracking tests

--- R/adp.rdi.R
+++ R/adp.rdi.R
@@ -461,10 +461,14 @@ read.adp.rdi <- function(file, from=1, to, by=1, tz=getOption("oceTz"),
                             bottomVelocity <- array(double(), dim=c(profilesToRead, numberOfBeams))
                             haveBottomTrack <- TRUE
                         }
-                        range.lsb <- readBin(buf[o+c(16:24)], "integer", n=4, size=2, signed=FALSE, endian="little")
-                        range.msb <- readBin(buf[o+78:81], "integer", n=4, size=1, signed=FALSE, endian="little")
-                        bottomRange[i,] <- (65536 * range.msb + range.lsb) / 100 # centimetres
-                        bottomVelocity[i,] <- readBin(buf[o+c(25:32)], "integer", n=4, size=2, signed=TRUE, endian="little") / 1000
+                        ## FIXME: figure out how to do bottom range, which is a bit confuing in the
+                        ## docs...below is the old way (commented out) and a newer way, but I think
+                        ## this newer way is wrong....
+                        ## range.lsb <- readBin(buf[o+c(16:23)], "integer", n=4, size=2, signed=FALSE, endian="little")
+                        ## range.msb <- readBin(buf[o+78:81], "integer", n=4, size=1, signed=FALSE, endian="little")
+                        ## bottomRange[i,] <- (65536 * range.msb + range.lsb) / 100 # centimetres
+                        bottomRange[i,] <- 0.01 * readBin(buf[o+c(16:23)], "integer", n=4, size=2, signed=FALSE, endian="little")
+                        bottomVelocity[i,] <- 0.001 * readBin(buf[o+c(24:31)], "integer", n=4, size=2, signed=TRUE, endian="little")
                     }
                     if (monitor) {
                         cat(".", ...)
