3b512f9f6c90893f7bf2339dbdc7e96b0b89249a
Dan Kelley <kelley.dan@gmail.com>
2012-04-10 21:50:55 -0300

    improve julianDay

--- R/astronomical.R
+++ R/astronomical.R
@@ -1,11 +1,13 @@
 julianDay <- function(t, year, month, day, hour, min, sec, tz="UTC")
 {
-    if (inherits(t, "POSIXt")) {
-        tt <- as.POSIXlt(t)
-    } else {
-        if (missing(month) || missing(day) || missing(hour) || missing(min) || missing(sec) || missing(tz)) stop("must supply month, day, hour, min, sec, and tz")
+    ## as.numeric(julian(t)+2440587.5)) is suggested in R doc on julian()
+    if (!inherits(t, "POSIXt"))  {
+        if (missing(month) || missing(day) || missing(hour)
+            || missing(min) || missing(sec) || missing(tz))
+            stop("must supply month, day, hour, min, sec, and tz")
         tt <- ISOdatetime(year, month, day, hour, min, sec, tz=tz)
     }
+    tt <- as.POSIXlt(t)
     year <- tt$year + 1900
     month <- tt$mon + 1
     day <- tt$mday + (tt$hour + tt$min / 60 + tt$sec / 3600) / 24 
@@ -14,8 +16,7 @@ julianDay <- function(t, year, month, day, hour, min, sec, tz="UTC")
     A <- floor(y / 100)
     B <- 2 - A + floor(A / 4)
     jd <- floor(365.25 * y) + floor(30.6001 * (m + 1)) + day + 1720994.5
+    ## correct for Gregorian calendar
     jd <- ifelse(tt > ISOdatetime(1582, 10, 15, 0, 0, 0), jd + B, jd)
-    ## rval <- list(jd=jd, old=as.numeric(julian(t)+2440587.5))
-    rval <- jd
-    rval
+    jd
 }
