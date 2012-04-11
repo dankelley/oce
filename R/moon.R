4fde363da7be1152fe8b942cf33baa627d11b036
Dan Kelley <kelley.dan@gmail.com>
2012-04-11 15:57:31 -0300

    checks on 30.a until "Then the Moon"

--- R/moon.R
+++ R/moon.R
@@ -34,6 +34,7 @@ julianCenturyAnomaly <- function(jd)
 
 moonAngle <- function(t, lat, lon, useRefraction=TRUE)
 {
+    RPD <- atan2(1, 1) / 45            # radians per degree
     ## In this cde, the symbol names follow Meeus chapter 30, with e.g. "p"
     ## used to indicate primes, e.g. Lp stands for L' in Meeus' notation.
     ## Also, T2 and T3 are powers on T.
@@ -44,28 +45,61 @@ moonAngle <- function(t, lat, lon, useRefraction=TRUE)
     T <- julianCenturyAnomaly(julianDay(t))
     T2 <- T * T
     T3 <- T * T2
-    ## Step 1 (top of Meuus page 148, chapter 30): mean quanties
+    ## Step 1 (top of Meuus page 148, chapter 30): mean quantaties
     ## moon mean longitude
     Lp <-    270.434164 + 481267.8831 * T - 0.001133 * T2 + 0.0000019 * T3
     ## sun mean amomaly
     M <-     358.475833 +  35999.0498 * T - 0.000150 * T2 - 0.0000033 * T3
     ## moon mean amomaly
-    Mp <-    296.104608 + 477198.8491 * T + 0.009192 * T3 + 0.0000144 * T3
+    Mp <-    296.104608 + 477198.8491 * T + 0.009192 * T2 + 0.0000144 * T3
     ## moon mean elongation
     D <-     350.737486 + 445267.1142 * T - 0.001436 * T2 + 0.0000019 * T3
     ## moon distance from ascending node
     F <-      11.250889 + 483202.0251 * T - 0.003211 * T2 - 0.0000003 * T3
     omega <- 259.183275 -   1934.1420 * T + 0.002078 * T2 + 0.0000022 * T3
-    ## Step 2 (bottom of p 148, chapter 30): add periodic variations ("additive terms")
+    ## Step 2 (to bottom of p 148, chapter 30): add periodic variations ("additive terms")
     if (TRUE) {
-        cat(sprintf("Step 1: mean values\n\tT %.10f\n\tLp %.4f\n\tM %.4f\n\tMp %.4f\n\tD %.4f\n\tF %.4f\n\tomega %.4f\n",
-                    T, Lp, M, Mp, D, F, omega))
+        cat(sprintf("Step 1: mean values\n\tLp %.4f (%.4f)\n\tM %.4f (%.4f)\n\tMp %.4f (%.4f)\n\tD %.4f (%.4f)\n\tF %.4f (%.4f)\n\tomega %.4f (%.4f)\n",
+                    Lp, Lp %% 360,
+                    M, M %% 360,
+                    Mp, Mp %% 360,
+                    D, D %% 360,
+                    F, F %% 360, 
+                    omega, omega %% 360))
         stopifnot(all.equal(T, 0.7993018480))
     }
+    ## note that 'tmp' is redefined every few lines
+    tmp <- sin(RPD * (51.2 + 20.2 * T))
+    Lp <- Lp + 0.000233 * tmp
+    M  <- M  - 0.001778 * tmp
+    Mp <- Mp + 0.000817 * tmp
+    D  <-  D + 0.002011 * tmp
+    tmp <- 0.003964 * sin(RPD * (346.560 + 132.870 * T - 0.0091731 * T2))
+    Lp <- Lp +  tmp
+    Mp <- Mp +  tmp
+    D  <- D  +  tmp
+    F  <- F  +  tmp
+    tmp <- sin(RPD * omega)
+    Lp <- Lp + 0.001964 * tmp
+    Mp <- Mp + 0.002541 * tmp
+    D  <- D  + 0.001964 * tmp
+    F  <- F  - 0.024691 * tmp
+    F  <- F  - 0.004328 * sin(RPD * (omega + 275.05 - 2.30 * T))
+    ## Step 3: Meeus p 149
+    e <- 1 - 0.002495 * T - 0.00000752 * T2
+    e2 <- e * e
     if (TRUE) {
-        cat(sprintf("Step 2: after additive terms\n\tT %.10f\n\tLp %.4f\n\tM %.4f\n\tMp %.4f\n\tD %.4f\n\tF %.4f\n\tomega %.4f\n",
-                    T, Lp, M, Mp, D, F, omega))
+        cat(sprintf("Step 2: after additive terms\n\tLp %.4f (%.4f)\n\tM %.4f (%.4f)\n\tMp %.4f (%.4f)\n\tD %.4f (%.4f)\n\tF %.4f (%.4f)\n\tomega %.4f (%.4f)\n\te %.10f\n",
+                    Lp, Lp %% 360,
+                    M, M %% 360,
+                    Mp, Mp %% 360,
+                    D, D %% 360,
+                    F, F %% 360, 
+                    omega, omega %% 360,
+                    e))
+        stopifnot(all.equal(e, 0.998001, 0.000001))
     }
+    #browser()
     data.frame(azimuth=1:3, elevation=1:3, diameter=rep(0, 3), distance=rep(0, 3))
 }
 
