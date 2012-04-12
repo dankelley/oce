8aec39e32c7003c54fb07016911c91fa3f783753
Dan Kelley <kelley.dan@gmail.com>
2012-04-12 16:33:37 -0300

    moon

--- R/moon.R
+++ R/moon.R
@@ -3,6 +3,33 @@
 ## Meeus, Jean, 1982.  Astronomical formuae for Calculators.
 ## Willmann-Bell. Richmond VA, USA. 201 pages.
 
+debugMoonAngle <- !TRUE
+
+##
+## > siderealTime(ISOdatetime(1978,11,13,4,34,0,tz="UTC"))
+## [1] 8.029539657
+## cf  8.0295394 (Meeus 1982 page 40)
+siderealTime <- function(t)
+{                                   
+    ## Calculate siderial time at greenwich (Meuss 1982 equation 7.1, page 39).
+    ## NB. the correction he later discusses seems to be sub-second,
+    ## so we ignore it.  A test value, below, matches his text.
+    ## > format(theta0(julianCenturyAnomaly(2443825.5)),digits=10)
+    ## [1] "3.450369881"
+    tt <- as.POSIXlt(t)
+    n <- length(tt$hour)
+    tt$hour <- rep(0, n)
+    tt$min <- rep(0, n)
+    tt$sec <- rep(0, n)
+    jd <- julianDay(t)
+    jd0 <- julianDay(tt)
+    T <- (jd0 - 2415020.0) / 36525      # Meeus 1982 (eq 7.1)
+    hoursLeftOver <- 24 * (jd - jd0)
+    rval <- 6.6460656 + 2400.051262 * T + 0.00002581 * T * T + 1.002737908 * hoursLeftOver
+    rval <- rval %% 24
+    rval
+}
+
 julianDay <- function(t, year, month, day, hour, min, sec, tz="UTC")
 {
     ## Meeus (1982 ch 3)
@@ -29,16 +56,16 @@ julianDay <- function(t, year, month, day, hour, min, sec, tz="UTC")
 
 julianCenturyAnomaly <- function(jd)
 {
-    (jd - 2415020.0) / 36525         # Meeus 1982 (eq 15.1)
+    (jd - 2415020.0) / 36525         # Meeus 1982 (eq 7.1 or 15.1)
 }
 
-moonAngle <- function(t, lat, lon, useRefraction=TRUE)
+moonAngle <- function(t, latitude, longitude, useRefraction=TRUE)
 {
     RPD <- atan2(1, 1) / 45            # radians per degree
     ## In this cde, the symbol names follow Meeus chapter 30, with e.g. "p"
     ## used to indicate primes, e.g. Lp stands for L' in Meeus' notation.
     ## Also, T2 and T3 are powers on T.
-    if (TRUE) {
+    if (debugMoonAngle) {
         t <- ISOdatetime(1979, 12, 7, 0, 0, 0, tz="ET")
         cat(sprintf("t %s\n", format(t)))
     }
@@ -59,7 +86,7 @@ moonAngle <- function(t, lat, lon, useRefraction=TRUE)
     ## longitude of moon ascending node
     Omega <- 259.183275 -   1934.1420 * T + 0.002078 * T2 + 0.0000022 * T3
     ## Step 2 (to bottom of p 148, chapter 30): add periodic variations ("additive terms")
-    if (TRUE) {
+    if (debugMoonAngle) {
         cat(sprintf("Step 1: mean values\n\tLp %.4f (%.4f)\n\tM %.4f (%.4f)\n\tMp %.4f (%.4f)\n\tD %.4f (%.4f)\n\tF %.4f (%.4f)\n\tOmega %.4f (%.4f)\n",
                     Lp, Lp %% 360,
                     M, M %% 360,
@@ -89,7 +116,7 @@ moonAngle <- function(t, lat, lon, useRefraction=TRUE)
     ## Step 3: Meeus p 149
     e <- 1 - 0.002495 * T - 0.00000752 * T2
     e2 <- e * e
-    if (TRUE) {
+    if (debugMoonAngle) {
         cat(sprintf("Step 2: after additive terms\n\tLp %.4f (%.4f)\n\tM %.4f (%.4f)\n\tMp %.4f (%.4f)\n\tD %.4f (%.4f)\n\tF %.4f (%.4f)\n\tOmega %.4f (%.4f)\n\te %.10f\n",
                     Lp, Lp %% 360,
                     M, M %% 360,
@@ -115,7 +142,8 @@ moonAngle <- function(t, lat, lon, useRefraction=TRUE)
     (    -0.034718 * sin(RPD * (D             ))) +
     (    -0.030465 * sin(RPD * (M + Mp        ))) +
     (     0.015326 * sin(RPD * (2 * D - 2 * F )))
-    warning("several terms in lambda ignored [and existing ones not checked]\n")
+    lambda <- lambda %% 360
+    ##warning("several terms in lambda ignored\n")
     B <-  0 +
     (     5.128189 * sin(RPD * (F             ))) +
     (     0.280606 * sin(RPD * (Mp + F        ))) +
@@ -125,26 +153,70 @@ moonAngle <- function(t, lat, lon, useRefraction=TRUE)
     (     0.046272 * sin(RPD * (2 * D - F - Mp))) +
     (     0.032573 * sin(RPD * (2 * D + F     ))) +
     (     0.017198 * sin(RPD * (2 * Mp + F    ))) +
-    (     0.009267 * sin(RPD * (2 * D + Mp - F)))
-    warning("several terms in B ignored [existing ones checked briefly]\n")
+    (     0.009267 * sin(RPD * (2 * D + Mp - F))) +
+    (     0.008823 * sin(RPD * (2 * Mp - F    ))) +
+    (     0.008247 * sin(RPD * (2 * D - M - F )))
+    ##warning("several terms in B ignored\n")
     omega1 <- 0.0004664 * cos(RPD * Omega)
-    omega2 <- 0.0000756 * cos(RPD * (Omega + 275.05 - 2.30 * T))
+    omega2 <- 0.0000754 * cos(RPD * (Omega + 275.05 - 2.30 * T))
     beta <- B * (1 - omega1 - omega2)
-    if (TRUE) {
-        cat(sprintf("Step 3:\n\tlambda %.4f (%.4f)\n\tB %.4f (%.4f)\n\tbeta %.4f (%.4f)\n\tomega1 %.7f\n\tomega2 %.7f\n",
+    pi <- 0.950724 +
+    (     0.051818 * cos(RPD * (Mp            ))) +
+    (     0.009531 * cos(RPD * (2 * D - Mp    ))) +
+    (     0.007843 * cos(RPD * (2 * D         ))) +
+    (     0.002824 * cos(RPD * (2 * Mp        ))) +
+    (     0.000857 * cos(RPD * (2 * D + Mp    ))) +
+    (e *  0.000533 * cos(RPD * (2 * D - M     )))
+    ##warning("several terms in pi ignored\n")
+    if (debugMoonAngle) {
+        cat(sprintf("Step 3:\n\tlambda %.4f (%.4f)\n\tB %.4f (%.4f)\n\tbeta %.4f (%.4f)\n\tomega1 %.7f\n\tomega2 %.7f\n\tpi %.7f\n",
                     lambda, lambda %% 360,
                     B, B %% 360,
                     beta, beta %% 360,
                     omega1,
-                    omega2))
+                    omega2, 
+                    pi))
         ##stopifnot(all.equal(omega1, -0.0004164, 0.001, scale=1e-4)) # obscure syntax
         stopifnot(abs(omega1 - (-0.0004164)) < 0.0000002)
         stopifnot(abs(omega2 - (+0.0000301)) < 0.0000002)
+        stopifnot(all.equal(lambda, 113.6604, tol=0.01))
+        stopifnot(all.equal(beta, -3.163037, tol=0.001))
+        stopifnot(all.equal(pi, 0.930249, tol=0.001))
     }
-    #browser()
-    ## convert to other coords
-    ## Meuus eq 8.3-8.4 converts (lambda,beta) to (alpha, delta)
-    ## Meuus eq 8.5-8.6 converts (delta) [how just 1 value?] to (A, h)
-    data.frame(azimuth=1:3, elevation=1:3, diameter=rep(0, 3), distance=rep(0, 3))
+    ## For coordinate conversions, need epsilon (obliquity of the ecliptic) 
+    ## as defined in Meuus eq 18.4, page 81.
+    epsilon <- 23.452294 - 0.0130125 * T - 0.00000164 * T2 + 0.000000503 * T3
+    ## Transform ecliptical to equatorial coordinates (Meuss eq 8.3 and 8.4) page 44
+    alpha <- atan((sin(RPD * lambda) * cos(RPD * epsilon) - tan(RPD * beta) * sin(RPD * epsilon)) / cos(RPD * lambda))
+    delta <- asin(sin(RPD * beta) * cos(RPD * epsilon) + cos(RPD * beta) * sin(RPD * epsilon) * sin(RPD * lambda))
+    ## sidereal time at Greenwhich at 0000UTC (in hours)
+    theta0 <- siderealTime(t)
+    H <- theta0 * 15 - longitude - alpha / RPD
+    ## Transform to local horizontal coordinates (A=azimuth, h=altitude); Meuss eq 8.5 and 8.6 page 44
+    A <- atan(sin(RPD * H) / (cos(RPD * H) * sin(RPD * latitude) - tan(delta) * cos(RPD * latitude)))
+    ##A <- ifelse(A < 0, A + 180*RPD, A)
+    h <- asin(sin(RPD * latitude) * sin(delta) + cos(RPD * latitude) * cos(delta) * cos(RPD * H))
+    rval <- data.frame(t=t, azimuth=A / RPD, altitude=h / RPD, diameter=pi, distance=6378.14 / pi,
+                       T=T, lambda=lambda %% 360, beta=beta, epsilon=epsilon, H=H, theta0=theta0)
+    rval
 }
 
+
+## moonrise: Apr 12, 2012	1:48 AM 
+par(mfrow=c(3,2))
+days <- 1:3
+rises <- ISOdatetime(2012,4,days,c(13,15,16), c(55, 04, 16),0,tz="UTC") + 4 * 3600
+sets <- ISOdatetime(2012,4,days,c(3,4,4), c(42, 15, 45),0,tz="UTC") + 4 * 3600
+for (i in 1:3) {
+    t <- ISOdatetime(2012,4,days[i],0,0,0,tz="UTC") + seq(0, 24*3600, 3600/4)
+    ma <- moonAngle(t, 44.65, -63.6) 
+    oce.plot.ts(t, ma$altitude)
+    abline(h=0)
+    abline(v=rises[i], col='red')
+    abline(v=sets[i], col='blue')
+    oce.plot.ts(t, ma$azimuth)
+    abline(h=0)
+    abline(v=rises[i], col='red')
+    abline(v=sets[i], col='blue')
+}
+## http://www.timeanddate.com/worldclock/astronomy.html?n=286&month=4&year=2012&obj=moon&afl=-12&day=1
