e9746e125de59cf28dfda42cdf8584fa6eece4b7
Dan Kelley <kelley.dan@gmail.com>
2012-04-17 15:09:08 -0300

    teos() added

---
+++ R/teos.R
@@ -0,0 +1,30 @@
+teos <- function(name, a1, a2, a3, a4, a5, a6, lib='/usr/local/lib/libgswteos-10.so') # FIXME: what's max arg?
+{
+    if (missing(name)) stop("must give function name, e.g. \"gsw_sa_from_sp\" to calculate absolute salinity from practical salinity")
+    if (missing(a1)) stop("must provide a1")
+    ## FIXME: later, can count when the missing list starts, and use that to figure out which C function to call
+    if (missing(a2)) stop("must provide a2")
+    if (missing(a3)) stop("must provide a3")
+    if (missing(a4)) args <- 3 else args <- 4
+    if (!missing(a5)) stop("cannot have 5 args yet")
+    if (!missing(a6)) stop("cannot have 6 args yet")
+    dim <- dim(a1)
+    a1 <- as.vector(a1)
+    n <- length(a1)
+    a2 <- as.vector(a2)
+    if (length(a2) != n) stop("length(a2) must match length(a1)")
+    if (length(a3) != n) stop("length(a3) must match length(a1)")
+    a3 <- as.vector(a3)
+    if (args == 3) {
+        rval <- .C("gsw3a", as.character(lib), as.character(name),
+                   as.integer(n), as.double(a1), as.double(a2), as.double(a3), rval=double(n))$rval
+    } else if (args == 4) {
+        a4 <- as.vector(a4)
+        if (length(a4) != n) stop("length(a4) must match length(a1)")
+        rval <- .C("gsw4a", as.character(lib), as.character(name),
+                   as.integer(n), as.double(a1), as.double(a2), as.double(a3), as.double(a4), rval=double(n))$rval
+    }
+    dim(rval) <- dim
+    rval
+}
+
