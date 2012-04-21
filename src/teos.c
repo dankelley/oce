d9f5bc32964f7cf5061461858581f2fe84fbeb6e
Dan Kelley <kelley.dan@gmail.com>
2012-04-21 07:47:42 -0300

    teos(): speed up function calls (hugely)
    Before this switch, the teos style for plotTS() took about 3 seconds.
    The problem was that dlopen() was being called for single evaulation of
    a TEOS function.  Drawing a TS diagram takes of order 1e4 function
    calls.  Since the TEOS library is about 1e6 byte in size, that means
    moving 1e10 bytes across the bus (or across a within-chip bus, depending
    on the CPU cache size).  This means a transfer rate of roughly 3e9 bytes
    per second.  I don't know the allowed speeds on my machines, but this
    seems like it might be close to the speed of the devide.

--- src/teos.c
+++ src/teos.c
@@ -4,27 +4,25 @@
 #include <R.h>
 #include <Rdefines.h>
 #include <Rinternals.h>
-
+static int first_teos_call = 1;
+static void *teos_handle = NULL;
 void gsw3a(char **lib, char **name, int *n, double *a1, double *a2, double *a3, double *rval)
 {
-    //*lib = "/usr/local/lib/libgswteos-10.so"; // FIXME: testing, since getting an error otherwise
-    //Rprintf("%s:%d about to do dlopen on '%s'\n", __FILE__, __LINE__, *lib);
-    void *handle = dlopen(*lib, RTLD_LOCAL|RTLD_LAZY);
-    //Rprintf("%s:%d about to do check handle for that dlopen\n", __FILE__, __LINE__);
-    if (!handle) {
-        error("cannot open TEOS library %s; error is: %s", *lib, dlerror());
+    if (first_teos_call) {
+        teos_handle = dlopen(*lib, RTLD_LOCAL|RTLD_LAZY);
+        first_teos_call = 0;
     }
+    if (!teos_handle)
+        error("cannot open TEOS library %s; error is: %s", *lib, dlerror());
     //Rprintf("%s:%d about to do try to find dlsym(handle, \"%s\"\n", __FILE__, __LINE__, *name);
-    double (*f3)(double, double, double) = dlsym(handle, *name);
+    double (*f3)(double, double, double) = dlsym(teos_handle, *name);
     if (!f3) 
-        error("cannot find \"%s\" in TEOS library %s", *name, *lib);
+        error("cannot find \"%s\" in TEOS library %s; error is: %s", *name, *lib, dlerror());
     //Rprintf("%s:%d about to do the loop calling f3 (\"%s\")\n", __FILE__, __LINE__, *name);
     for (int i = 0; i < *n; i++) {
         //Rprintf("%s:%d in loop i=%d, a1[i]=%f, a2[i]=%f a3[i]=%f\n",__FILE__,__LINE__,i,a1[i],a2[i],a3[i]);
         rval[i] = (*f3)(a1[i], a2[i], a3[i]);
     }
-    //Rprintf("      gsw3a(lib, '%s', n=%d, a1=%f a2=%f a3=%f  RETURNING %f\n", *name, *n, *a1, *a2, *a3, rval[0]);
-    dlclose(handle);
 }
 
 void gsw4a(char **lib, char **name, int *n, double *a1, double *a2, double *a3, double *a4, double *rval)
