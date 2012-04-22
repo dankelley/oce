38910eb1b3a62551b99c1d5af9aa77825eb726f4
Dan Kelley <kelley.dan@gmail.com>
2012-04-22 13:44:32 -0300

    delete .First.lib() which is an archaic form

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,3 @@
-.First.lib <- function(lib, pkg)
-{
-    if (version$major==0 && version$minor < 62)
-        stop("This version for R 0.62 or later")
-    library.dynam("oce", pkg, lib)
-}
-
 .onLoad <- function(libname, pkgname)
 {
     op <- options()
