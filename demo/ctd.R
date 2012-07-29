1640d80ae4d722d866c221754381581925d4af3c
Dan Kelley <kelley.dan@gmail.com>
2012-07-29 16:25:26 -0300

    make all demos work (fix S4 errors)

--- demo/ctd.R
+++ demo/ctd.R
@@ -1,9 +1,9 @@
-library(oce);
+library(oce)
 data(ctd)
 summary(ctd)
 plot(ctd)
 cat("Some of the data are...\n") 
 cat("  P/dbar      S/PSU         T/degC\n")
-for (i in 1:min(5,length(ctd$data$salinity))) {
-    cat(ctd$data$pressure[i], "\t", ctd$data$salinity[i], "\t", ctd$data$temperature[i], "\n")
+for (i in 1:min(5,length(ctd[["salinity"]]))) {
+    cat(ctd[["pressure"]][i], "\t", ctd[["salinity"]][i], "\t", ctd[["temperature"]][i], "\n")
 }
