8e640a2ced0e74580351097b2a097cc3c02cde4f
Dan Kelley <kelley.dan@gmail.com>
2013-08-22 15:47:54 -0300

    standardDepths() added

--- R/oce.R
+++ R/oce.R
@@ -570,6 +570,13 @@ oce.write.table <- function (x, file="", ...)
 ##    rval
 ##}
 
+standardDepths <- function()
+{
+    c(0,   10,   20,   30,   50,   75,  100,  125,  150,  200,
+      250,  300,  400,  500,  600,  700,  800,  900, 1000, 1100,
+      1200, 1300, 1400, 1500, 1750, 2000, 2500, 3000, 3500, 4000,
+      4500, 5000, 5500)
+}
 summary.oce <- function(object, ...)
 {
     if (!inherits(object, "oce"))
--- R/section.R
+++ R/section.R
@@ -1115,10 +1115,7 @@ sectionGrid <- function(section, p, method=c("approx","boxcar","lm"),
     } else {
 	if (length(p) == 1) {
 	    if (p=="levitus") {
-		pt <- c(0,   10,   20,   30,   50,   75,  100,  125,  150,  200,
-			250,  300,  400,  500,  600,  700,  800,  900, 1000, 1100,
-			1200, 1300, 1400, 1500, 1750, 2000, 2500, 3000, 3500, 4000,
-			4500, 5000, 5500)
+		pt <- standardDepths()
                 pt <- pt[pt < max(section[["pressure"]])]
 	    } else { # FIXME should insist numeric
 		p.max <- 0
