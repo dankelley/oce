2e5236669510e40f971fad089c901a94a1907126
Dan Kelley <kelley.dan@gmail.com>
2014-07-08 13:03:22 -0300

    transpose_flip

--- src/transpose_flip.c
+++ src/transpose_flip.c
@@ -6,21 +6,27 @@
 #include <Rdefines.h>
 #include <Rinternals.h>
 
+//#define DEBUG // comment this out when working.
+
 /* 
- 
+
+
 m <- 0.0 + matrix(1:12, nrow=3, byrow=TRUE)
 system("R CMD SHLIB transpose_flip.c")
 dyn.load('transpose_flip.so')
-r1 <- .Call("transpose_flip", m)
-mt <- t(m)
-r2 <- mt[, seq.int(dim(mt)[2], 1, -1)]
-r2[r2==0] <- NA
 print("m:")
 m
-print("r1:")
-r1
-print("r2:")
+mt <- t(m)
+print("t(m):")
+mt
+r2 <- mt[, seq.int(dim(mt)[2], 1, -1)]
+r2[r2==0] <- NA
+print("r2 (expected result):")
 r2
+r1 <- .Call("transpose_flip", m)
+print("r1 (this result):")
+r1
+
 
 */
 
@@ -43,26 +49,37 @@ SEXP transpose_flip(SEXP m)
   double *resp = REAL(res);
 
   // Transpose
+#ifdef DEBUG
   Rprintf("transpose:\n");
+#endif
   for (int i = 0; i < nrow; i++) {
     for (int j = 0; j < ncol; j++) {
+#ifdef DEBUG
       Rprintf("i %d, j %d, ij_res(i,j) %d, ij_m(j, i) %d\n",
 	  i, j, ij_res(i,j), ij_m(j,i));
+#endif
       resp[ij_res(j, i)] = mp[ij_m(i, j)];
     }
   }
   // Flip in second dimension
+#ifdef DEBUG
   Rprintf("flip:\n");
+#endif
+  // ncol_res_half is to avoid undoing the flip by repeating it.
+  int ncol_res_half = (int)floor(ncol_res / 2.0);
   for (int i = 0; i < nrow_res; i++) {
-    for (int j = 0; j < ncol_res; j++) {
-      Rprintf("i %d, j %d, ij_res(i,j) %d, ij_res(i,ncol_res-j-1) %d\n",
-	  i, j, ij_res(i,j), ij_res(i, ncol_res-j-1));
+    for (int j = 0; j < ncol_res_half; j++) {
+#ifdef DEBUG
+      Rprintf("i %d, j %d, ncol_res-j-1 %d, ij_res(i,j) %d, ij_res(i,ncol_res-j-1) %d (%f -> %f)\n",
+	  i, j, ncol_res-j-1,
+	  ij_res(i,j), ij_res(i, ncol_res-j-1),
+	  resp[ij_res(i, j)], resp[ij_res(i,ncol_res-j-1)]);
+#endif
       double tmp = resp[ij_res(i, j)];
       resp[ij_res(i, j)] = resp[ij_res(i, ncol_res-j-1)];
       resp[ij_res(i, ncol_res-j-1)] = tmp;
     }
   }
-  Rprintf("FLIP NOT WORKING\n");
   UNPROTECT(1);
   return(res);
 }
