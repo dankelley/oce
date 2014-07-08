bb7b1d9236db7538278ec303addbbccb64d6f796
Dan Kelley <kelley.dan@gmail.com>
2014-07-08 11:39:01 -0300

    working on transpose flip

--- src/transpose_flip.c
+++ src/transpose_flip.c
@@ -11,23 +11,22 @@
 m <- 0.0 + matrix(1:12, nrow=3, byrow=TRUE)
 system("R CMD SHLIB transpose_flip.c")
 dyn.load('transpose_flip.so')
-m2 <- .Call("transpose_flip",m, package="oce")
+r1 <- .Call("transpose_flip", m)
 mt <- t(m)
-m3 <- mt[, seq.int(dim(mt)[2], 1, -1)]
-m3[m3==0] <- NA
+r2 <- mt[, seq.int(dim(mt)[2], 1, -1)]
+r2[r2==0] <- NA
 print("m:")
 m
-print("mt:")
-mt
-print("m2:")
-m2
-print("m3:")
-m3
+print("r1:")
+r1
+print("r2:")
+r2
+
 */
 
-// the macros differ because res is transposed so #rows is ncol.
-#define ijm(i, j) ((i) + (nrow) * (j)) // normal indexing (see e.g. gradient.c)
-#define ijres(i, j) ((i) + (ncol) * (j)) // transposed indexing
+// Macros differ because res is transposed so #rows is ncol.
+#define ij_m(i, j) ((i) + (nrow) * (j)) // normal indexing (see e.g. gradient.c)
+#define ij_res(i, j) ((i) + (nrow_res) * (j)) // transposed indexing
 
 // FIXME: the method is wrong, including macros above and code below
 
@@ -38,26 +37,29 @@ SEXP transpose_flip(SEXP m)
   double *mp = REAL(m); // FIXME: only works for real matrices
 
   SEXP res;
-  PROTECT(res = allocMatrix(REALSXP, ncol, nrow)); // notice row-col flip
+  int nrow_res = ncol;
+  int ncol_res = nrow;
+  PROTECT(res = allocMatrix(REALSXP, nrow_res, ncol_res));
   double *resp = REAL(res);
 
   // Transpose
   Rprintf("transpose:\n");
   for (int i = 0; i < nrow; i++) {
     for (int j = 0; j < ncol; j++) {
-      Rprintf("i %d, j %d, ijres(i,j) %d, ijm(j, i) %d\n", i, j, ijres(i,j), ijm(j,i));
-      resp[ijres(j, i)] = mp[ijm(i, j)];
+      Rprintf("i %d, j %d, ij_res(i,j) %d, ij_m(j, i) %d\n",
+	  i, j, ij_res(i,j), ij_m(j,i));
+      resp[ij_res(j, i)] = mp[ij_m(i, j)];
     }
   }
   // Flip in second dimension
   Rprintf("flip:\n");
-  for (int i = 0; i < nrow; i++) {
-    for (int j = 0; j < ncol; j++) {
-      Rprintf("i %d, j %d, ijres(i,j) %d, ijres(i,ncol-j-1) %d\n",
-	  i, j, ijres(i,j), ijres(i, ncol-j-1));
-      double tmp = resp[ijres(i, j)];
-      resp[ijres(i, j)] = resp[ijres(i, ncol-j-1)];
-      resp[ijres(i, ncol-j-1)] = tmp;
+  for (int i = 0; i < nrow_res; i++) {
+    for (int j = 0; j < ncol_res; j++) {
+      Rprintf("i %d, j %d, ij_res(i,j) %d, ij_res(i,ncol_res-j-1) %d\n",
+	  i, j, ij_res(i,j), ij_res(i, ncol_res-j-1));
+      double tmp = resp[ij_res(i, j)];
+      resp[ij_res(i, j)] = resp[ij_res(i, ncol_res-j-1)];
+      resp[ij_res(i, ncol_res-j-1)] = tmp;
     }
   }
   Rprintf("FLIP NOT WORKING\n");
