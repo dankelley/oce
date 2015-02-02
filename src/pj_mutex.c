feb398bc4fd644b793632a92a8f12baecc510d83
Dan Kelley <kelley.dan@gmail.com>
2015-02-02 14:58:51 -0400

    windows build

--- src/pj_mutex.c
+++ src/pj_mutex.c
@@ -34,7 +34,7 @@
 #include "projects.h"
 PJ_CVSID("$Id: pj_transform.c 1504 2009-01-06 02:11:57Z warmerdam $")
 #else
-#include <proj_api.h>
+#include "proj_api.h"
 #endif
 
 /* on win32 we always use win32 mutexes, even if pthreads are available */
