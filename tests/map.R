53714a1a50fc1f045199954492bb1c8fb1e65e0e
Dan Kelley <kelley.dan@gmail.com>
2015-06-25 18:38:03 -0300

    lonlat2utm() bug fix (issue 676)

--- tests/map.R
+++ tests/map.R
@@ -1,14 +1,11 @@
 library(oce)
 
-## UTM (numbers come from a landsat-8 image).
-longitude <- -59.56594
-latitude <- 45.62391
-easting <- 767700.000
-northing <- 5058000.000
-zone <- 20
+## The tests below employ the UL,UR,LL,LR bounding box for the two Landsat-8
+## images whose metadata are listed next. Note that these cover north/south
+## hemispheres, and two different zones. Probably other tests should be done,
+## but the success of these is a good sign that issue 676 has been solved.
 
 ## LANDSAT_SCENE_ID = "LC82170632015124LGN00"
-## ...
 ## CORNER_UL_LAT_PRODUCT = -3.28874
 ## CORNER_UL_LON_PRODUCT = -40.27900
 ## CORNER_UR_LAT_PRODUCT = -3.28927
@@ -25,14 +22,37 @@ zone <- 20
 ## CORNER_LL_PROJECTION_Y_PRODUCT = -596100.000
 ## CORNER_LR_PROJECTION_X_PRODUCT = 585900.000
 ## CORNER_LR_PROJECTION_Y_PRODUCT = -596100.000
-## ...
 ## UTM_ZONE = 24
-longitude <- -40.27900                 # UL
-latitude <- -3.28874                   # UL
-easting <- 357900.000                  # UL
-northing <- -363600.000                # UL 
-zone <- 24
+##
+## LANDSAT_SCENE_ID = "LC80070292015029LGN00"
+## CORNER_UL_LAT_PRODUCT = 45.66729
+## CORNER_UL_LON_PRODUCT = -64.12716
+## CORNER_UR_LAT_PRODUCT = 45.65756
+## CORNER_UR_LON_PRODUCT = -61.13114
+## CORNER_LL_LAT_PRODUCT = 43.53138
+## CORNER_LL_LON_PRODUCT = -64.08660
+## CORNER_LR_LAT_PRODUCT = 43.52235
+## CORNER_LR_LON_PRODUCT = -61.19836
+## CORNER_UL_PROJECTION_X_PRODUCT = 412200.000
+## CORNER_UL_PROJECTION_Y_PRODUCT = 5057700.000
+## CORNER_UR_PROJECTION_X_PRODUCT = 645600.000
+## CORNER_UR_PROJECTION_Y_PRODUCT = 5057700.000
+## CORNER_LL_PROJECTION_X_PRODUCT = 412200.000
+## CORNER_LL_PROJECTION_Y_PRODUCT = 4820400.000
+## CORNER_LR_PROJECTION_X_PRODUCT = 645600.000
+## CORNER_LR_PROJECTION_Y_PRODUCT = 4820400.000
+## UTM_ZONE = 20
 
+
+longitude <- c(-40.27900, -38.22680, -40.28255, -38.22465,
+               -64.12716, -61.13114, -64.08660, -61.19836)
+latitude <- c(-3.28874,-3.28927, -5.39159, -5.39245,
+               45.66729, 45.65756, 43.53138, 43.52235)
+easting <- c(357900.000, 585900.000, 357900.000, 585900.000,
+             412200.000, 645600.000, 412200.000, 645600.000)
+northing <- c(-363600.000, -363600.000, -596100.000, -596100.000,
+              5057700.000, 5057700.000, 4820400.000, 4820400.000)
+zone <- c(rep(24, 4), rep(20, 4))
 
 ## Projections: utm
 lonlat <- utm2lonlat(easting, northing, zone, "N")
