68bbb101098bd8ecfa2966ec3d312f96758836f0
Dan Kelley <kelley.dan@gmail.com>
2015-07-04 19:26:42 -0300

    ctdTrim() tested in build tests

--- tests/ctd.R
+++ tests/ctd.R
@@ -15,7 +15,20 @@ stopifnot(all.equal.numeric(ctd[["salinity"]], ctd_l[["salinity"]]))
 stopifnot(all.equal.numeric(ctd[["temperature"]], ctd_l[["temperature"]]))
 stopifnot(all.equal.numeric(ctd[["pressure"]], ctd_l[["pressure"]]))
 
-## 2. alter metadata
+## 2. trimming.
+## NOTE: this is brittle to changes in data(ctd), but that's a good thing, becausing
+## changing the dataset should be done only when really necessary, e.g. the July 2015
+## transition to use ITS-90 based temperature.
+data(ctd)
+scanRange <- range(ctd[['scan']])
+newScanRange <- c(scanRange[1] + 20, scanRange[2] - 20)
+ctdTrimmed <- ctdTrim(ctd, "scan", parameters=newScanRange)
+stopifnot(all.equal.numeric(ctdTrimmed[["scan"]][1:3], c(150,151,152)))
+stopifnot(all.equal.numeric(ctdTrimmed[["salinity"]][1:3], c(30.8882,30.9301,30.8928)))
+stopifnot(all.equal.numeric(ctdTrimmed[["pressure"]][1:3], c(6.198,6.437,6.770)))
+stopifnot(all.equal.numeric(ctdTrimmed[["temperature"]][1:3], c(11.734383747900503536,11.630308725905782907,11.4245581060545475790))) 
+
+## 3. alter metadata
 ctd[["longitude"]] <- 1
 stopifnot(all.equal.numeric(ctd[["longitude"]], 1, 1e-5))
 ctd[["latitude"]] <- 2
@@ -27,7 +40,7 @@ stopifnot(all.equal.numeric(ctd[["salinity"]], S+1, 1e-5))
 top <- subset(ctd, pressure < 5)
 stopifnot(max(top[['pressure']]) < 5)
 
-## 3. GSW
+## 4. GSW
 SP <- 35
 t <- 10
 p <- 1000
