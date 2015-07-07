9360ac7a5453f7bec5d83fd2ffe5267b7ea89336
Dan Kelley <kelley.dan@gmail.com>
2015-07-07 09:15:13 -0300

    rgdal test

--- tests/map.R
+++ tests/map.R
@@ -63,10 +63,13 @@ stopifnot(all.equal(utm$easting, easting, tolerance=1e-5))
 stopifnot(all.equal(utm$northing, northing, tolerance=1e-5))
 stopifnot(all.equal(utm$zone, zone, tolerance=1e-5))
 
-## "cs" is near Cape Split, in the Bay of Fundy
-cs <- list(longitude=-64.4966,latitude=45.3346)
-xy <- lonlat2map(cs$longitude, cs$latitude, "+proj=merc")
-cs2 <- map2lonlat(xy$x, xy$y)
-stopifnot(all.equal(cs, cs2, tolerance=1e-6)) # on 64bit machine can go to 1e-15
+## Can only test lonlat2map() if the rgdal package is available.
+if (requireNamespace("rgdal", quietly=TRUE)) {
+    ## "cs" is near Cape Split, in the Bay of Fundy
+    cs <- list(longitude=-64.4966,latitude=45.3346)
+    xy <- lonlat2map(cs$longitude, cs$latitude, "+proj=merc")
+    cs2 <- map2lonlat(xy$x, xy$y)
+    stopifnot(all.equal(cs, cs2, tolerance=1e-6)) # on 64bit machine can go to 1e-15
+}
 
 
