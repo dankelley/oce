17333e72a80459174d469dd5bc3c9b1eb36d8626
Dan Kelley <kelley.dan@gmail.com>
2015-07-12 09:29:06 -0300

    rsk

--- tests/rsk.R
+++ tests/rsk.R
@@ -7,29 +7,30 @@ file <- system.file("extdata", "sample.rsk.gz", package="oce")
 ## supplied (i.e. NA) and (b) pressureAtmospheric a number.  Thus we have cases
 ## I.a, I.b, II.a, II.b, III.a and III.b below.
 tmp <- tempfile(fileext=".rsk")
-R.utils::decompressFile(file, destname=tmp, ext="gz", FUN=gzfile, remove=FALSE)
-## I.a
-rsk <- read.rsk(tmp, patm=TRUE)
-ctd <- as.ctd(rsk)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]))
-## I.b
-rsk <- read.rsk(tmp, patm=TRUE)
-ctd <- as.ctd(rsk, pressureAtmospheric=1)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-1))
-## II.a
-rsk <- read.rsk(tmp, patm=FALSE)
-ctd <- as.ctd(rsk)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-10.1325))
-## II.b
-rsk <- read.rsk(tmp, patm=FALSE)
-ctd <- as.ctd(rsk, pressureAtmospheric=1)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-10.1325-1))
-## III.a
-rsk <- read.rsk(tmp, patm=10)
-ctd <- as.ctd(rsk)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]))
-## III.b
-rsk <- read.rsk(tmp, patm=10)
-ctd <- as.ctd(rsk, pressureAtmospheric=1)
-stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-1))
-
+if (requireNamespace("R.utils")) {
+    R.utils::decompressFile(file, destname=tmp, ext="gz", FUN=gzfile, remove=FALSE)
+    ## I.a
+    rsk <- read.rsk(tmp, patm=TRUE)
+    ctd <- as.ctd(rsk)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]))
+    ## I.b
+    rsk <- read.rsk(tmp, patm=TRUE)
+    ctd <- as.ctd(rsk, pressureAtmospheric=1)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-1))
+    ## II.a
+    rsk <- read.rsk(tmp, patm=FALSE)
+    ctd <- as.ctd(rsk)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-10.1325))
+    ## II.b
+    rsk <- read.rsk(tmp, patm=FALSE)
+    ctd <- as.ctd(rsk, pressureAtmospheric=1)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-10.1325-1))
+    ## III.a
+    rsk <- read.rsk(tmp, patm=10)
+    ctd <- as.ctd(rsk)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]))
+    ## III.b
+    rsk <- read.rsk(tmp, patm=10)
+    ctd <- as.ctd(rsk, pressureAtmospheric=1)
+    stopifnot(all.equal.numeric(ctd[["pressure"]], rsk[["pressure"]]-1))
+}
