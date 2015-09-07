be8fa2b5545c2855eaba3f5204c27f982fb93be6
Dan Kelley <kelley.dan@gmail.com>
2015-09-07 08:46:23 -0300

    cm code/data reworking in progress

---
+++ create_data/cm/explore_cm.R
@@ -0,0 +1,42 @@
+## This script requires a certain data file.
+library(oce)
+source("~/src/oce/R/cm.R")
+file <- "/data/archive/sleiwex/2008/moorings/m11/cm/interocean_0811786/manufacturer/cm_interocean_0811786.s4a.tab"
+filename <- file
+sep <- "\t"
+lines <- readLines(file, n=20)
+headerEnd <- grep("Sample", lines)
+if (length(headerEnd) == 0) stop("Cannot find header in first 20 lines; no lines contain 'Sample'")
+if (length(headerEnd) > 1) stop("Cannot find header in first 20 lines; two lines contain 'Sample'")
+## Get names of columns. I have NO idea whether these are fixed names.
+names <- strsplit(lines[headerEnd], "\t")[[1]]
+names <- gsub("^ *", "", gsub(" *$", "", names))
+names
+## Make names be more sensible
+## Read data. Must use fill=TRUE because not all lines have the same number of data
+d <- read.table(file, sep=sep, fill=TRUE, skip=headerEnd+1, header=FALSE, stringsAsFactors=FALSE)
+##d <- read.csv("s4.csv", skip=headerEnd+1, header=FALSE, stringsAsFactors=FALSE)
+dim <- dim(d)
+## Chop last two lines, which contain statistical summaries
+d <- d[seq.int(1, dim(d)[1]-2),]
+names(d) <- names
+## Need to construct times because the day string only occurs in first line and day starts
+t0 <- as.POSIXct(paste(d[1,2], d[1,3]), format="%m/%d/%Y %H:%M:%S", tz="UTC")
+t1 <- as.POSIXct(paste(d[1,2], d[2,3]), format="%m/%d/%Y %H:%M:%S", tz="UTC")
+dt <- as.numeric(t1) - as.numeric(t0)
+time <- seq(t0, by=dt, length.out=dim(d)[1])
+
+## Do not bother trying to decode all of the columns. The sample dataset has
+## several columns with duplicate names, several unnamed columns, and columns
+## whose names mean very little to me. It also has extraneous stuff that
+## doesn't belong in a data file (e.g. apparently, computed progressive-vector
+## data). I am just going to grab what seems to be a minimal set of known
+## quantities. I'm retaining the code above in case a user can decode other
+## columns.
+rval <- new("cm", filename=filename, time=time,
+            u=d$Veast/100, v=d$Vnorth/100,
+            salinity=d$Sal, temperature=d[["T-Temp"]], depth=d$Depth)
+rval
+summary(rval)
+plot(rval)
+
