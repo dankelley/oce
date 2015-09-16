6c482a4b77fcabea52fefb0e282eb231ee7619e5
Dan Kelley <kelley.dan@gmail.com>
2015-09-16 15:40:08 -0300

    travis

--- tests/testthat/test_map.R
+++ tests/testthat/test_map.R
@@ -4,6 +4,7 @@ library(oce)
 context("Map and geod calculations")
 
 if (requireNamespace("rgdal", quietly=TRUE)) {
+
   test_that("utm2lonlat() on some points known from Landsat metadata", {
             longitude <- c(-40.27900, -38.22680, -40.28255, -38.22465,
                            -64.12716, -61.13114, -64.08660, -61.19836)
@@ -58,53 +59,10 @@ if (requireNamespace("rgdal", quietly=TRUE)) {
             cs <- list(longitude=-64.4966,latitude=45.3346)
             xy <- lonlat2map(cs$longitude, cs$latitude, "+proj=merc")
             cs2 <- map2lonlat(xy$x, xy$y)
-            expect_equal(cs, cs2, tolerance=1e-6) # on 64bit machine can go to 1e-15})
+            expect_equal(cs, cs2, tolerance=1e-6)})
 
   test_that("geodDist()", {
             d <- geodDist(10, 45, 10, 46)
             expect_equal(d, 111.1415, tolerance=1e-4)})
-
-  ## The tests below employ the UL,UR,LL,LR bounding box for the two Landsat-8
-  ## images whose metadata are listed next. Note that these cover north/south
-  ## hemispheres, and two different zones. Probably other tests should be done,
-  ## but the success of these is a good sign that issue 676 has been solved.
-
-  ## LANDSAT_SCENE_ID = "LC82170632015124LGN00"
-  ## CORNER_UL_LAT_PRODUCT = -3.28874
-  ## CORNER_UL_LON_PRODUCT = -40.27900
-  ## CORNER_UR_LAT_PRODUCT = -3.28927
-  ## CORNER_UR_LON_PRODUCT = -38.22680
-  ## CORNER_LL_LAT_PRODUCT = -5.39159
-  ## CORNER_LL_LON_PRODUCT = -40.28255
-  ## CORNER_LR_LAT_PRODUCT = -5.39245
-  ## CORNER_LR_LON_PRODUCT = -38.22465
-  ## CORNER_UL_PROJECTION_X_PRODUCT = 357900.000
-  ## CORNER_UL_PROJECTION_Y_PRODUCT = -363600.000
-  ## CORNER_UR_PROJECTION_X_PRODUCT = 585900.000
-  ## CORNER_UR_PROJECTION_Y_PRODUCT = -363600.000
-  ## CORNER_LL_PROJECTION_X_PRODUCT = 357900.000
-  ## CORNER_LL_PROJECTION_Y_PRODUCT = -596100.000
-  ## CORNER_LR_PROJECTION_X_PRODUCT = 585900.000
-  ## CORNER_LR_PROJECTION_Y_PRODUCT = -596100.000
-  ## UTM_ZONE = 24
-  ##
-  ## LANDSAT_SCENE_ID = "LC80070292015029LGN00"
-  ## CORNER_UL_LAT_PRODUCT = 45.66729
-  ## CORNER_UL_LON_PRODUCT = -64.12716
-  ## CORNER_UR_LAT_PRODUCT = 45.65756
-  ## CORNER_UR_LON_PRODUCT = -61.13114
-  ## CORNER_LL_LAT_PRODUCT = 43.53138
-  ## CORNER_LL_LON_PRODUCT = -64.08660
-  ## CORNER_LR_LAT_PRODUCT = 43.52235
-  ## CORNER_LR_LON_PRODUCT = -61.19836
-  ## CORNER_UL_PROJECTION_X_PRODUCT = 412200.000
-  ## CORNER_UL_PROJECTION_Y_PRODUCT = 5057700.000
-  ## CORNER_UR_PROJECTION_X_PRODUCT = 645600.000
-  ## CORNER_UR_PROJECTION_Y_PRODUCT = 5057700.000
-  ## CORNER_LL_PROJECTION_X_PRODUCT = 412200.000
-  ## CORNER_LL_PROJECTION_Y_PRODUCT = 4820400.000
-  ## CORNER_LR_PROJECTION_X_PRODUCT = 645600.000
-  ## CORNER_LR_PROJECTION_Y_PRODUCT = 4820400.000
-  ## UTM_ZONE = 20
 }
 
