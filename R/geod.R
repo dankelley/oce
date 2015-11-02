53428fa2886f3d4e57ec8fec3b86f37eb09d59b6
Dan Kelley <kelley.dan@gmail.com>
2015-11-02 08:56:04 -0400

    geod* functions updated for proper names

--- R/geod.R
+++ R/geod.R
@@ -2,15 +2,15 @@
 
 ## This is possibly useful, possibly not. The method changed in Oct, 2015.
 ## See the docs for methodology.
-geodXy <- function(lon, lat, lon.ref=0, lat.ref=0, rotate=0)
+geodXy <- function(longitude, latitude, longitudeRef=0, latitudeRef=0, rotate=0)
 {
     a <- 6378137.00          # WGS84 major axis
     f <- 1/298.257223563     # WGS84 flattening parameter
-    if (missing(lon) || missing(lat)) stop("must provide lon and lat")
-    n <- length(lon)
-    if (length(lat) != n) stop("lon and lat vectors of unequal length")
-    xy  <- .C("geod_xy", as.integer(n), as.double(lat), as.double(lon),
-              as.double(lat.ref), as.double(lon.ref), as.double(a), as.double(f),
+    if (missing(longitude) || missing(latitude)) stop("must provide longitude and latitude")
+    n <- length(longitude)
+    if (length(latitude) != n) stop("longitude and latitude vectors of unequal length")
+    xy  <- .C("geod_xy", as.integer(n), as.double(latitude), as.double(longitude),
+              as.double(latitudeRef), as.double(longitudeRef), as.double(a), as.double(f),
               x = double(n), y = double(n), PACKAGE = "oce")
     if (rotate != 0) {
         S <- sin(rotate * pi / 180)
@@ -34,10 +34,10 @@ geodXy <- function(lon, lat, lon.ref=0, lat.ref=0, rotate=0)
 #'
 #' @param x value of x in metres, as given by \code{\link{geodXy}}
 #' @param y value of y in metres, as given by \code{\link{geodXy}}
-#' @param lon.ref reference longitude, as supplied to \code{\link{geodXy}}
-#' @param lat.ref reference latitude, as supplied to \code{\link{geodXy}}
+#' @param longitudeRef reference longitude, as supplied to \code{\link{geodXy}}
+#' @param latitudeRef reference latitude, as supplied to \code{\link{geodXy}}
 #' @return a data frame containing \code{longitude} and \code{latitude}
-geodXyInverse <- function(x, y, lon.ref=0, lat.ref=0)
+geodXyInverse <- function(x, y, longitudeRef=0, latitudeRef=0)
 {
     a <- 6378137.00          # WGS84 major axis
     f <- 1/298.257223563     # WGS84 flattening parameter
@@ -45,50 +45,50 @@ geodXyInverse <- function(x, y, lon.ref=0, lat.ref=0)
     n <- length(x)
     if (length(y) != n) stop("x and y vectors of unequal length")
     ll <- .C("geod_xy_inverse", as.integer(n), as.double(x), as.double(y),
-             as.double(lat.ref), as.double(lon.ref), as.double(a), as.double(f),
+             as.double(latitudeRef), as.double(longitudeRef), as.double(a), as.double(f),
              longitude = double(n), latitude = double(n), PACKAGE = "oce")
     data.frame(longitude=ll$longitude, latitude=ll$latitude)
 }
 
-geodDist <- function (lon1, lat1=NULL, lon2=NULL, lat2=NULL, alongPath=FALSE)
+geodDist <- function (longitude1, latitude1=NULL, longitude2=NULL, latitude2=NULL, alongPath=FALSE)
 {
     a <- 6378137.00          # WGS84 major axis
     f <- 1/298.257223563     # WGS84 flattening parameter
-    if (inherits(lon1, "section")) {
-        section <- lon1
-        lat <- section[["latitude", "byStation"]]
-        lon <- section[["longitude", "byStation"]]
+    if (inherits(longitude1, "section")) {
+        section <- longitude1
+        latitude <- section[["latitude", "byStation"]]
+        longitude <- section[["longitude", "byStation"]]
         if (alongPath) {
-            res <- .Call("geoddist_alongpath", lat, lon, a, f) / 1000
+            res <- .Call("geoddist_alongpath", latitude, longitude, a, f) / 1000
         } else {
             n <- length(section@data$station)
             res <- .Call("geoddist",
-                         as.double(rep(lat[1], length.out=n)), as.double(rep(lon[1], length.out=n)),
-                         as.double(lat), as.double(lon),
-                         as.double(a), as.double(f), as.double(1), as.double(1),
-                         dist = double(n)) / 1000
+                         as.double(rep(latitude[1], length.out=n)),
+                         as.double(rep(longitude[1], length.out=n)),
+                         as.double(latitude), as.double(longitude),
+                         as.double(a), as.double(f)) / 1000
         }
     } else {
         if (alongPath) {
-            res <- .Call("geoddist_alongpath", lat1, lon1, a, f) / 1000
+            res <- .Call("geoddist_alongpath", latitude1, longitude1, a, f) / 1000
         } else {
-            n1 <- length(lat1)
-            if (length(lon1) != n1)
-                stop("lat1 and lon1 must be vectors of the same length")
-            n2 <- length(lat2)
-            if (length(lon2) != n2)
-                stop("lat2 and lon2 must be vectors of the same length")
-            if (n2 < n1) { # copy first (lat2, lon2)
-                lat2 <- rep(lat2[1], n1)
-                lon2 <- rep(lon2[1], n1)
+            n1 <- length(latitude1)
+            if (length(longitude1) != n1)
+                stop("latitude1 and longitude1 must be vectors of the same length")
+            n2 <- length(latitude2)
+            if (length(longitude2) != n2)
+                stop("latitude2 and longitude2 must be vectors of the same length")
+            if (n2 < n1) { # copy first (latitude2, longitude2)
+                latitude2 <- rep(latitude2[1], n1)
+                longitude2 <- rep(longitude2[1], n1)
             } else { # trim
-                lat2 <- lat2[1:n1]
-                lon2 <- lon2[1:n1]
+                latitude2 <- latitude2[1:n1]
+                longitude2 <- longitude2[1:n1]
             }
             res <- .Call("geoddist",
-                      as.double(lat1), as.double(lon1),
-                      as.double(lat2), as.double(lon2),
-                      as.double(a), as.double(f), as.double(1), as.double(1)) / 1000
+                      as.double(latitude1), as.double(longitude1),
+                      as.double(latitude2), as.double(longitude2),
+                      as.double(a), as.double(f)) / 1000
         }
     }
     res
