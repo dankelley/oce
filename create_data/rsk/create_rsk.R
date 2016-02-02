f8ac93eda651cbd9cee5e85659c69c220e9b078d
Clark Richards <clark.richards@gmail.com>
2016-02-01 21:15:26 -0500

    trim new rsk dataset to remove surface cond

--- create_data/rsk/create_rsk.R
+++ create_data/rsk/create_rsk.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 library(oce)
 raw <- read.oce('060130_20150904_1159.rsk')
-focus <- structure(c(1441381020.7302, 1441381482.90486), class = c("POSIXct", 
+focus <- structure(c(1441381041, 1441381483), class = c("POSIXct", 
 "POSIXt"), tzone = "UTC")
 rsk <- subset(raw, focus[1] <= time & time <= focus[2])
 save(file='rsk.rda', rsk)
