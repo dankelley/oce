dcd623be4c4d49971b3237b138e4c4ed47aba0a2
Clark Richards <clark.richards@gmail.com>
2016-02-01 21:50:05 -0500

    add rsk metadata

--- create_data/rsk/create_rsk.R
+++ create_data/rsk/create_rsk.R
@@ -1,6 +1,11 @@
 rm(list=ls())
 library(oce)
 raw <- read.oce('060130_20150904_1159.rsk')
+raw <- oceSetMetadata(raw, 'longitude', -(56 + 26.232/60))
+raw <- oceSetMetadata(raw, 'latitude', 73 + 13.727/60)
+raw <- oceSetMetadata(raw, 'station', 'C18')
+raw <- oceSetMetadata(raw, 'ship', 'Ault')
+raw <- oceSetMetadata(raw, 'institute', 'Ocean Research Project')
 focus <- structure(c(1441381041, 1441381483), class = c("POSIXct", 
 "POSIXt"), tzone = "UTC")
 rsk <- subset(raw, focus[1] <= time & time <= focus[2])
