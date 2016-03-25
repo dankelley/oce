88cc8505849f144717055f80b195044d8789e66d
Dan Kelley <kelley.dan@gmail.com>
2016-03-25 13:13:04 -0300

    data(section) and as.ctd() do flags better

--- tests/testthat/test_section.R
+++ tests/testthat/test_section.R
@@ -9,10 +9,33 @@ test_that("data(section) has not altered", {
           expect_equal(124, length(section@data$station))
 })
 
+test_that("as.section() data-quality flags", {
+          data(section)
+
+
+          ## The below is also in ../../create_data/section/check_section.R, and it would be
+          ## smart to update both at the same time.
+          stn2 <- section[['station', 2]]
+          twos <- rep(2, 16)
+          expect_equal(stn2@metadata$flags$salinity, c(2,3,2,2,2,3,2,2,2,2,2,2,2,2,2,2))
+          ## there are no flags on temperature or pressure
+          expect_equal(stn2@metadata$flags$oxygen, twos)
+          expect_equal(stn2@metadata$flags$silicate, twos)
+          expect_equal(stn2@metadata$flags$nitrate, twos)
+          expect_equal(stn2@metadata$flags$nitrite, twos)
+          expect_equal(stn2@metadata$flags$phosphate, c(2,2,2,2,3,2,2,2,2,2,2,2,2,2,2,2))
+
+
+
+
+
+})
+
 test_that("section station extraction", {
           data(section)
           expect_equal(length(section@data$station), length(section@data$station))
 })
+
 
 test_that("as.section() works with names of CTD objects", {
           data(ctd)
