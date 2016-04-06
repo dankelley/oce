02316fa043717f3f175befeadaad3275a2096b88
Dan Kelley <kelley.dan@gmail.com>
2016-04-06 19:14:18 -0300

    more flag tests

--- tests/testthat/test_flags.R
+++ tests/testthat/test_flags.R
@@ -9,7 +9,13 @@ test_that("handleFLags works with ctd data", {
           ##cat("ctd salinity: orig had", sum(is.na(ctd[['salinity']])), "NA values; new has",
           ##    sum(is.na(ctdNew[['salinity']])), "\n")
           expect_equal(sum(is.na(ctd[["salinity"]])), 0)
-          expect_equal(sum(is.na(ctdNew[["salinity"]])), 2)
+          nbad <- sum(ctd[['salinityFlag']] != 2)
+          expect_equal(2, nbad)
+          ## test replacement via function
+          f <- function(object) rep(30, length.out=length(object[['salinity']]))
+          ctdNew2 <- handleFlags(ctd, flags=list(salinity=4:5), actions=list(salinity=f))
+          expect_equal(sum(ctdNew[['salinity']]==30, na.rm=TRUE),
+                       sum(ctd[['salinityFlag']] == 4 | ctd[['salinityFlag']] == 5, na.rm=TRUE))
 })
 
 test_that("handleFLags works with argo data", {
@@ -20,5 +26,10 @@ test_that("handleFLags works with argo data", {
           ##    sum(is.na(argoNew[['salinity']])), "\n")
           expect_equal(sum(is.na(argo[["salinity"]])), 90)
           expect_equal(sum(is.na(argoNew[["salinity"]])), 110)
+          ## test replacement via function
+          f <- function(object) rep(30, length.out=length(object[['salinity']]))
+          argoNew2 <- handleFlags(argo, flags=list(salinity=4:5), actions=list(salinity=f))
+          expect_equal(sum(argoNew2[['salinity']]==30, na.rm=TRUE),
+                       sum(argo[['salinityFlag']] == 4 | argo[['salinityFlag']] == 5, na.rm=TRUE))
 })
 
