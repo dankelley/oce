7786333e8145fcb37c564f1de90942e700120bf8
Dan Kelley <kelley.dan@gmail.com>
2016-08-03 12:07:00 -0300

    fix 5-min-old typo

--- R/ctd.R
+++ R/ctd.R
@@ -2921,7 +2921,7 @@ setMethod(f="plot",
                           if (!is.null(x@metadata$startTime) && 4 < nchar(x@metadata$startTime, "bytes"))
                               mtext(format(x@metadata$startTime, "%Y-%m-%d %H:%S"),
                                     side=3, adj=1, cex=0.8*par("cex"), line=1.125)
-                          else (!is.null(x@data$time))
+                          else if (!is.null(x@data$time))
                               mtext(format(x@data$time[1], "%Y-%m-%d %H:%S"),
                                     side=3, adj=1, cex=0.8*par("cex"), line=1.125)
                       }
