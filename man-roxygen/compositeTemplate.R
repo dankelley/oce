d3bf0efebe71fbe704ed7ca7be396047442c904e
Dan Kelley <kelley.dan@gmail.com>
2016-08-13 11:56:06 -0300

    composite,amsr-method added

---
+++ man-roxygen/compositeTemplate.R
@@ -0,0 +1,10 @@
+#' @description Items within the \code{data} slots of the
+#' objects that are supplied as arguments are averaged in a way that
+#' makes sense for the object class, i.e. taking into account
+#' the particular bad-data codes of that particular class.
+#'
+#' @param object An object inheriting from \link{oce-class}.
+#' @param ... Other oce objects of the same inheritance as \code{object}.
+#'
+#' @family functions that create composite objects
+
