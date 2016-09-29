6e31c9f5ebbbc354d69d67f98c10ed4d598f7c24
Dan Kelley <kelley.dan@gmail.com>
2016-09-29 16:45:08 -0300

    ldc_rdi_2 speedup by factor 34

--- src/ldc_rdi_2.c
+++ src/ldc_rdi_2.c
@@ -13,43 +13,23 @@ dyn.load('ldc_rdi_2.so')
 filename <- "/data/archive/sleiwex/2008/moorings/m09/adp/rdi_2615/raw/adp_rdi_2615.000"
 ## next is 1.6s
 system.time(d <- read.adp(filename, 1, 10))
-# next is 15.2s using feof()
-# next is  6.9s using EOF
-system.time(np <- .Call("ldc_rdi_2", filename))
-expect_equal(np, 79134)
-message("got ", np, " profiles")
-buf<-readBin(file(filename,"rb"), "raw", 500000)
+# next is  5.5s using EOF; was 15.2s using feof()
+system.time(p <- .Call("ldc_rdi_2", filename))
+expect_equal(length(p), 79134)
+f <- file(filename, "rb")
+buf<-readBin(f, "raw", 500000)
+close(f)
 
-
-
-## for reference:
-## 79135 profiles
-## > buf[1835+0:3]
-## [1] 7f 7f 28 07
-
-## for reference:
-## > d <- read.adp(filename, 1, 10)
-##   ic 0  byte 127  check_sum 127
-##   ic 1  byte 127  check_sum 254
-##   ic 2  byte 40  check_sum 294
-##   ic 3  byte 7  check_sum 301
-##   ic 4  byte 0  check_sum 301
-##   ic 5  byte 6  check_sum 307
-##   ic 6  byte 18  check_sum 325
-##   ic 7  byte 0  check_sum 325
-##   ic 8  byte 77  check_sum 402
-##   ic 9  byte 0  check_sum 402
- 
 */
 
 
 /*
- * old method, for the file used for data(adp): 1.2s elapsed
+ * old method, for the file used for data(adp): 1.6s elapsed
  *
- * this method, same file, simply scanning: 9.9s elapsed [but I have
+ * this method, same file, simply scanning: 5.5s elapsed [but I have
  * some ideas for speedup, albeit with more alloc() complications.
  */
-SEXP ldc_rdi_2(SEXP filename)
+SEXP ldc_rdi_2(SEXP filename) // FIXME: add from,to args so we can look within
 {
   /* Locate Data Chunk for RDI
    *   buf = buffer
@@ -67,54 +47,74 @@ SEXP ldc_rdi_2(SEXP filename)
   FILE *fp = fopen(filenamestring, "rb");
   if (!fp)
     error("cannot open file '%s'\n", filenamestring);
-  SEXP res;
-  PROTECT(res = NEW_INTEGER(1));
-  int *pres = INTEGER_POINTER(res);
-  int lres = 0;
   int c, clast=0x00;
-  unsigned char byte1 = 0x7f;
-  unsigned char byte2 = 0x7f;
+  int byte1 = 0x7f;
+  int byte2 = 0x7f;
   unsigned short int check_sum, desired_check_sum;
   unsigned int bytes_to_check = 0;
   unsigned int cindex = 0; // character index
   clast = fgetc(fp);
-  if (clast == EOF) error("empty file '%s'", filenamestring);
   cindex++;
-  pres[0] = 0;
-  unsigned int matches = 0;
+  if (clast == EOF)
+    error("empty file '%s'", filenamestring);
+  // Growable buffer for ensemble starting points
+  unsigned long int nensembles = 100000;
+  int *ensembles;
+  unsigned long int iensemble = 0;
+  ensembles = (int *)Calloc((size_t)nensembles, int);
+  // Growable buffer for the present ensemble.
+  unsigned long int nebuf = 30000;
+  unsigned char *ebuf = (unsigned char *)Calloc((size_t)nebuf, unsigned char);
+
   unsigned int FIXME = 0;
   int b1, b2;
+  int last_start = 0;
+
   while (1) { // new 
     c = fgetc(fp); 
     if (c == EOF) break;
     cindex++;
     if (clast == byte1 && c == byte2) {
-      check_sum = byte1;
-      if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD a]\n", cindex-2, byte1, check_sum);
-      check_sum += byte2;
-      if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD b]\n", cindex-1, byte2, check_sum);
+      last_start = cindex - 1;
+      // The checksum includes the starting 0x7f, 0x7f pair, and also
+      // the two bytes that specify the number of bytes in the
+      // ensemble. So we start by adding these four bytes.
+      check_sum = (unsigned short int)byte1;
+      check_sum += (unsigned short int)byte2;
       b1 = fgetc(fp);
       if (b1 == EOF) break;
-      check_sum += (unsigned short int)b1;
-      if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD c]\n", cindex, b1, check_sum);
       cindex++;
+      check_sum += (unsigned short int)b1;
       b2 = fgetc(fp);
       if (b2 == EOF) break;
-      check_sum += (unsigned short int)b2;
-      if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD d]\n", cindex, b2, check_sum);
       cindex++;
-      bytes_to_check = (unsigned int)b1 + 256 * (unsigned int)b2; // we do the first 4 manually; see next 4 lines
-      if (FIXME < 5) Rprintf("bytes_to_check %d; matches=%d\n", bytes_to_check, matches);
-      // FIXME: consider reading all bytes in the chunk into a buffer,
-      // which may speed things up.
-      for (int ic = 4; ic < bytes_to_check; ic++) { // note starting index. we already have 4 bytes done
-	int ccheck = fgetc(fp);
-	if (ccheck == EOF)
-	  error("end of file while looking for a checksum");
-	cindex++;
-	check_sum += (unsigned short int)ccheck;
-	if (FIXME < 1 && (ic < 5 || ic > (bytes_to_check-5)))
-	  Rprintf("  cindex %d, ic %d, byte 0x%02x, check_sum %d [NEW METHOD]\n", cindex, ic, ccheck, check_sum);
+      check_sum += (unsigned short int)b2;
+      // Now we are ready to look at the rest of the bytes. Note that
+      // our loop starts at index 4, because we have already handled
+      // those 4 bytes of the ensemble (i.e. those 4 bytes are include
+      // in the bytes_to_check value that we now calculate).
+      bytes_to_check = (unsigned int)b1 + 256 * (unsigned int)b2;
+      if (bytes_to_check < 5) {
+	Free(ensembles);
+	Free(ebuf);
+	error("cannot decode the length of ensemble number %d", iensemble);
+      }
+      unsigned int bytes_to_read = bytes_to_check - 4; // check_sum has used first 4 bytes already
+
+      // Read the bytes in one operation, because fgetc() is too slow.
+      if (bytes_to_read > nebuf) {
+	  ebuf = (unsigned char *)Realloc(ebuf, bytes_to_read + 1000, unsigned char); // ask for extra space
+	  nebuf = bytes_to_read + 1000;
+      }
+      fread(ebuf, bytes_to_read, sizeof(unsigned char), fp);
+      cindex += bytes_to_check;
+      if (feof(fp)) {
+	Free(ensembles);
+	Free(ebuf);
+	error("end of file while looking for a checksum"); // this exits, I think, so we must Free() first
+      }
+      for (int ib = 0; ib < bytes_to_read; ib++) {
+	check_sum += (unsigned short int)ebuf[ib];
       }
       int cs1, cs2;
       cs1 = fgetc(fp);
@@ -123,17 +123,20 @@ SEXP ldc_rdi_2(SEXP filename)
       cs2 = fgetc(fp);
       if (cs2 == EOF) break;
       cindex++;
-      // new
       desired_check_sum = ((unsigned short int)cs1) | ((unsigned short int)(cs2 << 8));
       if (FIXME < 3) Rprintf("  icindex=%d, cs1=%d cs2=%d check_sum %d; desired_check_sum=%d\n",
 	  cindex, cs1, cs2, check_sum, desired_check_sum);
       if (check_sum == desired_check_sum) {
-	matches++;
-	if (FIXME < 5) Rprintf("   MATCH\n");
-      } else {
-	if (FIXME < 5) Rprintf("   NO MATCH\n");
-      }
-      R_CheckUserInterrupt(); // only check once per profile, for speed
+	if (iensemble >= nensembles) {
+	  //Rprintf("iensemble=%d : present  storage starts at 0x%x and can contain %d elements...\n", iensemble, ensembles, nensembles);
+	  ensembles = (int *)Realloc(ensembles, 2*nensembles, int);
+	  nensembles = 2 * nensembles;
+	  //Rprintf("            : upgraded storage starts at 0x%x and can contain %d elements...\n", ensembles, nensembles);
+	}
+	ensembles[iensemble] = last_start;
+	iensemble++;
+      } // if it doesn't match the check_sum, we just ignore it as a coincidence of a 0x7f 0x7f pair
+      R_CheckUserInterrupt(); // only check once per ensemble, for speed
       clast = c;
       FIXME++;
     }
@@ -142,10 +145,15 @@ SEXP ldc_rdi_2(SEXP filename)
     if (c == EOF) break;
     cindex++;
   }
-  pres[0] = matches;
-  UNPROTECT(1);
   fclose(fp);
-  Rprintf("DEBUG: ldc_rdi_2 is only returning # of profiles (and that's ok for data(adp) file)\n");
+  SEXP res;
+  PROTECT(res = NEW_INTEGER(iensemble));
+  int *pres = INTEGER_POINTER(res);
+  for (int i = 0; i < iensemble; i++)
+    pres[i] = ensembles[i];
+  Free(ensembles);
+  Free(ebuf);
+  UNPROTECT(1);
   return(res);
 }
 
