3de0b72d780be60f23a4412448307fd238f6c8c9
Dan Kelley <kelley.dan@gmail.com>
2016-09-29 14:56:43 -0300

    ldc_rdi_2.c steppingstone (2X speedup)

--- src/ldc_rdi_2.c
+++ src/ldc_rdi_2.c
@@ -7,11 +7,16 @@
 
 /*
 
+library(testthat)
 system("R CMD SHLIB ldc_rdi_2.c")
 dyn.load('ldc_rdi_2.so')
 filename <- "/data/archive/sleiwex/2008/moorings/m09/adp/rdi_2615/raw/adp_rdi_2615.000"
-d <- read.adp(filename, 1, 10)
-np <- .Call("ldc_rdi_2", filename)
+## next is 1.6s
+system.time(d <- read.adp(filename, 1, 10))
+# next is 15.2s using feof()
+# next is  6.9s using EOF
+system.time(np <- .Call("ldc_rdi_2", filename))
+expect_equal(np, 79134)
 message("got ", np, " profiles")
 buf<-readBin(file(filename,"rb"), "raw", 500000)
 
@@ -66,71 +71,58 @@ SEXP ldc_rdi_2(SEXP filename)
   PROTECT(res = NEW_INTEGER(1));
   int *pres = INTEGER_POINTER(res);
   int lres = 0;
-  unsigned char c, clast=0x00;
+  int c, clast=0x00;
   unsigned char byte1 = 0x7f;
   unsigned char byte2 = 0x7f;
   unsigned short int check_sum, desired_check_sum;
   unsigned int bytes_to_check = 0;
-  // // check that we can read a file
-  // while (1) {
-  //   c = fgetc(fp);
-  //   if (feof(fp))
-  //     break;
-  //   lres++;
-  // }
-  // pres[0] = lres;
   unsigned int cindex = 0; // character index
   clast = fgetc(fp);
-  if (feof(fp))
-    error("empty file '%s'", filenamestring);
+  if (clast == EOF) error("empty file '%s'", filenamestring);
   cindex++;
   pres[0] = 0;
   unsigned int matches = 0;
   unsigned int FIXME = 0;
-  unsigned char b1, b2;
+  int b1, b2;
   while (1) { // new 
     c = fgetc(fp); 
+    if (c == EOF) break;
     cindex++;
-    if (feof(fp))
-      break;
     if (clast == byte1 && c == byte2) {
       check_sum = byte1;
       if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD a]\n", cindex-2, byte1, check_sum);
       check_sum += byte2;
       if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD b]\n", cindex-1, byte2, check_sum);
-      unsigned char b1, b2;
       b1 = fgetc(fp);
+      if (b1 == EOF) break;
       check_sum += (unsigned short int)b1;
       if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD c]\n", cindex, b1, check_sum);
       cindex++;
-      if (feof(fp)) break;
       b2 = fgetc(fp);
+      if (b2 == EOF) break;
       check_sum += (unsigned short int)b2;
       if (FIXME < 3) Rprintf("  cindex %d, byte 0x%02x, check_sum %d [NEW METHOD d]\n", cindex, b2, check_sum);
       cindex++;
-      if (feof(fp)) break;
       bytes_to_check = (unsigned int)b1 + 256 * (unsigned int)b2; // we do the first 4 manually; see next 4 lines
       if (FIXME < 5) Rprintf("bytes_to_check %d; matches=%d\n", bytes_to_check, matches);
       // FIXME: consider reading all bytes in the chunk into a buffer,
       // which may speed things up.
       for (int ic = 4; ic < bytes_to_check; ic++) { // note starting index. we already have 4 bytes done
-	unsigned char ccheck = fgetc(fp);
+	int ccheck = fgetc(fp);
+	if (ccheck == EOF)
+	  error("end of file while looking for a checksum");
 	cindex++;
 	check_sum += (unsigned short int)ccheck;
 	if (FIXME < 1 && (ic < 5 || ic > (bytes_to_check-5)))
 	  Rprintf("  cindex %d, ic %d, byte 0x%02x, check_sum %d [NEW METHOD]\n", cindex, ic, ccheck, check_sum);
-	if (feof(fp))
-	  error("end of file while looking for a checksum");
       }
-      unsigned char cs1, cs2;
+      int cs1, cs2;
       cs1 = fgetc(fp);
+      if (cs1 == EOF) break;
       cindex++;
-      if (feof(fp))
-	break;
       cs2 = fgetc(fp);
+      if (cs2 == EOF) break;
       cindex++;
-      if (feof(fp))
-	break;
       // new
       desired_check_sum = ((unsigned short int)cs1) | ((unsigned short int)(cs2 << 8));
       if (FIXME < 3) Rprintf("  icindex=%d, cs1=%d cs2=%d check_sum %d; desired_check_sum=%d\n",
@@ -147,9 +139,8 @@ SEXP ldc_rdi_2(SEXP filename)
     }
     clast = c;
     c = fgetc(fp); 
+    if (c == EOF) break;
     cindex++;
-    if (feof(fp))
-      break;
   }
   pres[0] = matches;
   UNPROTECT(1);
