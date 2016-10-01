e5b63849ba0be5f0b11bfb49d52bf88afe1da65f
Dan Kelley <kelley.dan@gmail.com>
2016-10-01 10:53:27 -0300

    .

--- tests_private/adp-rdi-01.R
+++ tests_private/adp-rdi-01.R
@@ -6,12 +6,15 @@ library(oce)
 ## stop()
 
 files <- list.files("~/Dropbox/oce-working-notes/tests/adp-rdi", "*", recursive=TRUE, full.names=TRUE)
-#files <- '~/Dropbox/oce-working-notes/tests/adp-rdi/moored_workhorse_1200kHz/adp_rdi_1997.000'
+files <- '/Users/kelley/Dropbox/oce-working-notes/tests/adp-rdi/vmdas_workhorse_300kHz_01/ADCP061_000000.NMS' # multibyte string error
 for (file in files) {
     try({
+        cat("file '", file, "' ", sep="")
         if ("adp/rdi" == oceMagic(file)) {
-            cat("file '", file, "'\n", sep="")
+            cat(" is adp/rdi\n")
             d <- read.oce(file)
+        } else {
+            cat(" is not adp/rdi, so it is being skipped\n")
         }
     })
 }
