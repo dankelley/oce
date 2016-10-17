b6d1d33e4bc838d7a40a5d10496e7721d87fce9e
Dan Kelley <kelley.dan@gmail.com>
2016-10-17 07:58:52 -0300

    prevent some code-check warnings

--- tests_private/ctd_beaufort_sea.R
+++ tests_private/ctd_beaufort_sea.R
@@ -1,5 +1,5 @@
 library(oce)
-library(testthat)
+require(testthat)
 options(width=100)                     # makes summaries easier to read
 options(warn=2)                        # die on warning, to catch unrecognized SBE names
 years <- 2003:2012
@@ -10,9 +10,9 @@ if (!interactive()) png('ctd_beaufort_sea_%03d.png')
 for (i in seq_along(files)) {
     cat("\n##\n# file[", i, "]= '",  files[i], "'\n", sep="")
     d <- read.oce(files[i])
-    expect_false(is.na(d[['latitude']][1]))
-    expect_false(is.na(d[['longitude']][1]))
-    expect_true("temperature" %in% names(d[['data']]))
+    testthat::expect_false(is.na(d[['latitude']][1]))
+    testthat::expect_false(is.na(d[['longitude']][1]))
+    testthat::expect_true("temperature" %in% names(d[['data']]))
     summary(d)                         # so we can look ... hard to do, though
     plot(d)
 }
