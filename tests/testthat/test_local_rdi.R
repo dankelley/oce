1e2d5982d05239a83fa6f37c3d77790159111e01
Dan Kelley <kelley.dan@gmail.com>
2016-11-08 17:21:29 -0400

    fix 1/2h old bug

--- tests/testthat/test_local_rdi.R
+++ tests/testthat/test_local_rdi.R
@@ -1,10 +1,11 @@
 library(oce)
 context("read adp data")
 test_that("Teledyn/RDI", {
-          skip_on_cran()
-          beam <- read.oce("local_data/rdi", from=1, to=10, latitude=47.88126, longitude=-69.73433)
-          xyz <- beamToXyzAdp(beam)
-          adp <- xyzToEnuAdp(xyz, declination=-18.1)
-          ## FIXME: add some tests on the data here
+          if (1 == length(list.files(path=".", pattern="local_data"))) {
+              beam <- read.oce("local_data/rdi", from=1, to=10, latitude=47.88126, longitude=-69.73433)
+              xyz <- beamToXyzAdp(beam)
+              adp <- xyzToEnuAdp(xyz, declination=-18.1)
+              ## FIXME: add some tests on the data here
+          }
 })
 
