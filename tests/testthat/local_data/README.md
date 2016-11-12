a2ba9fa524386c839a720c8329745f11c720084a
Dan Kelley <kelley.dan@gmail.com>
2016-11-12 11:36:33 -0400

    test (a) local coastline, (b) adv_sontek

--- tests/testthat/local_data/README.md
+++ tests/testthat/local_data/README.md
@@ -8,4 +8,7 @@ dd if=/data/archive/sleiwex/2008/moorings/m05/adp/nortek_aqd2843/raw/adp_nortek_
 
 dd if=/data/archive/sleiwex/2008/moorings/m05/adv/nortek_1943/raw/adv_nortek_1943.vec of=adv_nortek_vector bs=40000 count=1
 
+dd if=/data/archive/sleiwex/2008/moorings/m03/adv/sontek_b373h/raw/adv_sontek_b373h.adr of=adv_sontek bs=40000 count=1
+
+
 ```
--- tests/testthat/test_local_adv.R
+++ tests/testthat/test_local_adv.R
@@ -1,11 +1,23 @@
 library(oce)
 context("read adv data")
-test_that("nortek_vector", {
+test_that("nortek vector", {
           if (1 == length(list.files(path=".", pattern="local_data"))) {
               beam <- read.oce("local_data/adv_nortek_vector", from=1, to=10,
                                latitude=47.87943, longitude=-69.72533)
               xyz <- beamToXyzAdv(beam)
-              adp <- xyzToEnuAdv(xyz)
+              enu <- xyzToEnuAdv(xyz)
+              ## FIXME: add some tests on the data here
+          }
+})
+
+test_that("sontek", {
+          if (1 == length(list.files(path=".", pattern="local_data"))) {
+              xyz <- read.adv.sontek.adr("local_data/adv_sontek", from=1, to=20,
+                                         latitude=47.87943, longitude=-69.72533)
+              expect_equal(c(19, 3), dim(xyz[["v"]]))
+              expect_equal(-69.72533, xyz[["longitude"]])
+              expect_equal(47.87943, xyz[["latitude"]])
+              expect_equal("xyz", xyz[["originalCoordinate"]])
               ## FIXME: add some tests on the data here
           }
 })
