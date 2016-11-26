48ebba9d6b2b898e52a2378063dba2d3e1499a6a
Dan Kelley <kelley.dan@gmail.com>
2016-11-26 09:17:47 -0400

    slightly improve sw tests+docs

--- R/sw.R
+++ R/sw.R
@@ -1288,7 +1288,14 @@ swLapseRate <- function(salinity, temperature=NULL, pressure=NULL,
 #' ISBN 978-0-646-55621-5.
 #' @examples
 #' library(oce)
-#' rho <- swRho(35, 13, 1000)
+#' # The numbers in the comments are the check values listed in reference [1];
+#' # note that temperature in that reference was on the T68 scale, but that
+#' # the present function works with the ITS-90 scale, so a conversion
+#' # is required.
+#' swRho(35, T90fromT68(5),      0, eos="unesco") # 1027.67547
+#' swRho(35, T90fromT68(5),  10000, eos="unesco") # 1069.48914
+#' swRho(35, T90fromT68(25),     0, eos="unesco") # 1023.34306
+#' swRho(35, T90fromT68(25), 10000, eos="unesco") # 1062.53817
 #'
 #' @family functions that calculate seawater properties
 swRho <- function(salinity, temperature=NULL, pressure=NULL,
