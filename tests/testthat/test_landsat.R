d29dd8e7439aece102ac85a3e881c4d804dcc939
Dan Kelley <kelley.dan@gmail.com>
2017-02-01 19:41:34 -0400

    test show(landsat) for codecov

--- tests/testthat/test_landsat.R
+++ tests/testthat/test_landsat.R
@@ -3,6 +3,7 @@ context("landsat")
 test_that("landsatTrim", {
           data(landsat)
           lt <- landsatTrim(landsat,list(longitude=-64,latitude=44),list(longitude=-63,latitude=45))
+          show(lt)
           expect_equal(lt[["lllat"]], 44)
           expect_equal(lt[["urlat"]], 45)
           expect_equal(lt[["lllon"]], -64)
