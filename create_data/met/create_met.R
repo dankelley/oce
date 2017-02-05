606a7ee217142169e9d0f3426d08e4b88d0fa213
Dan Kelley <kelley.dan@gmail.com>
2017-02-05 17:37:08 -0400

    remove debugging line

--- create_data/met/create_met.R
+++ create_data/met/create_met.R
@@ -1,5 +1,4 @@
 library(oce)
-source("~/git/oce/R/met.R")
 metFile <- download.met(6358, 2003, 9, destdir=".")
 met <- read.met(metFile)
 met <- oceSetData(met, "time", met[["time"]] + 4 * 3600, note="add 4h to local time to get UTC time")
