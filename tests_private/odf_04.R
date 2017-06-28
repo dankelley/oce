1feb1f11357eb9d15cfc6e6b324f6cdd431c9a8f
Dan Kelley <kelley.dan@gmail.com>
2017-06-28 12:23:45 -0300

    working on odf issue

--- tests_private/odf_04.R
+++ tests_private/odf_04.R
@@ -3,19 +3,29 @@ options(warn=1) # print warnings as they occur
 
 dir <- "/data/odf/ctd"
 files <- system(sprintf("find '%s' -name '*.ODF'", dir), intern=TRUE)
+if (length(files)) {
 
-i <- 0
-for (file in files) {
-    cat("\n# ", file, "\n")
-    d <- read.oce(file)
-    summary(d)                     # VISUALLY: check .out file for incorrect units or unmatched names
-    if ("temperature" %in% names(d[["data"]]) && min(d[["temperature"]], na.rm=TRUE) < -5)
-        stop("bad min temperature in file '", file,
-             "'; value is ", min(d[["temperature"]], na.rm=TRUE), "; did read.oce() catch the NullValue?")
-    if ("u" %in% names(d[["data"]]) && min(d[["u"]], na.rm=TRUE) < -5)
-        stop("bad min u in file '", file,
-             "'; value is ", min(d[["u"]], na.rm=TRUE), "; did read.oce() catch the NullValue?")
-    i <- i + 1
+    i <- 0
+    for (file in files) {
+        cat("\n# ", file, "\n")
+        d <- read.oce(file)
+        summary(d)                     # VISUALLY: check .out file for incorrect units or unmatched names
+        if ("temperature" %in% names(d[["data"]]) && min(d[["temperature"]], na.rm=TRUE) < -5)
+            stop("bad min temperature in file '", file,
+                 "'; value is ", min(d[["temperature"]], na.rm=TRUE), "; did read.oce() catch the NullValue?")
+        if ("u" %in% names(d[["data"]]) && min(d[["u"]], na.rm=TRUE) < -5)
+            stop("bad min u in file '", file,
+                 "'; value is ", min(d[["u"]], na.rm=TRUE), "; did read.oce() catch the NullValue?")
+        i <- i + 1
+    }
+    cat("Successfully checked", i, "ODF files in ", dir, "\n")
+
+    ## test for issue 1263 (a particular file)
+    if (file.exists("/data/odf/ctd/CTD_HUD2000009_1_1_DN.ODF")) {
+        oce <- read.oce("/data/odf/ctd/CTD_HUD2000009_1_1_DN.ODF")
+        odf <- read.odf("/data/odf/ctd/CTD_HUD2000009_1_1_DN.ODF")
+        print(setdiff(names(oce@metadata), names(odf@metadata)))
+        ## expect_equal(names(odf@metadata), names(oce@metadata))
+        expect_equal(names(odf@data), names(oce@data))
+    }
 }
-cat("Successfully checked", i, "ODF files in ", dir, "\n")
-
