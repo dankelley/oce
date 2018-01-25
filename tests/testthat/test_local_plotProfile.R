e3bc5cf3956d8f1f792d647ca0c35b6da4edcdf6
dankelley <kelley.dan@gmail.com>
2018-01-25 09:49:25 -0400

    typo

--- tests/testthat/test_local_plotProfile.R
+++ tests/testthat/test_local_plotProfile.R
@@ -2,7 +2,7 @@ library(oce)
 context("plotProfile handling of *lim")
 ## Need to set EOS because we are testing ranges on salinity profiles,
 ## and these vary according to the EOS.
-options(oceEOS="unesco")
+options(oceEOS="gsw")
 
 ## These tests derive from issue 1371.
 ## https://github.com/dankelley/oce/issues/1371
