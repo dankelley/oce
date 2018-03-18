f1796b0f6476b0258a7cc64bb821594dfce3df76
dankelley <kelley.dan@gmail.com>
2018-03-18 07:33:08 -0300

    remove some EOL spaces

--- tests/testthat/test_argo.R
+++ tests/testthat/test_argo.R
@@ -6,12 +6,12 @@ context("Argo")
 
 test_that("subset.argo(argo, \"adjusted\") correctly alters metadata and data", {
           a <- subset(argo, "adjusted")
-          expect_equal(a@metadata$flags$pressureQc, argo@metadata$flags$pressureAdjustedQc) 
-          expect_equal(a@metadata$flags$temperatureQc, argo@metadata$flags$temperatureAdjustedQc) 
-          expect_equal(a@metadata$flags$salinityQc, argo@metadata$flags$salinityAdjustedQc) 
-          expect_equal(a@metadata$flags$pressure, argo@metadata$flags$pressureAdjusted) 
-          expect_equal(a@metadata$flags$salinity, argo@metadata$flags$salinityAdjusted) 
-          expect_equal(a@metadata$flags$temperature, argo@metadata$flags$temperatureAdjusted) 
+          expect_equal(a@metadata$flags$pressureQc, argo@metadata$flags$pressureAdjustedQc)
+          expect_equal(a@metadata$flags$temperatureQc, argo@metadata$flags$temperatureAdjustedQc)
+          expect_equal(a@metadata$flags$salinityQc, argo@metadata$flags$salinityAdjustedQc)
+          expect_equal(a@metadata$flags$pressure, argo@metadata$flags$pressureAdjusted)
+          expect_equal(a@metadata$flags$salinity, argo@metadata$flags$salinityAdjusted)
+          expect_equal(a@metadata$flags$temperature, argo@metadata$flags$temperatureAdjusted)
 })
 
 test_that("argo [[ handles SA and CT", {
