ff9f74296f17f00bf0da18afebc513751c2717c1
dankelley <kelley.dan@gmail.com>
2018-09-01 14:32:21 -0300

    add a sontek adp to local test suite

---
+++ tests_private/adp_sontek.R
@@ -0,0 +1,8 @@
+library(oce)
+if (file.exists("/data/archive/sleiwex/2008/moorings/m07/adp/sontek_h53/raw/adp_sontek_h53.adp")) {
+    d <- read.oce("/data/archive/sleiwex/2008/moorings/m07/adp/sontek_h53/raw/adp_sontek_h53.adp")
+    summary(d)
+    plot(d, which="temperature")
+}
+
+
