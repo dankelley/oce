7c6fb96e2fcc7892cc1962e6587b114cd5966b8f
dankelley <kelley.dan@gmail.com>
2018-11-11 06:46:57 -0400

    indent a closing-brace to the oce standard

--- R/rsk.R
+++ R/rsk.R
@@ -746,7 +746,7 @@ read.rsk <- function(file, from=1, to, by=1, type, tz=getOption("oceTz", default
         if (length(pressureAtmospheric) == 0) {
             warning("empty pressureAtmospheric value in rsk file. Setting to default value of 10.1325")
             pressureAtmospheric <- 10.1325
-            }
+        }
         ##message("NEW: pressureAtmospheric:", pressureAtmospheric)
         oceDebug(debug, "after studying the RSK file, now have pressureAtmospheric=", pressureAtmospheric, "\n")
 
