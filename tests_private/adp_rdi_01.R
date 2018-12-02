d761249ca510b5d0c74ac2f22db51783298302eb
dankelley <kelley.dan@gmail.com>
2018-12-02 13:09:38 -0400

    make private adp tests do plots

--- tests_private/adp_rdi_01.R
+++ tests_private/adp_rdi_01.R
@@ -5,6 +5,7 @@ files <- list.files(path, "*", recursive=TRUE, full.names=TRUE)
 options(warn=1)                        # see warnings when they occur
 
 n <- 0
+if (!interactive()) png("adp_rdi_01-%02d.png")
 for (file in files) {
     try({
         cat("\nfile '", file, "' ", sep="")
@@ -12,12 +13,14 @@ for (file in files) {
             cat("is adp/rdi\n")
             d <- read.oce(file)
             summary(d)
+            plot(d)
             n <- n + 1
         } else {
             cat("is not adp/rdi, so it is being skipped\n")
         }
     })
 }
+if (!interactive()) dev.off()
 cat("Successfully checked", n, "ADP files in", path, "\n")
 
 
