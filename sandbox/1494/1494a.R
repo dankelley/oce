1069faa79a15cd97e6219dfbe7b2d13d8ffd384e
dankelley <kelley.dan@gmail.com>
2019-02-19 14:47:44 -0400

    more notes

--- sandbox/1494/1494a.R
+++ sandbox/1494/1494a.R
@@ -11,6 +11,7 @@ read.xbt <- function(filename)
     res <- new("oce")
     res@data <- data
     res@metadata$filename <- filename
+    ## FIXME: read other metadata (location, time, etc.) once we know the format for sure
     res@metadata$units$depth <- list(unit=expression(m), scale="")
     res@metadata$units$temperature <- list(unit=expression(degree*C), scale="ITS-90")
     res@metadata$units$soundspeed <- list(unit=expression(m/s), scale="")
@@ -18,4 +19,7 @@ read.xbt <- function(filename)
 }
 d <- read.xbt("fake.edf")
 summary(d)
+## FIXME: once we make a formal object, we can code plot methods
 plot(d[["temperature"]], d[["depth"]], ylim=rev(range(d[["depth"]])))
+## FIXME: get larger dataset (open-data) so we can work on subset() etc
+
