702250bd1c98e916d6b1a550fad426ab09ffe205
dankelley <kelley.dan@gmail.com>
2019-02-26 13:44:58 -0400

    .

--- sandbox/coastlineSubset/05.R
+++ sandbox/coastlineSubset/05.R
@@ -46,40 +46,11 @@ if (REAL) {
 box <- as(raster::extent(W, E, S, N), "SpatialPolygons")
 lines(c(W, W, E, E, W), c(S, N, N, S, S), col="magenta")
 
-cllon <- NAendpoints(coastlineWorld[["longitude"]])
-cllat <- NAendpoints(coastlineWorld[["latitude"]])
-na <- which(is.na(cllon))
-nseg <- length(na)
-owarn <- options("warn")$warn
-options(warn=-1)
-col <- 0
-for (iseg in 2:nseg) {
-    look <- seq.int(na[iseg-1]+1, na[iseg]-1)
-    lon <- cllon[look]
-    if (any(is.na(lon))) stop("step 1: double lon NA at iseg=", iseg) # checks ok on coastlineWorld
-    lat <- cllat[look]
-    if (any(is.na(lat))) stop("step 1: double lat NA at iseg=", iseg) # checks ok on coastlineWorld
-    n <- length(lon)
-    if (n < 1) stop("how can we have no data?")
-    if (length(lon) > 0) {
-        A <- sp::Polygon(cbind(lon, lat))
-        B <- sp::Polygons(list(A), "A")
-        C <- sp::SpatialPolygons(list(B))
-        ##? if (iseg == 42) browser()
-        i <- raster::intersect(box, C)
-        if (!is.null(i)) {
-            xy <- i@polygons[[1]]@Polygons[[1]]@coords
-            polygon(xy[,1], xy[,2], col=col+1)
-            cat("World view: iseg=", iseg, ": plotted in col=", col+1, "\n")
-            col <- (col + 1) %% 8
-        } else {
-            #cat("iseg=", iseg, ": no intersection\n")
-        }
-    }
-}
 options(warn=owarn)
 options(warn=-1)
-plot(as.coastline(c(E, W), c(S, N)), type="n")
+EWdel <- 0.1 * (E - W)
+NSdel <- 0.1 * (N - S)
+plot(as.coastline(c(E-EWdel, W+EWdel), c(S-NSdel, N+NSdel)), type="n")
 col <- 0
 for (iseg in 2:nseg) {
     look <- seq.int(na[iseg-1]+1, na[iseg]-1)
@@ -89,22 +60,30 @@ for (iseg in 2:nseg) {
     if (any(is.na(lat))) stop("step 1: double lat NA at iseg=", iseg) # checks ok on coastlineWorld
     n <- length(lon)
     if (n < 1) stop("how can we have no data?")
-    if (length(lon) > 0) {
+    if (length(lon) > 1) {
         A <- sp::Polygon(cbind(lon, lat))
         B <- sp::Polygons(list(A), "A")
         C <- sp::SpatialPolygons(list(B))
         ##? if (iseg == 42) browser()
         i <- raster::intersect(box, C)
         if (!is.null(i)) {
-            xy <- i@polygons[[1]]@Polygons[[1]]@coords
-            polygon(xy[,1], xy[,2], col=col+1)
-            cat("Zoomed-in: iseg=", iseg, ": plotted in col=", col+1, "\n")
-            col <- (col + 1) %% 8
+            for (j in seq_along(i@polygons)) {
+                ##> message("is this 1? ", length(i@polygons[[1]]@Polygons))
+                ##> message("and is this 1? ", length(i@polygons))
+                for (k in seq_along(i@polygons[[1]]@Polygons)) {
+                    xy <- i@polygons[[j]]@Polygons[[k]]@coords
+                    polygon(xy[,1], xy[,2], col=col+1)
+                    cat("iseg=", iseg, ", j=", j, ", k=", k, ": plotted in col=", col+1, "\n", sep="")
+                    col <- (col + 1) %% 8
+                }
+            }
         } else {
-            #cat("iseg=", iseg, ": no intersection\n")
+                                        #cat("iseg=", iseg, ": no intersection\n")
         }
     }
 }
+lines(cllon, cllat, lwd=1/2, col="gray", type="o", pch=20, cex=1/3)
+lines(c(W, W, E, E, W), c(S, N, N, S, S), col="magenta")
 mtext(sprintf("Box: W=%.2f E=%.2f S=%.2f N=%.2f", W, E, S, N), font=2, col=2)
 options(warn=owarn)
 
