4130c8e6c3fb3954ee0681c57b964a59fc7079a2
dankelley <kelley.dan@gmail.com>
2019-02-26 16:06:09 -0400

    05

--- sandbox/coastlineSubset/05.R
+++ sandbox/coastlineSubset/05.R
@@ -1,6 +1,7 @@
-REAL <- TRUE # to avoid having to click whilst writing code
+REAL <- !TRUE # to avoid having to click whilst writing code
 library(raster)
 library(oce)
+## install.packages("rgeos")
 
 #' Ensure that vector starts and ends with NA
 #' @param x a vector
@@ -23,10 +24,12 @@ getCoords <- function(sp)
 }
 
 data(coastlineWorld, package="oce")
+cllon <- coastlineWorld[["longitude"]]
+cllat <- coastlineWorld[["latitude"]]
+par(mfrow=c(2, 1), mar=rep(1, 4))
 plot(coastlineWorld)
 mtext("Click twice to define a box", font=2, col=2)
 requireNamespace("raster") # we only need a few functions and I prefer to name them
-par(mar=rep(1, 4))
 if (REAL) {
     bb <- locator(2)
     W <- min(bb$x)
@@ -34,22 +37,27 @@ if (REAL) {
     S <- min(bb$y)
     N <- max(bb$y)
 } else {
-    W <- -100
-    E <- -50
-    S <- 30
-    N <- 60
+    W <- -70.79
+    E <- -61.79
+    S <- 39.56
+    N <- 53.42
+    ## DK: testing effect of moving the boundary
+    N <- N + 10
+    E <- E + 5
+    W <- W - 2
 }
 box <- as(raster::extent(W, E, S, N), "SpatialPolygons")
 lines(c(W, W, E, E, W), c(S, N, N, S, S), col="magenta")
 
-cllon <- NAendpoints(coastlineWorld[["longitude"]])
-cllat <- NAendpoints(coastlineWorld[["latitude"]])
+owarn <- options("warn")$warn
+options(warn=-1)
+EWdel <- 0.1 * (E - W)
+NSdel <- 0.1 * (N - S)
+plot(as.coastline(c(E-EWdel, W+EWdel), c(S-NSdel, N+NSdel)), type="n")
+col <- 0
 na <- which(is.na(cllon))
 nseg <- length(na)
-col <- 1
-
 for (iseg in 2:nseg) {
-    message("iseg=", iseg)             # 290 in coastlineWorld
     look <- seq.int(na[iseg-1]+1, na[iseg]-1)
     lon <- cllon[look]
     if (any(is.na(lon))) stop("step 1: double lon NA at iseg=", iseg) # checks ok on coastlineWorld
@@ -57,23 +65,30 @@ for (iseg in 2:nseg) {
     if (any(is.na(lat))) stop("step 1: double lat NA at iseg=", iseg) # checks ok on coastlineWorld
     n <- length(lon)
     if (n < 1) stop("how can we have no data?")
-    if (length(lon) > 2) {
-        message("  long enough")
+    if (length(lon) > 1) {
         A <- sp::Polygon(cbind(lon, lat))
         B <- sp::Polygons(list(A), "A")
         C <- sp::SpatialPolygons(list(B))
         ##? if (iseg == 42) browser()
         i <- raster::intersect(box, C)
         if (!is.null(i)) {
-            xy <- i@polygons[[1]]@Polygons[[1]]@coords
-            polygon(xy[,1], xy[,2], col=col)
-            col <- (col + 1) %% 5 + 1
-            cat("iseg=", iseg, ": plotted in col=", col, "\n")
+            for (j in seq_along(i@polygons)) {
+                ##> message("is this 1? ", length(i@polygons[[1]]@Polygons))
+                ##> message("and is this 1? ", length(i@polygons))
+                for (k in seq_along(i@polygons[[1]]@Polygons)) {
+                    xy <- i@polygons[[j]]@Polygons[[k]]@coords
+                    polygon(xy[,1], xy[,2], col=col+1)
+                    cat("iseg=", iseg, ", j=", j, ", k=", k, ": plotted in col=", col+1, "\n", sep="")
+                    col <- (col + 1) %% 8
+                }
+            }
         } else {
-            cat("iseg=", iseg, ": no intersection\n")
+                                        #cat("iseg=", iseg, ": no intersection\n")
         }
-    } else {
-        cat("iseg=", iseg, ": < 3 points\n")
     }
 }
+lines(cllon, cllat, lwd=1/2, col="gray", type="o", pch=20, cex=1/3)
+lines(c(W, W, E, E, W), c(S, N, N, S, S), col="magenta")
+mtext(sprintf("Box: W=%.2f E=%.2f S=%.2f N=%.2f", W, E, S, N), font=2, col=2)
+options(warn=owarn)
 
