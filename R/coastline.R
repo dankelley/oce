e390af9425a26a4b29846e2c03e5c45f9e341a34
dankelley <kelley.dan@gmail.com>
2019-03-08 15:43:11 -0400

    plotcoastline(..., proj=...) fixup (close 1505)

--- R/coastline.R
+++ R/coastline.R
@@ -565,7 +565,7 @@ setMethod(f="plot",
                   oceDebug(debug, "plot,coastline-method calling mapPlot (code location 1)\n")
                   mapPlot(x[["longitude"]], x[["latitude"]], projection=projection,
                           longitudelim=longitudelim, latitudelim=latitudelim,
-                          bg=col, col=fill, border=border, debug=debug-1)
+                          bg=col, col=if (missing(fill)) "lightgray" else fill, border=border, debug=debug-1)
                   return(invisible())
               }
               if (!missing(clongitude))
