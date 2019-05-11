2c4696953fd25148b6f409cbd0532d03dfc68e20
dankelley <kelley.dan@gmail.com>
2019-05-11 10:16:50 -0300

    create_argo updated for R 3.6.x

--- create_data/argo/create_argo.R
+++ create_data/argo/create_argo.R
@@ -2,5 +2,9 @@ library(oce)
 argo <- read.oce("6900388_prof.nc")
 summary(argo)
 save(argo, file="argo.rda")
-tools::resaveRdaFiles("argo.rda")
+if (utils::compareVersion(R.Version()$minor, '3.6') >= 0) {
+    tools::resaveRdaFiles('argo.rda', version=2)
+} else {
+    tools::resaveRdaFiles('argo.rda')
+}
 
