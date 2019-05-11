20f3959ca30c8531675f682a34576cace6b934a1
dankelley <kelley.dan@gmail.com>
2019-05-11 09:26:44 -0300

    Save data(section) so more R versions can read it
    This is for issue 1548 (https://github.com/dankelley/oce/issues/1548),
    and it follows the method used in the cmocean R project
    (https://github.com/aitap/cmocean/commit/f4d0a1d1d48f6687a3ec16ebf86c11a3738fff92).
    
    Below is a file listing, with 'section.rda' being the update and
    'section_old.rda' being a version made (with R 3.6.0) a couple of days
    ago. The size is slightly different, indicating at least that the
    updated code changed something. I do not have access to an R of version
    3.4.x, so I cannot check on whether this newly-created file is
    compatable with it, although it is certainly compatable with R 3.6.0,
    because my build-test suite works okay.
    
    -rw-r--r--  1 kelley  staff   71757 11 May 08:58 section.rda
    -rw-r--r--  1 kelley  staff   71983  9 May 09:47 section_old.rda

--- create_data/section/create_section.R
+++ create_data/section/create_section.R
@@ -1,14 +1,15 @@
 library(oce)
 ## next worked 2017-02-20
-# download.file("https://cchdo.ucsd.edu/data/7872/a03_hy1.csv", "a03_hy1.csv")
+## download.file("https://cchdo.ucsd.edu/data/7872/a03_hy1.csv", "a03_hy1.csv")
 section <- read.section("a03_hy1.csv", sectionId="a03", institute="SIO",
                         ship="R/V Professor Multanovskiy", scientist="Vladimir Tereschenkov") 
 section <- initializeFlagScheme(section, "WHP bottle")
-save(section, file="section.rda")
-tools::resaveRdaFiles("section.rda")
-n <- length(section[['station',1]]@data)
-names <- names(section[['station',1]]@data)
-par(mfrow=c(3,3), mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0)) # I checked: n==9
-for (name in names) {
-    hist(section[[name]], main="", xlab=name) # a check on issue 983
+save(section, file="section.rda", version=2)
+if (utils::compareVersion(R.Version()$minor, '3.6') >= 0) {
+    tools::resaveRdaFiles('section.rda', version=2)
+} else {
+    tools::resaveRdaFiles('section.rda')
 }
+
+
+
