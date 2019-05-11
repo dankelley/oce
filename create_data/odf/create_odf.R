c44677c39427a7e9b6290a3f952ccc30ab2d9081
dankelley <kelley.dan@gmail.com>
2019-05-11 14:38:02 -0300

    update create_odf.R for R 3.6.x

--- create_data/odf/create_odf.R
+++ create_data/odf/create_odf.R
@@ -1,6 +1,9 @@
 library(oce)
 odf <- read.oce("../../inst/extdata/CTD_BCD2014666_008_1_DN.ODF.gz")
 save(odf, file="odf.rda")
-tools::resaveRdaFiles("odf.rda")
-
+if (utils::compareVersion(R.Version()$minor, "3.6") >= 0) {
+    tools::resaveRdaFiles("odf.rda", version=2)
+} else {
+    tools::resaveRdaFiles("odf.rda")
+}
 
