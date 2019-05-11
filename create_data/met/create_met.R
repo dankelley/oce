0dcd8a63f37ca9fd490e2032a912bc0eaffe68ea
dankelley <kelley.dan@gmail.com>
2019-05-11 14:38:57 -0300

    update create_met.R for R 3.6.x

--- create_data/met/create_met.R
+++ create_data/met/create_met.R
@@ -3,5 +3,9 @@ metFile <- download.met(6358, 2003, 9, destdir=".")
 met <- read.met(metFile)
 met <- oceSetData(met, "time", met[["time"]] + 4 * 3600, note="add 4h to local time to get UTC time")
 save(met, file="met.rda")
-tools::resaveRdaFiles("met.rda")
+if (utils::compareVersion(R.Version()$minor, "3.6") >= 0) {
+    tools::resaveRdaFiles("met.rda", version=2)
+} else {
+    tools::resaveRdaFiles("met.rda")
+}
 
