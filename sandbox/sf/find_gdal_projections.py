45926c4c9498d4c4cfb8b5decf76829d0515ca4f
dankelley <kelley.dan@gmail.com>
2020-01-22 12:59:51 -0400

    find gdal projections

---
+++ sandbox/sf/find_gdal_projections.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from osgeo import osr
+from subprocess import Popen, PIPE
+
+osr.UseExceptions()
+
+# Get the list of PROJ.4 projections
+proj = {}
+p = Popen(['proj', '-lp'], stdout=PIPE)
+for line in p.communicate()[0].split('\n'):
+    if ':' in line:
+        a, b = line.split(':')
+        proj[a.strip()] = b.strip()
+
+# Brute force method of testing GDAL's OSR module
+supported = set()
+not_supported = set()
+for k in proj.keys():
+    sr = osr.SpatialReference()
+    try:
+        _ = sr.ImportFromProj4('+proj=' + k)
+        supported.add(k)
+    except RuntimeError as e:
+        not_supported.add(k)
+
+print('{0} total projections, {1} supported, {2} not supported'
+      .format(len(proj), len(supported), len(not_supported)))
+print('Supported: ' + ', '.join(sorted(supported)))
+print('Not supported: ' + ', '.join(sorted(not_supported)))
