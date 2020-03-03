1b9b9f0b08a753b8e75fcf857044e091bb93a1f0
dankelley <kelley.dan@gmail.com>
2020-03-03 12:41:39 -0400

    remove debugging source() call

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -275,7 +275,6 @@ test_that("magneticField version 12 (why not perfect?)", {
 test_that("magneticField version 13 (why not perfect?)", {
           ## REF: http://www.geomag.bgs.ac.uk/data_service/models_compass/wmm_calc.html
           ## version 13 by default as of oce "develop" branch date 2020-03-03
-          source("~/git/oce/R/misc.R")
           mf <- magneticField(-63.562, 44.640, as.POSIXct("2020-03-03 00:00:00", tz="UTC"), version=13)
           mf2 <- magneticField(-63.562, 44.640, as.Date("2020-03-03", tz="UTC"), version=13)
           cbind(mf, mf2) # Q: why so much difference here?
