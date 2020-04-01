2b9506df6c24aa7870b8fa78dd6c2c4128227836
dankelley <kelley.dan@gmail.com>
2020-04-01 18:52:37 -0300

    update tests re sort order
    Evidently, R-4.0 is ordering alphabetical lists differently (with
    respect to upper- and lower-case) than had been so previously, so I
    updated a couple of tests.

--- tests/testthat/test_local_satellite.R
+++ tests/testthat/test_local_satellite.R
@@ -5,10 +5,10 @@ test_that("amsr", {
               amsr <- read.amsr("local_data/f34_20160808v7.2.gz")
               expect_equal("amsr", amsr[["spacecraft"]])
               expect_equal(c(1440, 720), dim(amsr[["SSTDay"]]))
-              expect_equal(c("cloudDay", "cloudNight", "LFwindDay", "LFwindNight", "MFwindDay", "MFwindNight",
+              expect_equal(tolower(c("cloudDay", "cloudNight", "LFwindDay", "LFwindNight", "MFwindDay", "MFwindNight",
                              "rainDay", "rainNight", "SSTDay", "SSTNight", "timeDay", "timeNight", "vaporDay",
-                             "vaporNight"), 
-                           sort(names(amsr@data)))
+                             "vaporNight")),
+                           sort(tolower(names(amsr@data))))
               ##> summary(amsr)
               expect_warning(plot(amsr), "auto-decimating first index of large image by 3")
           }
