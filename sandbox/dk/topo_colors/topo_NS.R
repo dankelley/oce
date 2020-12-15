cae0b216fad9140fb4b13bdec64ebe68aaf0b3ca
dankelley <kelley.dan@gmail.com>
2020-12-15 09:40:28 -0400

    topoNS

--- sandbox/dk/topo_colors/topo_NS.R
+++ sandbox/dk/topo_colors/topo_NS.R
@@ -6,6 +6,11 @@ topoFile <- download.topo(-67,-59.5,43.3,47.2, resolution=1) # NS
 ## topoFile <- download.topo(-67,-42,40,54, resolution=8) # nwatl
 topo <- read.topo(topoFile)
 save(topo, file="topoNS.rda")
+system("ls -l topoNS.rda")
+load("topoNS.rda")
+tools::resaveRdaFiles("topoNS.rda")
+system("ls -l topoNS.rda")
+
 H <- quantile(abs(topo[["z"]]), 0.98)
 cm <- colormap(H*c(-1, 1), col=function(n) oceColorsGebco(region="both",n=n))#, breaks=201)
 
