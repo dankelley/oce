62fe99e6342ce54f7576ad388701c2778026b328
dankelley <kelley.dan@gmail.com>
2020-12-16 19:39:03 -0400

    rebuild topoNS to be backwards compat with R 2.15

--- create_data/topoNS/create_topoNS.R
+++ create_data/topoNS/create_topoNS.R
@@ -3,9 +3,9 @@ library(oce)
 # 1 minute resolution is 1.8km north-south, about 1.5km in east-west
 topoFile <- download.topo(-67, -59.5, 43.3, 47.2, resolution=1)
 topoNS <- read.topo(topoFile)
-save(topoNS, file="topoNS.rda")
+save(topoNS, file="topoNS.rda", version=2)
 system("ls -l topoNS.rda")
 load("topoNS.rda")
-tools::resaveRdaFiles("topoNS.rda")
+tools::resaveRdaFiles("topoNS.rda", version=2)
 system("ls -l topoNS.rda")
 
