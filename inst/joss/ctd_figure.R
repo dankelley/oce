97040105d639bded09f049daad8f4e05f30bd3f5
dankelley <kelley.dan@gmail.com>
2021-05-26 09:30:06 -0300

    try to get paper.Rmd (joss) working

--- inst/joss/ctd_figure.R
+++
@@ -1,8 +0,0 @@
-png("ctd_figure.png", width=7, height=6, unit="in", res=300, pointsize=12)
-
-library(oce)                           # load library
-data(ctd)                              # sea level in Halifax Harbour
-plot(ctd)                              # plot summary diagram
-
-dev.off()
-
