3569ba9b8ee1ae6f4644fd7705168381199ea565
dankelley <kelley.dan@gmail.com>
2021-07-08 13:07:33 -0300

    move 'sf' from Depends to Suggests

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // do_ad2cp_ahrs
 NumericMatrix do_ad2cp_ahrs(NumericMatrix v, NumericMatrix ahrs);
 RcppExport SEXP _oce_do_ad2cp_ahrs(SEXP vSEXP, SEXP ahrsSEXP) {
