fa9c6b1a1134a844f51f75d047ca66449b070e91
dankelley <kelley.dan@gmail.com>
2022-05-12 11:21:52 -0300

    new data loc

--- create_data/cm/create_cm.R
+++ create_data/cm/create_cm.R
@@ -1,7 +1,7 @@
 library(oce)
 
 ## One day of in-water data.
-cm <- read.oce("/data/archive/sleiwex/2008/moorings/m11/cm/interocean_0811786/manufacturer/cm_interocean_0811786.s4a.tab",
+cm <- read.oce("~/data/archive/sleiwex/2008/moorings/m11/cm/interocean_0811786/manufacturer/cm_interocean_0811786.s4a.tab",
                from=840, to=840+24*60)
 cm <- subset(cm, time < as.POSIXct("2008-06-27 00:00:00", tz="UTC"))
 summary(cm)
