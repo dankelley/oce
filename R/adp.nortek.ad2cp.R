ac3a6f2099c4f0aef5ae03e077e56e62a06637e4
dankelley <kelley.dan@gmail.com>
2022-06-29 09:19:17 -0300

    add test_ad2cp_3.R (Signature250 from CR)

--- R/adp.nortek.ad2cp.R
+++ R/adp.nortek.ad2cp.R
@@ -1023,7 +1023,12 @@ read.adp.ad2cp <- function(file, from=1, to=0, by=1, which="all",
             powerLevel=powerLevel[p$burstAltimeterRaw])
         if (TRUE) {
             # Vectorized reading, for speed.
-            # See BOOKMARK 1 for *Included 
+            #
+            # See CR's snapshot at
+            # https://github.com/dankelley/oce/issues/1959#issuecomment-1141409542
+            # which is p89 of Nortek AS. “Signature Integration
+            # 55|250|500|1000kHz.” Nortek AS, March 31, 2022)
+            #
             #save(burstAltimeterRaw, file="~/01.rda")
             #message("DAN: burstAltimeterRaw is saved in ~/01.rda)")
             oceDebug(debug, "nbeam=", nbeamsBurstAltimeterRaw, ", ncell=", ncellsBurstAltimeterRaw, "\n")
@@ -1082,7 +1087,7 @@ read.adp.ad2cp <- function(file, from=1, to=0, by=1, which="all",
                     stop("altimeterRawNumberOfSamples not all equal.  Range is ", dNS[1], " to ", dNS[2])
                 NS <- NS[1]
                 burstAltimeterRaw$altimeterRawNumberOfSamples <- NS
-                message(vectorShow(burstAltimeterRaw$altimeterRawNumberOfSamples))
+                #message(vectorShow(burstAltimeterRaw$altimeterRawNumberOfSamples))
                 i0v <- i0v + 4L # skip the 4 bytes we just read
                 # FIXME: read 2b=altimeterRawSampleDistance here
                 i0v <- i0v + 2L # skip the 2 bytes we just read (FIXME)
@@ -1094,7 +1099,7 @@ read.adp.ad2cp <- function(file, from=1, to=0, by=1, which="all",
                 # FIXME: the factor should be velocityFactor[...]
                 tmp <- readBin(buf[iv], "integer", size=2L, endian="little",
                     n=NP*burstAltimeterRaw$altimeterRawNumberOfSamples)
-                burstAltimeterRaw$altimeterRawSamplesNEW <- matrix(tmp, nrow=NP, ncol=NS, byrow=TRUE)
+                burstAltimeterRaw$altimeterRawSamples <- matrix(tmp, nrow=NP, ncol=NS, byrow=TRUE)
                 i0v <- i0v + 2L*burstAltimeterRaw$altimeterRawNumberOfSamples
                 oceDebug(debug, "  new i0v=", i0v, "\n")
             }
@@ -1513,7 +1518,9 @@ read.adp.ad2cp <- function(file, from=1, to=0, by=1, which="all",
                 burst$altimeterRawSampleDistance <- 0.1e-3*readBin(d$buf[i+i0+0:1],"integer",size=2,n=1,endian="little",signed=FALSE)
                 message(vectorShow(burst$altimeterRawSampleDistance))
                 i0 <- i0 + 2
-                burst$altimeterRawSamples <- readBin(d$buf[i+i0+0:1],"integer",size=2,n=1,endian="little") # 'signed frac' in docs FIXME(DEK) what does that mean??
+                # ERROR: not increasing i0 properly. Never caught this because sample
+                # dataset did not have following data (echosounder etc).
+                burst$altimeterRawSamplesOLD <- readBin(d$buf[i+i0+0:1],"integer",size=2,n=1,endian="little") # 'signed frac' in docs FIXME(DEK) what does that mean??
                 message(vectorShow(burst$altimeterRawSamples))
                 i0 <- i0 + 2
                 # See the Nortek manual snippet below, for more on format.
@@ -1764,35 +1771,35 @@ read.adp.ad2cp <- function(file, from=1, to=0, by=1, which="all",
                 i0 <- i0 + 12 # skip spare (8 bytes)
             }
             if (altimeterRawIncluded[ch]) {
-                burstAltimeterRaw$altimeterRawNumberOfSamples <- readBin(d$buf[i+i0+0:3],"integer",size=4,n=1,endian="little")
-                oceDebug(debug>1, "saving altimeterRawNumberOfSamples=",
-                    burstAltimeterRaw$altimeterRawNumberOfSamples, " at i0=", i0, "\n")
+                #| burstAltimeterRaw$altimeterRawNumberOfSamples <- readBin(d$buf[i+i0+0:3],"integer",size=4,n=1,endian="little")
+                #| oceDebug(debug>1, "saving altimeterRawNumberOfSamples=",
+                    #| burstAltimeterRaw$altimeterRawNumberOfSamples, " at i0=", i0, "\n")
                 i0 <- i0 + 4
-                # Create space
-                if (!"altimeterRawSampleDistance" %in% names(burstAltimeterRaw)) {
-                    oceDebug(debug>1, "creating burstAltimeterRaw$altimeterRawSampleDistance (", sum(d$id==0x1a), ")\n")
-                    burstAltimeterRaw$altimeterRawSampleDistance <- rep(NA_real_, sum(d$id==0x1a))
-                }
-                # Docs say 0.1mm in one line, and then m in another, but the
-                # former seems to fit the data better.
-                burstAltimeterRaw$altimeterRawSampleDistance[burstAltimeterRaw$i] <- 1.0e-4*readBin(d$buf[i+i0+0:1],"integer",size=2,n=1,endian="little",signed=FALSE)
-                oceDebug(debug>1, "saving altimeterRawSampleDistance[", burstAltimeterRaw$i,  "]=",
-                    burstAltimeterRaw$altimeterRawSampleDistance[burstAltimeterRaw$i], " at i0=", i0, "\n")
+                #| # Create space
+                #| if (!"altimeterRawSampleDistance" %in% names(burstAltimeterRaw)) {
+                #|     oceDebug(debug>1, "creating burstAltimeterRaw$altimeterRawSampleDistance (", sum(d$id==0x1a), ") at location 1\n")
+                #|     burstAltimeterRaw$altimeterRawSampleDistance <- rep(NA_real_, sum(d$id==0x1a))
+                #| }
+                #| # Docs say 0.1mm in one line, and then m in another, but the
+                #| # former seems to fit the data better.
+                #| burstAltimeterRaw$altimeterRawSampleDistance[burstAltimeterRaw$i] <- 1.0e-4*readBin(d$buf[i+i0+0:1],"integer",size=2,n=1,endian="little",signed=FALSE)
+                #| oceDebug(debug>1, "saving altimeterRawSampleDistance[", burstAltimeterRaw$i,  "]=",
+                #|     burstAltimeterRaw$altimeterRawSampleDistance[burstAltimeterRaw$i], " at i0=", i0, "\n")
                 i0 <- i0 + 2
                 # Create space for data, or check that dimensionality agrees with existing space.
                 dim2 <- burstAltimeterRaw$altimeterRawNumberOfSamples
-                if ("altimeterRawSamples" %in% names(burstAltimeterRaw)) {
-                    dim2Old <- dim(burstAltimeterRaw$altimeterRawSamples)[2]
-                    if (dim2 != dim2Old)
-                        stop("burstAltimeterRaw$altimeterRawSamples was set up to hold ", dim2Old, " samples, but data chunk ", ch, " has ", dim2, " samples")
-                } else {
-                    oceDebug(debug>1, "creating burstAltimeterRaw$altimeterRawSamples (", sum(d$id==0x1a), "X", dim2, ")\n")
-                    burstAltimeterRaw$altimeterRawSamples <- array(double(), dim=c(sum(d$id==0x1a), dim2))
-                }
-                burstAltimeterRaw$altimeterRawSamples[burstAltimeterRaw$i,] <- readBin(d$buf[i+i0+seq(0, 2*dim2-1)],
-                    "integer", size=2L, n=dim2, endian="little")
-                oceDebug(debug>1, "saving burstAltimeterRaw$altimeterRawSamples[",
-                    burstAltimeterRaw$i, ",] at i0=", i0, " (note: dim2=", dim2, ")\n")
+                #| if ("altimeterRawSamplesOLD" %in% names(burstAltimeterRaw)) {
+                #|     dim2Old <- dim(burstAltimeterRaw$altimeterRawSamplesOLD)[2]
+                #|     if (dim2 != dim2Old)
+                #|         stop("burstAltimeterRaw$altimeterRawSamplesOLD was set up to hold ", dim2Old, " samples, but data chunk ", ch, " has ", dim2, " samples")
+                #| } else {
+                #|     oceDebug(debug>1, "creating burstAltimeterRaw$altimeterRawSamplesOLD (", sum(d$id==0x1a), "X", dim2, ") at location 2\n")
+                #|     burstAltimeterRaw$altimeterRawSamplesOLD <- array(double(), dim=c(sum(d$id==0x1a), dim2))
+                #| }
+                #| burstAltimeterRaw$altimeterRawSamplesOLD[burstAltimeterRaw$i,] <- readBin(d$buf[i+i0+seq(0, 2*dim2-1)],
+                #|     "integer", size=2L, n=dim2, endian="little")
+                #| oceDebug(debug>1, "saving burstAltimeterRaw$altimeterRawSamplesOLD[",
+                #|     burstAltimeterRaw$i, ",] at i0=", i0, " (note: dim2=", dim2, ")\n")
                 i0 <- i0 + 2*dim2
                 # See CR's snapshot at
                 # https://github.com/dankelley/oce/issues/1959#issuecomment-1141409542
---
+++ tests/testthat/test_ad2cp_3.R
@@ -0,0 +1,99 @@
+# vim:textwidth=80:expandtab:shiftwidth=4:softtabstop=4
+
+# This Nortek Signature100 dataset in AD2CP format was kindly provided by
+# @richardsc.  It contains burstAltimeterRaw data (unlike the dataset used by
+# test_ad2cp_1.R and test_ad2cp_2.R).
+
+# Much of what is done here is to check for consistency with values found on
+# 2022-06-29, as a guard against any changes to oce that might alter how it
+# reads Nortk ad2cp data.
+
+library(oce)
+
+file <- "local_data/ad2cp/S102791A002_Barrow_v2.ad2cp"
+
+if (file.exists(file)) {
+    test_that("local_data/ad2cp/S102791A002_Barrow_v2.ad2cp (signature 250)", {
+        expect_warning(
+            expect_warning(
+                d <- read.oce(file),
+                "using to=1477 based on file contents"),
+            "'plan' defaulting to 0")
+        # Identifiers
+        expect_equal(d[["type"]], "Signature250")
+        expect_equal(d[["fileType"]], "AD2CP")
+        expect_equal(d[["serialNumber"]], 102791)
+        # Entry names
+        expect_equal(sort(names(d[["data"]])), c("activeConfiguration",
+                "average", "bottomTrack", "burst", "burstAltimeterRaw",
+                "orientation", "powerLevel", "status"))
+
+        expect_equal(sort(names(d[["average"]])),
+            c("a", "accelerometerx", "accelerometery", "accelerometerz",
+                "blankingDistance", "cellSize", "datasetDescription",
+                "ensemble", "heading", "magnetometerx", "magnetometery",
+                "magnetometerz", "nominalCorrelation", "numberOfBeams",
+                "numberOfCells", "oceCoordinate", "orientation",
+                "originalCoordinate", "pitch", "powerLevel", "pressure", "q",
+                "roll", "soundSpeed", "temperature", "temperatureMagnetometer",
+                "temperatureRTC", "time", "transmitEnergy", "v"))
+        # Beams and cells
+        expect_equal(d[["oceCoordinate"]], "beam")
+        expect_equal(d[["cellSize", "average"]], 1)
+        expect_equal(d[["blankingDistance", "average"]], 0.5)
+        expect_equal(d[["numberOfBeams", "average"]], 4)
+        expect_equal(d[["numberOfCells", "average"]], 87)
+        # Some velocity slices
+        v <- d[["v"]]
+        expect_equal(v[1,,1],
+            c(-0.553, 2.255, 2.003, -1.161, 2.121, 1.061, 2.274, 2.231, 0.602,
+                2.28, 2.061, -2.174, 0.267, -0.979, 1.874, 2.339, -1.965, 1.31,
+                1.297, 2.34, 1.619, 1.846, -2.148, -0.709, 1.776, 2.569, 0.275,
+                1.937, 2.469, 2.405, 0.958, 0.608, 0.193, 2.443, 1.023, -1.218,
+                -0.328, -0.751, -1.775, -2.386, -2.006, 2.397, 0.432, -1.961,
+                -1.587, -1.973, -0.209, 0.541, -2.069, -2.571, 0.647, 0.406,
+                -0.197, 1.148, -2.386, 0.4, -1.223, -1.118, -1.235, -0.055, 0.649,
+                1.906, 2.149, -0.186, 1.775, 2.362, 2.104, -0.517, -2.445, -1.806,
+                2.138, -2.557, 1.898, 1.279, 1.778, 0.371, 0.886, 0.814, -0.86,
+                -2.291, 1.665, -0.911, -1.491, -1.318, 0.396, 1.649, -0.927))
+        expect_equal(v[1,1,],
+            c(-0.553, 0.647, 0.099, 0.045))
+        expect_equal(v[1,,2],
+            c(0.647, 1.701, 0.861, 1.612, 2.451, 2.157, -0.347, 0.131, 1.487,
+                -0.937, -0.98, 1.664, 2.114, -2.224, -2.434, 2.004, 1.927,
+                1.105, 1.34, -2.118, -2.236, -0.206, 1.583, -1.877, 0.846,
+                0.672, -1.246, -0.162, -2.063, -2.069, -0.158, 1.725, 0.688,
+                -0.789, -0.092, 1.385, -1.389, 0.318, -0.334, 0.433, -2.564,
+                2.014, 2.355, -1.102, 0.829, 2.156, 2.599, -1.577, -0.562,
+                0.001, 2.088, 0.484, -1.586, 1.221, 0.645, -2.272, 1.802, 2.563,
+                0.654, -1.036, -0.618, -0.333, -0.73, -0.021, 1.204, 0.059,
+                2.475, 1.655, -2.077, -2.062, -1.222, -0.958, 1.279, -1.897,
+                -0.208, 1.232, 1.185, -1.423, -2.429, 0.218, 2.204, 1.773,
+                -1.272, -0.794, -1.35, 1.464, -1.952))
+        # Time
+        expect_equal(head(d[["time"]]),
+            structure(c(1653479685.6677, 1653479687.6677, 1653479689.6677,
+                    1653479691.6677, 1653479693.6677, 1653479695.6677), class =
+                c("POSIXct", "POSIXt"), tzone = "UTC"))
+        expect_equal(d[["time"]], d[["time", "average"]])
+
+        # Pressure, temperature etc
+        expect_equal(head(d[["pressure"]]),
+            c(0.791, 0.801, 0.807, 0.796, 0.803, 0.799))
+        expect_equal(d[["pressure", "average"]], d[["pressure"]])
+        expect_equal(head(d[["temperature"]]),
+            c(20.18, 20.18, 20.18, 20.18, 20.18, 20.18))
+        expect_equal(d[["temperature"]], d[["temperature", "average"]])
+        expect_equal(head(d[["heading"]]),
+            c(280.21, 279.75, 279.97, 279.75, 279.72, 279.82))
+        expect_equal(d[["heading"]], d[["heading", "average"]])
+        expect_equal(head(d[["pitch"]]),
+            c(-45.4, -45.43, -45.43, -45.41, -45.42, -45.42))
+        expect_equal(d[["pitch"]], d[["pitch", "average"]])
+
+        expect_equal(head(d[["roll"]]),
+            c(88.64, 88.62, 88.62, 88.64, 88.64, 88.62))
+        expect_equal(d[["roll"]], d[["roll", "average"]])
+})
+}
+
