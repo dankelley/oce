29ccb168b702426668747ecf5d0522cdd017d1e1
dankelley <kelley.dan@gmail.com>
2022-07-28 13:01:25 -0300

    remove superflous file

--- 000.R
+++
@@ -1,7 +0,0 @@
-library(oce)
-#> Loading required package: gsw
-data(section)
-n <- length(section[['station']][[100]][['pressure']])
-section[['station']][[100]][['pressure']] <- rep(NA, n)
-plot(section, which='temperature', ztype='image', xtype='longitude', xlim=c(-73, 0))
-
