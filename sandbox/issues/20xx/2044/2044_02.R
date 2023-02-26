71e1d732bf7db818256fd1fed0fc150b75f097ad
dankelley <kelley.dan@gmail.com>
2023-02-26 13:20:13 -0400

    fiddle with tests a bit (some work was interactive)

--- sandbox/issues/20xx/2044/2044_02.R
+++ sandbox/issues/20xx/2044/2044_02.R
@@ -7,9 +7,11 @@ library(oce)
 ctd <- read.oce("d201211_0047.cnv")
 
 Slim <- c(34.8327, 34.8355)
+Slim <- c(34.8327, 34.8328)
 Tlim<- c(0.8638, 0.879)
+Tlim<- c(0.8638, 0.8640)
 
-plotTS(ctd, Slim=Slim, Tlim=Tlim, eos="unesco", type="o", cex=0.6)
+plotTS(ctd, Slim=Slim, Tlim=Tlim, eos="unesco", type="o", cex=0.6, gridIsopycnals=NULL)
 usr <- par("usr")
 NS <- 50
 NT <- 60
@@ -18,4 +20,4 @@ TT <- seq(usr[3], usr[4], length.out=NT)
 grid <- expand.grid(S=SS, T=TT, KEEP.OUT.ATTRS=FALSE)
 rho <- swRho(grid$S, grid$T, rep(0.0, length.out=NS*NT), eos="unesco")
 dim(rho) <- c(NS, NT)
-contour(SS, TT, rho, add=TRUE, col=2, labels=FALSE)
+contour(SS, TT, rho, add=TRUE, col=2, drawlabels=FALSE, lty=2)
