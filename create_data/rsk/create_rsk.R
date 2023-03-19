6eedccb58665248dff0bcefb7af6a8e2da8ac2dc
dankelley <kelley.dan@gmail.com>
2023-03-19 08:47:40 -0300

    data(rsk) trimmed (again) to downcast
    I removed that during some tests.  It helps because (a) it is common
    practice and (b) it chops a bit over 100K from the size of data/ in the
    package.

--- create_data/rsk/create_rsk.R
+++ create_data/rsk/create_rsk.R
@@ -5,6 +5,8 @@ rsk <- oceSetMetadata(rsk, "latitude", 73 + 13.727/60)
 rsk <- oceSetMetadata(rsk, "station", "C18")
 rsk <- oceSetMetadata(rsk, "ship", "Ault")
 rsk <- oceSetMetadata(rsk, "institute", "Ocean Research Project")
+# isolate to downcast (reduces object size, and we normally do this anyway)
+rsk <- subset(rsk, 1441381040 < time & time < 1441381480)
 
 # Save in version 2, because otherwise users with R 3.5.x and earlier will not
 # be able to use data("rsk")
