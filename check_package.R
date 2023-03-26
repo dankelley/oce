4036848f06b857c9e01cb730196916686948795a
dankelley <kelley.dan@gmail.com>
2023-03-26 09:35:38 -0300

    add a mac check

--- check_package.R
+++ check_package.R
@@ -12,16 +12,32 @@ t <- devtools::spell_check()
 print(t)
 stopifnot(t == "No spelling errors found.")
 urlchecker::url_check()
+
+# devtools checks.
+# These are reliable, and useful, in contrast to the rhub checks that follow.
+devtools::check_mac_release()
 devtools::check_win_release()
 devtools::check_win_devel()
 devtools::check_win_oldrelease()
-# rhub broken (2022 Jun,Jul), working (2022 Aug and later)
+
+# Rhub checks.
+# [2023-03-26] The next two checks are not very reliable.  Quite often,
+# a run gets to the end with no problems but the system reports a PREPERROR.
+# More rarely, but certainly not uncommonly, the test system dies
+# before it gets to the stage of actually trying to build oce.  And,
+# even when these tests (and all others) pass, sometimes the CRAN machines
+# report other problems.  Given that the above block always seems
+# to be useful, and that the next one is so unreliable, I don't know
+# if there is any point in keeping the next.  Oh, and bonus: the
+# tests in the next block often don't report for half a day.
 rhub::check_for_cran(email="Dan.Kelley@Dal.Ca", show_status=FALSE)
 rhub::check(platform="debian-clang-devel", show_status=FALSE)
 #> rhub::platforms()
 #debian-clang-devel:
 #    Debian Linux, R-devel, clang, ISO-8859-15 locale
 #> rhub::check_rhub()
+
+# Reverse dependency checks.
 # remotes::install_github("r-lib/revdepcheck")
 revdepcheck::revdep_reset()
 revdepcheck::revdep_check(num_workers=4)
@@ -30,4 +46,7 @@ revdepcheck::revdep_check(num_workers=4)
 # * [ ] local build + check
 # * [ ] `devtools::spell_check()`
 # * [ ] `urlchecker::url_check()`
+# * [ ] `devtools::check_mac_release()`
 # * [ ] `devtools::check_win_release()`
+# * [ ] `devtools::check_win_devel()`
+# * [ ] `devtools::check_win_oldrelease()`
