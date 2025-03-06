be042fac39ae1bad6043a53e6fbb43834454840f
dankelley <kelley.dan@gmail.com>
2025-03-06 14:39:12 -0400

    possibly fix problem noted in https://github.com/dankelley/oce/issues/2294#issuecomment-2704595402

--- R/adp.R
+++ R/adp.R
@@ -4575,7 +4575,7 @@ adpFlagPastBoundary <- function(x = NULL, fields = NULL, df = 20, smoother, trim
         y <- brVector[ok]
         #s <- smooth.spline(X, y, df = df)
         #boundary <- predict(s, timeSeconds)$y
-        s <- smoother(X, y)
+        boundary <- smoother(X, y)
         for (itime in seq_along(x[["time"]])) {
             jbad <- x[["distance"]] > (1.0 - trim) * boundary[itime]
             mask[itime, jbad, ] <- bad
