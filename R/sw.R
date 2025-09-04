3580e449472e091b1795aa60a2eb479bcaab50d6
JoaoFilipeResende <joaofiliperesende@gmail.com>
2025-09-04 11:34:33 +0100

    add example

--- R/sw.R
+++ R/sw.R
@@ -2586,9 +2586,19 @@ swSigma4 <- function(
 #' plot(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 0, formulation = "fr"),
 #'     xlab = " Freq [kHz]", ylab = " dB/km", type = "l", log = "xy"
 #' )
-#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 0, 10, 0, formulation = "fr"), lty = "dashed")
+#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 0, 10, 0, formulation = "fr"), lty = "solid")
 #' legend("topleft", lty = c("solid", "dashed"), legend = c("S=35", "S=0"))
 #'
+#' # reproduce a comparison plot between the existing absorption models
+#' f <- 1e3 * 10^(seq(-1, 3, 0.1))
+#' plot(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 1000, formulation = "fi"), xlab = " Freq [kHz]", ylab = " dB/km", type = "l", col="blue")
+#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 1000, formulation = "fr"), lty = "solid", col="red")
+#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 1000, formulation = "ai"), lty = "solid", col="green")
+#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 1000, formulation = "sc"), lty = "solid", col="purple")
+#' lines(f / 1000, 1e3 * swSoundAbsorption(f, 35, 10, 1000, formulation = "th"), lty = "solid", col="yellow")
+#' legend("topleft", legend = c("fisher-simmons", "francois-garrison", "ainslie-mccolm", "schulkin-marsh", "thorp"), col = c("blue", "red", "green", "purple", "yellow"), lty = 1, lwd = 2)
+#' title("Comparison of sound absorption models")
+#' 
 #' @family functions that calculate seawater properties
 swSoundAbsorption <- function(
     frequency, salinity, temperature, pressure, pH = 8,
