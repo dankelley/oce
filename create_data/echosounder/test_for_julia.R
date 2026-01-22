8e0e08eae0eb0c249a4d8827eff21eaa873b16c0
dankelley <kelley.dan@gmail.com>
2026-01-22 16:19:10 -0400

    Add a test for julia code to read echosounder data

--- create_data/echosounder/test_for_julia.R
+++ create_data/echosounder/test_for_julia.R
@@ -1,13 +1,12 @@
 library(oce)
 file <- "/Users/kelley/Dropbox/data/archive/sleiwex/2008/fielddata/2008-07-01/Merlu/Biosonics/20080701_163942.dt4"
-
-# Timing test: the next takes 0.14 s (on a 10MiB file)
-#    system.time(echosounder <- read.oce(file))
-
-# Next is to record the output, for comparison during the coding of the
-# OceanAnalysis Julia library.
-e <- read.oce(file, debug=5)
-summary(e)
-# imagep(log10(echosounder[["a"]]))
-print(head(e[["time"]]))
+# Next is for comparison during OceanAnalysis development
+e <- read.oce(file, debug = 1)
+t <- e[["time"]]
+a <- e[["a"]]
+N <- 10
+cat("File starts at ", format(t[1]), ", with following decoded values, followed by ", dim(a)[2] - N, " other values\n", sep="")
+print(a[1,1:N])
+cat("First 6 times\n")
+print(t[1:6])
 
